cbfbfad14505fe9ebebdb63a1a304e4781e5f274
Sami Lehti <sami.lehti@helsinki.fi>
2009-07-15 17:31:42 +0200

    removed check prints from convert.cc

--- HeavyChHiggsToTauNu/src/convert.cc
+++ HeavyChHiggsToTauNu/src/convert.cc
@@ -1,7 +1,7 @@
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/MyEventConverter.h"
 
 void MyEventConverter::convert(const edm::Event& iEvent,const edm::EventSetup& iSetup){
-cout << "check1" << endl;
+
 	allEvents++;
 
 //        tauMETTriggerAnalysis->analyse(iEvent);
@@ -15,7 +15,7 @@ cout << "check1" << endl;
 	getCaloHits(iEvent); // needed if calohits are to be stored
         getTracks(iEvent); // needed if tracks inside jet cones are to be stored
 ////	getTrajectories(iEvent); // needed if tracker hits are to be stored
-cout << "check2" << endl;
+
 	MyEvent* saveEvent = new MyEvent;
 	saveEvent->eventNumber          = iEvent.id().event();
 	saveEvent->runNumber		= iEvent.run();
@@ -25,15 +25,15 @@ cout << "check2" << endl;
 	saveEvent->primaryVertex        = getPrimaryVertex();
 //	saveEvent->L1objects            = getL1objects(iEvent);
 //	saveEvent->HLTobjects           = getHLTObjects(iEvent);
-cout << "check3" << endl;
+
 	saveEvent->addCollection("electrons",getElectrons(iEvent,iSetup));
 //	saveEvent->photons              = getPhotons(iEvent);
-cout << "check4" << endl;
+
 	saveEvent->addCollection("muons",getMuons(iEvent));
 	saveEvent->addCollection("calotaus",getTaus(iEvent));
 	saveEvent->addCollection("pftaus",getPFTaus(iEvent));
 	saveEvent->addCollection("icone05jets",getJets(iEvent));
-cout << "check5" << endl;
+
 	saveEvent->mets			= getMET(iEvent);
 //	saveEvent->addMET("pfMET",getPFMET(iEvent));
 //	saveEvent->addMET("tcMET",getTCMET(iEvent));
@@ -45,11 +45,11 @@ cout << "check5" << endl;
 
 	saveEvent->addCollection("removedMuons",getExtraObjects(iEvent));
 ////	saveEvent->addExtraObjects("",getExtraObjects(iEvent));
-cout << "check6" << endl;
+
 	userRootTree->fillTree(saveEvent);
 	savedEvents++;
-cout << "check7" << endl;
+
 	delete saveEvent;
-cout << "check8" << endl;
+
 //	tauResolutionAnalysis->analyse(iEvent);
 }
