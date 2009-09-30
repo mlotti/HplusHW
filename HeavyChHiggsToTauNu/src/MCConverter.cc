6cd36a916596b1454c03bb9114c05526c9870aa0
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2009-09-30 10:47:08 +0300

    Improving MCConverter code

--- HeavyChHiggsToTauNu/src/MCConverter.cc
+++ HeavyChHiggsToTauNu/src/MCConverter.cc
@@ -10,13 +10,11 @@ using std::vector;
 using edm::Handle;
 using edm::SimVertexContainer;
 using edm::HepMCProduct;
-using reco::GenJetCollection;
-using reco::GenJet;
 
 MCConverter::MCConverter() {}
 MCConverter::~MCConverter() {}
 
-MyMCParticle MCConverter::convert(const GenJet& genJet){
+MyMCParticle MCConverter::convert(const reco::GenJet& genJet){
 	MyMCParticle mcJet(genJet.px(),genJet.py(),genJet.pz(),genJet.energy());
 	mcJet.pid = 0; //= genJet.pdgId();
 	mcJet.status = 4;
@@ -24,26 +22,21 @@ MyMCParticle MCConverter::convert(const GenJet& genJet){
 }
 
 void MCConverter::addMCJets(const edm::Event& iEvent, vector<MyMCParticle>& mcParticles){
-	Handle<GenJetCollection> genJetHandle;
-        try{
-          iEvent.getByLabel("iterativeCone5GenJets",genJetHandle);
-        }catch(...) {;}
+	Handle<reco::GenJetCollection> genJetHandle;
+        iEvent.getByLabel("iterativeCone5GenJets",genJetHandle);
 
-        if(genJetHandle.isValid()){
+        const reco::GenJetCollection & genJetCollection = *(genJetHandle.product());
+        //cout << "MC GenJets " << genJetCollection.size();
 
-		const reco::GenJetCollection & genJetCollection = *(genJetHandle.product());
-                //cout << "MC GenJets " << genJetCollection.size();
+        reco::GenJetCollection::const_iterator iJet;
+        for(iJet = genJetCollection.begin(); iJet!= genJetCollection.end(); ++iJet){
 
-	        GenJetCollection::const_iterator iJet;
-        	for(iJet = genJetCollection.begin(); iJet!= genJetCollection.end(); ++iJet){
-
-			if(iJet->et() < 20 || fabs(iJet->eta()) > 2.5) continue;
-			//cout << "     GenJet Et,eta " << iJet->et() << " " << iJet->eta() << endl;
-	                MyMCParticle mcJet = convert(*iJet);
-                	mcParticles.push_back(mcJet);
-        	}
-		//cout << ", saved " << mcParticles.size() << endl;
-	}
+          if(iJet->et() < 20 || fabs(iJet->eta()) > 2.5) continue;
+          //cout << "     GenJet Et,eta " << iJet->et() << " " << iJet->eta() << endl;
+          MyMCParticle mcJet = convert(*iJet);
+          mcParticles.push_back(mcJet);
+        }
+        //cout << ", saved " << mcParticles.size() << endl;
 }
 
 
@@ -71,20 +64,16 @@ void MCConverter::addMCParticles(const edm::Event& iEvent, vector<MyMCParticle>&
         addMCJets(iEvent, mcParticles);
 
         Handle<HepMCProduct> mcEventHandle;
-        try{
-//          iEvent.getByLabel("source",mcEventHandle);
-	    iEvent.getByLabel("generator",mcEventHandle);
-        }catch(...) {;}
+//      iEvent.getByLabel("source",mcEventHandle);
+        iEvent.getByLabel("generator",mcEventHandle);
 
-        if(mcEventHandle.isValid()){
+        const HepMC::GenEvent* mcEvent = mcEventHandle->GetEvent() ;
+        //cout << "MC particles size " << mcEvent->particles_size() << endl;
 
-                const HepMC::GenEvent* mcEvent = mcEventHandle->GetEvent() ;
-                //cout << "MC particles size " << mcEvent->particles_size() << endl;
-
-		double mcMetX = 0;
-		double mcMetY = 0;
-                HepMC::GenEvent::particle_const_iterator i;
-                for(i = mcEvent->particles_begin(); i!= mcEvent->particles_end(); i++){
+        double mcMetX = 0;
+        double mcMetY = 0;
+        HepMC::GenEvent::particle_const_iterator i;
+        for(i = mcEvent->particles_begin(); i!= mcEvent->particles_end(); i++){
 /*
                         cout << "  particle " << (*i)->barcode()
                              << " " << (*i)->pdg_id()
@@ -95,69 +84,65 @@ void MCConverter::addMCParticles(const edm::Event& iEvent, vector<MyMCParticle>&
                              << endl;
 */
 
-			int id = (*i)->pdg_id();
+		int id = (*i)->pdg_id();
 
-                        if((*i)->status() == 1 && (abs(id) == 12 || abs(id) == 14 || abs(id) == 16)){
-                                mcMetX += (*i)->momentum().x();
-                                mcMetY += (*i)->momentum().y();
-                        }
+                if((*i)->status() == 1 && (abs(id) == 12 || abs(id) == 14 || abs(id) == 16)){
+                        mcMetX += (*i)->momentum().x();
+                        mcMetY += (*i)->momentum().y();
+                }
 
 //                        if( ( (*i)->status() == 1 && (*i)->momentum().perp() > 1) ||
-                        if( ( (*i)->status() == 1 ) ||
-			    ( abs(id) == 12 || abs(id) == 14 || abs(id) == 16 || abs(id) <= 21) ||
-                            ( (*i)->status() == 3) ) {
+                if( ( (*i)->status() == 1 ) ||
+                    ( abs(id) == 12 || abs(id) == 14 || abs(id) == 16 || abs(id) <= 21) ||
+                    ( (*i)->status() == 3) ) {
 
-                                // searching parents
+                        // searching parents
 
-                                vector<int> motherList;
-                                vector<int> motherBarcodes;
-                                if( (*i)->production_vertex() ) {
-                                        HepMC::GenVertex::particle_iterator iMother =
-                                                (*i)->production_vertex()->particles_begin(HepMC::parents);
-					  if(*iMother != 0) {
-                                            while( (*iMother)->production_vertex() ) {
-                                                int motherId = (*iMother)->pdg_id();
-						int motherBarCode = (*iMother)->barcode();
-                                                //cout << "          mother ids,barcode " << motherId 
-                                                //     << " " << (*iMother)->barcode() << endl;
-                                                iMother = (*iMother)->production_vertex()->particles_begin(HepMC::parents);
-						if((*iMother)->pdg_id() != motherId) {
-							motherList.push_back(motherId);
-							motherBarcodes.push_back(motherBarCode);
-						}
-                                            }
-					  }
-                                }
-
-				if(motherList.size() > 0){
-				  if(motherList[0] != id){
-					MyMCParticle mcParticle;
-					mcParticle.pid    = id;
-					mcParticle.status = (*i)->status();
-					mcParticle.barcode = (*i)->barcode();
-                                	mcParticle.SetE((*i)->momentum().e());
-                                	mcParticle.SetPx((*i)->momentum().px());
-                                	mcParticle.SetPy((*i)->momentum().py());
-                                	mcParticle.SetPz((*i)->momentum().pz());
-
-                                        //mcParticle.motherLine = myMCMotherIterator;
-	                                mcParticle.mother = motherList;
-					mcParticle.motherBarcodes = motherBarcodes;
-					//cout << "SAVING particle " << mcParticle.barcode << " " << mcParticle.pid << endl;
-					mcParticles.push_back(mcParticle);
-				  }
+                        vector<int> motherList;
+                        vector<int> motherBarcodes;
+                        if( (*i)->production_vertex() ) {
+                                HepMC::GenVertex::particle_iterator iMother =
+                                  (*i)->production_vertex()->particles_begin(HepMC::parents);
+				if(*iMother != 0) {
+                                    while( (*iMother)->production_vertex() ) {
+                                        int motherId = (*iMother)->pdg_id();
+					int motherBarCode = (*iMother)->barcode();
+                                        //cout << "          mother ids,barcode " << motherId 
+                                        //     << " " << (*iMother)->barcode() << endl;
+                                        iMother = (*iMother)->production_vertex()->particles_begin(HepMC::parents);
+					if((*iMother)->pdg_id() != motherId) {
+						motherList.push_back(motherId);
+						motherBarcodes.push_back(motherBarCode);
+					}
+                                    }
 				}
+                        }
+
+			if(motherList.size() > 0){
+			  if(motherList[0] != id){
+				MyMCParticle mcParticle;
+				mcParticle.pid    = id;
+				mcParticle.status = (*i)->status();
+				mcParticle.barcode = (*i)->barcode();
+                               	mcParticle.SetE((*i)->momentum().e());
+                               	mcParticle.SetPx((*i)->momentum().px());
+                               	mcParticle.SetPy((*i)->momentum().py());
+                               	mcParticle.SetPz((*i)->momentum().pz());
+
+                                //mcParticle.motherLine = myMCMotherIterator;
+                                mcParticle.mother = motherList;
+				mcParticle.motherBarcodes = motherBarcodes;
+				//cout << "SAVING particle " << mcParticle.barcode << " " << mcParticle.pid << endl;
+				mcParticles.push_back(mcParticle);
+			  }
 			}
 		}
-		mcMET.Set(mcMetX,mcMetY);
 	}
-
+	mcMET.Set(mcMetX,mcMetY);
 
 	// newSource from muon->tau conversion, saving the MC tau and its decay products
         Handle<HepMCProduct> mcEventHandle2;
-        try{
-          iEvent.getByLabel("newSource",mcEventHandle2);
-        }catch(...) {;}
+        iEvent.getByLabel("newSource",mcEventHandle2);
 
         if(mcEventHandle2.isValid()){
 
