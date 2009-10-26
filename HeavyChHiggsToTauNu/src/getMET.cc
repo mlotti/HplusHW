3eba4c37b0c28babd8ea8db37a00151bb05c2420
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2009-10-26 12:14:19 +0200

    MET printout uses MessageLogger

--- HeavyChHiggsToTauNu/src/getMET.cc
+++ HeavyChHiggsToTauNu/src/getMET.cc
@@ -1,5 +1,7 @@
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/MyEventConverter.h"
 
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 #include "DataFormats/METReco/interface/CaloMET.h"
 #include "DataFormats/METReco/interface/CaloMETCollection.h"
 #include "DataFormats/METReco/interface/PFMET.h"
@@ -7,15 +9,14 @@
 #include "DataFormats/METReco/interface/MET.h"
 #include "DataFormats/METReco/interface/METCollection.h"
 
+#include<sstream>
+
 template <class T>
 MyMET getMEThelper(const edm::Event& iEvent, const edm::InputTag& label, const char *print) {
         Handle<T> met;
         iEvent.getByLabel(label, met);
-        //if(!met.isValid())
-        //        return MyMET();
 
         typename T::const_iterator imet = met->begin();
-        std::cout << "     " << print << " " << label.label() << " :" << imet->px() << " " << imet->py() << std::endl;
         return MyMET(imet->px(), imet->py());
 }
 
@@ -31,4 +32,12 @@ void MyEventConverter::getMET(const edm::Event& iEvent, std::map<std::string, My
         getCaloMETs(iEvent, mets);
 	mets["pfMET"] = getMEThelper<reco::PFMETCollection>(iEvent, edm::InputTag("pfMet"), "PF MET");
 	mets["tcMET"] = getMEThelper<reco::METCollection>(iEvent, edm::InputTag("tcMet"), "track corrected MET");
+
+        if(edm::isDebugEnabled()) {
+                std::stringstream ss;
+                for(std::map<std::string, MyMET>::const_iterator iter = mets.begin(); iter != mets.end(); ++iter) {
+                        ss << "     " << iter->first << " :" << iter->second.X() << " " << iter->second.Y() << std::endl;
+                }
+                LogDebug("MyEventConverter") << "METs: " << std::endl << ss.str() << std::endl;
+        }
 }
