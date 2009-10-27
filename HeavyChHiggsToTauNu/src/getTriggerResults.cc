5015309d884f8e5098f57ff350b7506061d9cf99
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2009-10-27 10:01:41 +0200

    getTriggerResults prints with MessageLogger

--- HeavyChHiggsToTauNu/src/getTriggerResults.cc
+++ HeavyChHiggsToTauNu/src/getTriggerResults.cc
@@ -1,31 +1,31 @@
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/MyEventConverter.h"
 
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 #include "DataFormats/Common/interface/TriggerResults.h"
 #include "FWCore/Framework/interface/TriggerNames.h"
-
-#include<iostream>
 
 using edm::Handle;
 using edm::TriggerResults;
 using edm::TriggerNames;
 using edm::InputTag;
-using std::cout;
-using std::endl;
 using std::vector;
 using std::string;
 
 void MyEventConverter::getTriggerResults(const edm::Event& iEvent, const edm::InputTag& label, std::map<std::string, bool>& trigger){
         vector<Handle<TriggerResults> > hltHandles;
 	iEvent.getManyByType(hltHandles);
-	cout << "check MyEventConverter::getTriggerResults " << hltHandles.size() << endl;
+        if(edm::isDebugEnabled())
+                LogDebug("MyEventConverter") << "check MyEventConverter::getTriggerResults " << hltHandles.size() << std::endl;
 
 	for(vector<Handle<TriggerResults> >::const_iterator iHandle = hltHandles.begin();
             iHandle!= hltHandles.end(); ++iHandle){
 		if((*iHandle)->size() < 10) continue;
 
         	const std::string hltTableName = iHandle->provenance()->processName();
-        	if(printTrigger) cout << "trigger table " << hltTableName 
-                                      << " size " << (*iHandle)->size() << endl;
+        	if(edm::isDebugEnabled() && printTrigger)
+                        LogDebug("MyEventConverter") << "trigger table " << hltTableName 
+                                                     << " size " << (*iHandle)->size() << std::endl;
 
                 TriggerNames triggerNames;
                 triggerNames.init(**iHandle);
@@ -33,7 +33,8 @@ void MyEventConverter::getTriggerResults(const edm::Event& iEvent, const edm::In
                 int n = 0;
                 for(vector<string>::const_iterator i = hlNames.begin();
                                                    i!= hlNames.end(); i++){
-                        if(printTrigger) cout << "trigger: " << *i << " " << (*iHandle)->accept(n) << endl;
+                        if(edm::isDebugEnabled() && printTrigger)
+                                LogDebug("MyEventConverter") << "trigger: " << *i << " " << (*iHandle)->accept(n) << std::endl;
 
 			string s_trigger = hltTableName + "_" + *i;
 			trigger[s_trigger] = (*iHandle)->accept(n);
