ff320f4c93d6db6666c386487be8e08a6a26b1d5
Sami Lehti <sami.lehti@helsinki.fi>
2010-05-25 16:30:38 +0200

    HeavyChHiggsToTauNu: starting constructing the H+ discriminator

---
+++ HeavyChHiggsToTauNu/python/ChargedHiggsTauIDDiscrimination_cfi.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+from RecoTauTag.RecoTau.TauDiscriminatorTools import noPrediscriminants
+
+#chargedHiggsTauIDDiscrimination = cms.EDProducer("TCRecoTauDiscriminationAlgoComponent",
+#        TauProducer = cms.InputTag('pfRecoTauProducer'),
+#        Prediscriminants = noPrediscriminants
+#)
