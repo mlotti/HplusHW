3379e88755f7232429acbed1afbbffa0f0ff5db5
Sami Lehti <sami.lehti@helsinki.fi>
2010-05-26 11:23:49 +0200

    HeavyChHiggsToTauNu: H+ discriminator track quality cuts

--- HeavyChHiggsToTauNu/python/ChargedHiggsTauIDDiscrimination_cfi.py
+++ HeavyChHiggsToTauNu/python/ChargedHiggsTauIDDiscrimination_cfi.py
@@ -1,14 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoTauTag.RecoTau.TauDiscriminatorTools import noPrediscriminants
+from RecoTauTag.RecoTau.PFRecoTauQualityCuts_cfi import *
+hplusTrackQualityCuts = PFTauQualityCuts.clone()
+hplusTrackQualityCuts.maxTrackChi2 = cms.double(10.)
+hplusTrackQualityCuts.minTrackHits = cms.uint32(8)
+
+#from RecoTauTag.RecoTau.TauDiscriminatorTools import noPrediscriminants
 
 from RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingTrackPtCut_cfi import *
 hplusTauDiscriminationByLeadingTrackPtCut = pfRecoTauDiscriminationByLeadingTrackPtCut.clone()
 hplusTauDiscriminationByLeadingTrackPtCut.PFTauProducer = cms.InputTag('fixedConePFTauProducer')
 hplusTauDiscriminationByLeadingTrackPtCut.MinPtLeadingObject = cms.double(20.0)
-
-#from RecoTauTag.RecoTau.PFRecoTauQualityCuts_cfi import *
-#hplusPFTauQualityCuts = copy.deepcopy(PFTauQualityCuts)
+hplusTauDiscriminationByLeadingTrackPtCut.qualityCuts = hplusTrackQualityCuts
 
 hplusTauPrediscriminants = cms.PSet(
     BooleanOperator = cms.string("and"),
@@ -22,6 +25,7 @@ from RecoTauTag.RecoTau.PFRecoTauDiscriminationByTrackIsolation_cfi import *
 hplusTauDiscrimination = pfRecoTauDiscriminationByTrackIsolation.clone()
 hplusTauDiscrimination.PFTauProducer = cms.InputTag('fixedConePFTauProducer')
 hplusTauDiscrimination.Prediscriminants = hplusTauPrediscriminants
+hplusTauDiscrimination.qualityCuts = hplusTrackQualityCuts
 
 hplusTauDiscriminationSequence = cms.Sequence(
     hplusTauDiscriminationByLeadingTrackPtCut *
