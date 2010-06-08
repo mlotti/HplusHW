4410dbce30d6dce63833136b008a18cb744f6051
Sami Lehti <sami.lehti@helsinki.fi>
2010-06-08 10:57:14 +0200

    TauFakeRate: trying to get plotting working..

--- TauFakeRate/test/plotCaloTau2.py
+++ TauFakeRate/test/plotCaloTau2.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     ROOT.gROOT.SetBatch(True)
     ROOT.gROOT.SetStyle("Plain")
 
+
     # Pull out the defintion of the ntuple (this should be improved)
     # Don't ever access this file directly, just use it to parse event content...
     dummy_file = ROOT.TFile("../data/tauIdEff_ntuple.root")
@@ -21,10 +22,13 @@ if __name__ == "__main__":
     selectedTaus = ntuple_manager.get_ntuple(
         "patPFTausDijetTagAndProbeShrinkingCone")
 
+    my_selection = selectedTaus.expr('abs($eta) < 2.5') & \
+                   selectedTaus.expr('$byIsolation > 0.5')
+
     # Build the plot manager.  The plot manager keeps track of all the samples
     # and ensures they are correctly normalized w.r.t. luminosity.  See 
     # samples.py for available samples.
-    data_sample = samples.data_test2
+    data_sample = samples.data_test
     mc_sample = samples.mc_test
    
     plotter = PlotManager()
@@ -44,9 +48,9 @@ if __name__ == "__main__":
     canvas_pt = ROOT.TCanvas("canvas_pt", "", 500, 500)
 
     pt_result = plotter.distribution(
-        expression=selectedTaus.expr('$pt'),
-        selection=selectedTaus.expr('abs($eta) < 10'),
-        binning = (100, 0, 50),
+        expression = selectedTaus.expr('$pt'),
+        selection  = my_selection,
+        binning    = (100, 0, 50),
         x_axis_title = "P_{T}"
     )
     # this should draw a comparison on the canvas, but pt_result
@@ -57,13 +61,28 @@ if __name__ == "__main__":
     canvas_pt.SaveAs("caloTau_pt.png")
 
 ###################################################################################
-
+#    canvas_ptraw = ROOT.TCanvas("canvas_ptraw", "", 500, 500)
+#                                                                                             
+#    ptraw_result = plotter.distribution(
+#        expression = selectedTaus.expr('$jetPt'),
+#        selection  = my_selection,
+#        binning    = (100, 0, 50)
+#        x_axis_title = "P_{T}(raw)"
+#    )                                                                                        
+#    # this should draw a comparison on the canvas, but pt_result                             
+#    # now contains some helpful stuff.                                                       
+##    print "MC average raw pt: %f" % \
+##            ptraw_result['samples']['mc_test']['mean']
+#
+#    canvas_ptraw.SaveAs("caloTau_rawpt.png")
+#  
+###################################################################################
     canvas_eta = ROOT.TCanvas("canvas_eta", "", 500, 500)
 
     eta_result = plotter.distribution(
-        expression=selectedTaus.expr('$eta'),
-        selection=selectedTaus.expr('abs($eta) < 5'),
-        binning = (100, -5, 5),
+        expression = selectedTaus.expr('$eta'),
+        selection  = selectedTaus.expr('abs($eta) < 5'),
+        binning    = (100, -5, 5),
         x_axis_title = "eta"
 #        expression=selectedTaus.expr('$eta'),
 #        selection=selectedTaus.expr('$pt > 5.0'),
@@ -74,28 +93,13 @@ if __name__ == "__main__":
 
 ###################################################################################
 
+#    canvas_phi = ROOT.TCanvas("canvas_phi", "", 500, 500)                       
+#
 #    phi_result = plotter.distribution(
-#        expression=selectedTaus.expr('$phi'),
-#	selection=selectedTaus.expr('abs($eta) < 2.5'),
-#        binning = (100, -5, 5),
+#        expression = selectedTaus.expr('$phi'),
+#	selection  = my_selection,
+#        binning    = (100, -500, 500),
 #        x_axis_title = "phi"
 #    )
-#    canvas.SaveAs("caloTau_phi.C")
+#    canvas_phi.SaveAs("caloTau_phi.png")
 
-
-#    denominator = selectedTaus.expr('abs($eta) < 2.5 & $pt > 5')
-#    numerator = selectedTaus.expr('$byTaNCfrHalfPercent') & denominator
-#
-#    pt_eff_result = plotter.efficiency(
-#        expression=selectedTaus.expr('$pt'),
-#        denominator = denominator,
-#        numerator = numerator,
-#        binning = (10, 0, 50),
-#        x_axis_title = "P_{T}"
-#    )
-#
-#    # Add a legend
-#    pt_eff_result['legend'].make_legend().Draw()
-#
-#    canvas.SaveAs("test_pt_eff.png")
-#
