53192a94884ce26d3169f9705c1d91866b0006f5
Sami Lehti <sami.lehti@helsinki.fi>
2010-06-17 10:47:15 +0200

    TauFakeRate: switched to v212 ntuples

--- TauFakeRate/python/samples.py
+++ TauFakeRate/python/samples.py
@@ -7,7 +7,7 @@ samples.py
 
 Central defintion of data sources for commissioning.
 
-$Id: samples.py,v 1.4 2010/06/12 00:39:01 friis Exp $
+$Id: samples.py,v 1.6 2010/06/17 00:41:43 friis Exp $
 
 '''
 
@@ -24,25 +24,28 @@ _MC_LUMI_MAP_FILE = os.path.join(
 # Arugments: lumi map file, name of output collection, merge/add, list of samples
 # to take from the JSON file
 print "loading definition of Ztautau signal Monte Carlo samples..."
-ztautau_mc = build_sample(_MC_LUMI_MAP_FILE, "mc_ztt", "merge", "Ztautau")
+ztautau_mc = build_sample(_MC_LUMI_MAP_FILE, "mc_ztt", "merge", datasets = ["Ztautau"])
 
 # Merge multiple pt hat bins
+#
+# CV: restrict analysis to first pt hat bin for now...
+#
 print "loading definition of QCD background Monte Carlo samples..."
-qcd_mc = build_sample(_MC_LUMI_MAP_FILE, "mc_qcd", "merge", "QCD_Pt15", "QCD_Pt30", "QCD_Pt80", "QCD_Pt170")
-#qcd_mc = build_sample(_MC_LUMI_MAP_FILE, "mc_qcd", "merge", "QCD_Pt15")
+##qcd_mc = build_sample(_MC_LUMI_MAP_FILE, "mc_qcd", "merge", "QCD_Pt15", "QCD_Pt30", "QCD_Pt80", "QCD_Pt170")
+qcd_mc = build_sample(_MC_LUMI_MAP_FILE, "mc_qcd", "merge", datasets = ["QCD_Pt15"])
 
 print "loading definition of min. Bias background Monte Carlo samples..."
-minbias_mc = build_sample(_MC_LUMI_MAP_FILE, "mc_minbias", "merge", "minBias")
+minbias_mc = build_sample(_MC_LUMI_MAP_FILE, "mc_minbias", "merge", datasets = ["minBias"])
 
 # For data, we use the add mode, to concatenate data
 print "loading definition of Data samples..."
-data = build_sample(_DATA_LUMI_MAP_FILE, "data", "add", "Data_part01")
+data = build_sample(_DATA_LUMI_MAP_FILE, "data", "add", datasets = ["Data_part01"])
 
 # Small test mc sample
 print "loading mc test"
-mc_test   = build_sample(_MC_LUMI_MAP_FILE, "mc_test", "merge", "MC_test")
+mc_test   = build_sample(_MC_LUMI_MAP_FILE, "mc_test", "merge", datasets = ["MC_test"])
 
 # Small test data sample
 print "loading data test"
-data_test = build_sample(_DATA_LUMI_MAP_FILE, "data_test", "add", "Data_test")
+data_test = build_sample(_DATA_LUMI_MAP_FILE, "data_test", "add", datasets = ["Data_test"])
 
--- TauFakeRate/test/readme.txt
+++ TauFakeRate/test/readme.txt
@@ -4,6 +4,6 @@ running:
 
 8.6.2010/S.Lehti CMSSW_3_6_2	
   Tags needed:
-  cvs co -r b3_6_x_TauIdCommissioning_v2_1_2 TauAnalysis/TauIdEfficiency/python/ntauples
-  cvs co -r b3_6_x_TauIdCommissioning_v2_1_2 TauAnalysis/TauIdEfficiency/python/tools
+  cvs co -r b3_6_x_TauIdCommissioning_v2_1_4 TauAnalysis/TauIdEfficiency/python/ntauples
+  cvs co -r b3_6_x_TauIdCommissioning_v2_1_4 TauAnalysis/TauIdEfficiency/python/tools
 
