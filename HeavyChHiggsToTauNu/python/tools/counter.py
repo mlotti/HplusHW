e133365f35aaccacfc39e0ae10ccaf0a2a1c6060
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-09-09 10:17:20 +0300

    Reorganizing counter printing code

--- HeavyChHiggsToTauNu/python/tools/counter.py
+++ HeavyChHiggsToTauNu/python/tools/counter.py
@@ -25,3 +25,129 @@ def rescaleInfo(d):
         ret[k] = v*factor
 
     return ret
+
+class Dataset:
+    def __init__(self, name, info):
+        self.name = name
+        self.info = info
+
+    def getName(self):
+        return self.name
+
+    def setCrossSection(self, value):
+        self.info["crossSection"] = value
+
+    def getCrossSection(self):
+        return self.info["crossSection"]
+
+    def setAllEvents(self, allevTuple):
+        self.normFactor = self.getCrossSection() / allevTuple[1]
+
+    def getNormFactor(self):
+        return self.normFactor
+
+class Counter:
+    def __init__(self, counter):
+        self.data = counter
+        self.dataset = None
+
+    def setDataset(self, dataset):
+        self.dataset = dataset
+
+    def getNames(self):
+        return [x[0] for x in self.data]
+
+    def getDataset(self):
+        return self.dataset
+
+    def getCounterValue(self, index):
+        return self.data[index][1]
+
+    def getCounterCrossSection(self, index):
+        return self.dataset.getNormFactor() * self.getCounterValue(index)
+
+class DatasetCounter:
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.mainCounter = None
+        self.subCounters = {}
+
+    def setMainCounter(self, counter):
+        counter.setDataset(self.dataset)
+        self.mainCounter = counter
+
+    def addSubCounter(self, name, counter):
+        counter.setDataset(self.dataset)
+        self.subCounters[name] = counter
+
+    def getDataset(self):
+        return self.dataset
+
+    def getMainCounter(self):
+        return self.mainCounter
+
+    def getSubCounterNames(self):
+        return self.subCounters.keys()
+
+    def getSubCounter(self, name):
+        return self.subCounters[name]
+
+class Counters:
+    def __init__(self):
+        self.counters = []
+
+    def addCounter(self, counter):
+        self.counters.append(counter)
+
+    def getDatasets(self):
+        return [x.getDataset() for x in self.counters]
+
+    def getMainCounters(self):
+        return [x.getMainCounter() for x in self.counters]
+
+    def getSubCounterNames(self):
+        if len(self.counters) == 0:
+            return []
+
+        names = self.counters[0].getSubCounterNames()
+        # Check that the subcounter names for all datasets are same
+        for c in self.counters[1:]:
+            cnames = c.getSubCounterNames()
+            if len(names) != len(cnames):
+                raise Exception("The lengths of subcounters are not equal! Dataset %s has %d, %s has %d" % (self.counters[0].getDataset().getName(), len(names), c.getDataset().getName(), len(cnames)))
+            for ind, val in enumerate(names):
+                if val != cnames[ind]:
+                    raise Exception("The subcounter names are not equal!")
+
+        return names
+
+    def getSubCounters(self, name):
+        return [x.getSubCounter(name) for x in self.counters]
+
+
+def readCountersFileDir(fname, dirname, datasetname, crossSections):
+    f = ROOT.TFile.Open(fname)
+    info = rescaleInfo(histoToDict(f.Get("configInfo").Get("configinfo")))
+
+    dataset = Dataset(datasetname, info)
+    if datasetname in crossSections:
+        dataset.setCrossSection(crossSections[datasetname])
+
+    dctr = DatasetCounter(dataset)
+
+    directory = f.Get(dirname)
+    dirlist = directory.GetListOfKeys()
+    diriter = dirlist.MakeIterator()
+    key = diriter.Next()
+
+    while key:
+        # main counter
+        if key.GetName() == "counter":
+            ctr = histoToCounter(key.ReadObj())
+            dataset.setAllEvents(ctr[0])
+            dctr.setMainCounter(Counter(ctr))
+        else:
+            dctr.addSubCounter(key.GetName(), Counter(histoToCounter(key.ReadObj())))
+        key = diriter.Next()
+
+    return dctr
--- HeavyChHiggsToTauNu/scripts/hplusPrintCounters.py
+++ HeavyChHiggsToTauNu/scripts/hplusPrintCounters.py
@@ -10,102 +10,64 @@ import HiggsAnalysis.HeavyChHiggsToTauNu.tools.counter as counter
 
 import ROOT
 
-class Dataset:
-    def __init__(self, name, info):
-        self.name = name
-        self.info = info
-
-    def getName(self):
-        return self.name
-
-    def setCrossSection(self, value):
-        self.info["crossSection"] = value
-
-    def getCrossSection(self):
-        return self.info["crossSection"]
-
-    def setAllEvents(self, allevTuple):
-        self.normFactor = self.getCrossSection() / allevTuple[1]
-
-    def getNormFactor(self):
-        return self.normFactor
-
-class Counter:
-    def __init__(self, dataset, counter):
-        self.dataset = dataset
-        self.data = counter
-
-    def getNames(self):
-        return [x[0] for x in self.data]
-
-    def getDataset(self):
-        return self.dataset
-
-    def getCounterValue(self, index):
-        return self.data[index][1]
-
-    def getCounterCrossSection(self, index):
-        return self.dataset.getNormFactor() * self.getCounterValue(index)
-
-
 class EventPrinter:
-    def value(self, dataset, ictr):
-        return dataset.getCounterValue(ictr)
+    def value(self, counter, ictr):
+        return counter.getCounterValue(ictr)
     def format(self, width):
         return "%%%dd"%width
     def name(self):
         return "(events)"
 
 class CrossSectionPrinter:
-    def value(self, dataset, ictr):
-        return dataset.getCounterCrossSection(ictr)
+    def value(self, counter, ictr):
+        return counter.getCounterCrossSection(ictr)
     def format(self, width):
         return "%%%d.4g"%width
     def name(self):
         return "(cross section in pb)"
 
-def printCounter(counter, printer):
-    if len(counter) == 0:
+# counters is an array of counter.Counter objects, one element per
+# dataset
+def printCounter(counters, printer):
+    if len(counters) == 0:
         return
 
-    counters = counter[0].getNames()
-    maxlen = max([len(x) for x in counters])
+    counterNames = counters[0].getNames()
+    maxlen = max([len(x) for x in counterNames])
 
     clen = []
-    for dataset in counter:
-        clen.append(max(15, len(dataset.getDataset().getName())+2))
+    for counter in counters:
+        clen.append(max(15, len(counter.getDataset().getName())+2))
 
     lfmt = "%%-%ds" % (maxlen+2)
     dfmt = "%%%ds"
 
     hdr = lfmt % "Counter"
-    for i, cdataset in enumerate(counter):
-        hdr += (dfmt%clen[i]) % cdataset.getDataset().getName()
+    for i, counter in enumerate(counters):
+        hdr += (dfmt%clen[i]) % counter.getDataset().getName()
     print hdr
 
-    for ictr, ctr in enumerate(counters):
+    for ictr, ctr in enumerate(counterNames):
         line = lfmt % ctr
 
-        for i, dataset in enumerate(counter):
-            line += printer.format(clen[i]) % printer.value(dataset, ictr)
+        for i, counter in enumerate(counters):
+            line += printer.format(clen[i]) % printer.value(counter, ictr)
 
         print line
 
-def printDatasetInfo(counter):
+def printDatasetInfo(datasets):
     col1hdr = "Dataset"
     col2hdr = "Cross section (pb)"
     col3hdr = "Norm. factor"
 
-    maxlen = max([len(x.getDataset().getName()) for x in counter]+[len(col1hdr)])
+    maxlen = max([len(x.getName()) for x in datasets]+[len(col1hdr)])
     c1fmt = "%%-%ds" % (maxlen+2)
     c2fmt = "%%%d.4g" % (len(col2hdr)+2)
     c3fmt = "%%%d.4g" % (len(col3hdr)+2)
 
     print (c1fmt%col1hdr)+"  "+col2hdr+"  "+col3hdr
-    for cdataset in counter:
-        dataset = cdataset.getDataset()
+    for dataset in datasets:
         print (c1fmt % dataset.getName()) + c2fmt%dataset.getCrossSection() + c3fmt%dataset.getNormFactor()
-
 
 def main(opts):
     taskdirs = multicrab.getTaskDirectories(opts)
@@ -115,8 +77,7 @@ def main(opts):
         (name, value) = o.split(":")
         crossSections[name] = float(value)
 
-    mainCounters = []
-    subCounters = {}
+    counters = counter.Counters()
 
     for d in taskdirs:
         files = glob.glob(os.path.join(d, "res", opts.input))
@@ -127,40 +88,13 @@ def main(opts):
             print "Error: no files matched to input for task %s" % d
             return 1
 
+        counters.addCounter(counter.readCountersFileDir(files[0], opts.counterdir, d, crossSections))
 
-        f = ROOT.TFile.Open(files[0])
-        info = counter.rescaleInfo(counter.histoToDict(f.Get("configInfo").Get("configinfo")))
-
-        dataset = Dataset(d, info)
-        if d in crossSections:
-            dataset.setCrossSection(crossSections[d])
-
-        directory = f.Get(opts.counterdir)
-        dirlist = directory.GetListOfKeys()
-        diriter = dirlist.MakeIterator()
-        key = diriter.Next()
-
-        while key:
-            # main counter
-            if key.GetName() == "counter":
-                ctr = counter.histoToCounter(key.ReadObj())
-                dataset.setAllEvents(ctr[0])
-                mainCounters.append( Counter(dataset, ctr) )
-            else:
-                if key.GetName() in subCounters:
-                    #subCounters[d][key.GetName()] = counter.histoToCounter(key.ReadObj())
-                    subCounters[key.GetName()].append( Counter(dataset, counter.histoToCounter(key.ReadObj())) )
-                else:
-                    #subCounters[d] = {key.GetName(): counter.histoToCounter(key.ReadObj())}
-                    subCounters[key.GetName()] = [ Counter(dataset, counter.histoToCounter(key.ReadObj())) ]
-
-            key = diriter.Next()
 
     print "============================================================"
     print "Dataset info: "
-    printDatasetInfo(mainCounters)
+    printDatasetInfo(counters.getDatasets())
     print
-
 
     printer = None
     if opts.mode == "events":
@@ -173,13 +107,13 @@ def main(opts):
 
     print "============================================================"
     print "Main counter %s: " % printer.name()
-    printCounter(mainCounters, printer)
+    printCounter(counters.getMainCounters(), printer)
     print 
 
-    for key, sub in subCounters.iteritems():
+    for name in counters.getSubCounterNames():
         print "============================================================"
-        print "Subcounter %s %s: " % (key, printer.name())
-        printCounter(sub, printer)
+        print "Subcounter %s %s: " % (name, printer.name())
+        printCounter(counters.getSubCounters(name), printer)
         print
 
     print
