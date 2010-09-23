c9b8a6a0655bcf27beff114fa2a4ab496fe6a85f
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-09-23 09:37:50 +0200

    Updated signalAnalysisNtuple_cfg

--- HeavyChHiggsToTauNu/test/signalAnalysisNtuple_cfg.py
+++ HeavyChHiggsToTauNu/test/signalAnalysisNtuple_cfg.py
@@ -1,13 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 from HiggsAnalysis.HeavyChHiggsToTauNu.HChOptions import getOptions
 
+dataVersion = "35X"
+#dataVersion = "35Xredigi"
+#dataVersion = "36X"
+#dataVersion = "36Xspring10"
+#dataVersion = "37X"
+
 options = getOptions()
+if options.dataVersion != "":
+    dataVersion = options.dataVersion
+print "Assuming data is ", dataVersion
 
 process = cms.Process("HChSignalAnalysis")
 
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = cms.string("START38_V9::All")
@@ -31,21 +40,18 @@ process.MessageLogger.categories.append("EventCounts")
 process.MessageLogger.cerr.FwkReport.reportEvery = 5000
 process.TFileService.fileName = "histograms.root"
 
-process.signalAnalysisNtuple = cms.EDFilter("HPlusSignalAnalysisNtupleProducer",
-    branchAliasPrefix = cms.untracked.string(""),
+# Tau ID and jet selection
+process.tauSelection = cms.EDFilter("HPlusTauPtrSelectorFilter",
     tauSelection = cms.untracked.PSet(
         src = cms.untracked.InputTag("patPFTauProducerFixedCone"),
         #src = cms.untracked.InputTag("selectedPatTaus"),
         ptCut = cms.untracked.double(20),
         etaCut = cms.untracked.double(2.4),
         leadingTrackPtCut = cms.untracked.double(10)
-    ),
-    tauDiscriminators = cms.untracked.VPSet(
-        cms.untracked.PSet(
-            discriminator = cms.untracked.string("HChTauIDtauPolarizationCont"),
-            branch = cms.untracked.string("discrHChTauIDtauPolarizationCont")
-        )
-    ),
+    )
+)
+process.jetSelection = cms.EDFilter("HPlusJetPtrSelectorFilter",
+    tauSrc = cms.untracked.InputTag("tauSelection"),
     jetSelection = cms.untracked.PSet(
         src = cms.untracked.InputTag("selectedPatJets"),
 #        src = cms.untracked.InputTag("selectedPatJetsAK5JPT"),
@@ -54,29 +60,78 @@ process.signalAnalysisNtuple = cms.EDFilter("HPlusSignalAnalysisNtupleProducer",
         etaCut = cms.untracked.double(2.4),
         minNumber = cms.untracked.uint32(3)
     ),
-    btags = cms.untracked.vstring("trackCountingHighEffBJetTags"),
-    METs = cms.untracked.VPSet(
-       cms.untracked.PSet(
-          src = cms.untracked.InputTag("patMETs"),
-          label = cms.untracked.string("caloMET"),
-       ),
-       cms.untracked.PSet(
-          src = cms.untracked.InputTag("patMETsPF"),
-          label = cms.untracked.string("pfMET"),
-       ),
-       cms.untracked.PSet(
-          src = cms.untracked.InputTag("patMETsTC"),
-          label = cms.untracked.string("tcMET"),
-       )
-    )
 )
+
+# Add trigger bits to the ntuple
+import HiggsAnalysis.HeavyChHiggsToTauNu.HChTrigger_cfi as HChTrigger
+HChTrigger.customise(process, dataVersion)
+
+# Tau and jet ntuple producers
+process.tauNtuple = cms.EDProducer("HPlusTauNtupleProducer",
+    src = cms.untracked.InputTag("tauSelection"),
+    prefix = cms.untracked.string("tau_"),
+    tauDiscriminators = cms.untracked.VPSet(
+        cms.untracked.PSet(
+            discriminator = cms.untracked.string("HChTauIDtauPolarizationCont"),
+            branch = cms.untracked.string("discrHChTauIDtauPolarizationCont")
+        )
+    )
+)    
+process.jetNtuple = cms.EDProducer("HPlusJetNtupleProducer",
+    src = cms.untracked.InputTag("jetSelection"),
+    prefix = cms.untracked.string("jet_"),                              
+    btags = cms.untracked.vstring("trackCountingHighEffBJetTags")
+)
+
+# All and ntuplized event counters
 process.allEvents = cms.EDProducer("EventCountProducer")
 process.ntupleEvents = cms.EDProducer("EventCountProducer")
+
+# Path without MET data, they come below
 process.ntuplePath = cms.Path(
     process.allEvents *
-    process.signalAnalysisNtuple *
-    process.ntupleEvents
+    process.HChTriggers *
+    process.tauSelection *
+    process.jetSelection *
+    process.ntupleEvents *
+    process.tauNtuple *
+    process.jetNtuple
 )
+
+# Helper function for MET producers to decrease the mount of typing
+def addMet(process, src, alias):
+    metNtuple = cms.EDProducer("HPlusMETNtupleProducer",
+        src = cms.untracked.InputTag(src),
+        alias = cms.untracked.string(alias)
+    )
+    transverseMass = cms.EDProducer("HPlusTransverseMassProducer",
+        tauSrc = cms.InputTag("tauSelection"),
+        metSrc = cms.InputTag(src)
+    )
+    transverseMassNtuple = cms.EDProducer("HPlusMassNtupleProducer",
+        src = cms.untracked.InputTag(alias+"TransverseMass"),
+        alias = cms.untracked.string(alias+"_transverseMass")
+    )
+    deltaPhiNtuple = cms.EDProducer("HPlusDeltaPhiNtupleProducer",
+        src1 = cms.untracked.InputTag("tauSelection"),
+        src2 = cms.untracked.InputTag(src),
+        alias = cms.untracked.string(alias+"_deltaPhi")
+    )
+
+    process.__setattr__(alias+"Ntuple", metNtuple)
+    process.__setattr__(alias+"TransverseMass", transverseMass)
+    process.__setattr__(alias+"TransverseMassNtuple", transverseMassNtuple)
+    process.__setattr__(alias+"DeltaPhiNtuple", deltaPhiNtuple)
+    process.ntuplePath *= metNtuple
+    process.ntuplePath *= transverseMass
+    process.ntuplePath *= transverseMassNtuple
+    process.ntuplePath *= deltaPhiNtuple
+
+# Add the METs with the helper function
+addMet(process, "patMETs", "caloMET")
+addMet(process, "patMETsPF", "pfMET")
+addMet(process, "patMETsTC", "tcMET")
+
 
 ################################################################################
 
@@ -86,10 +141,17 @@ process.out = cms.OutputModule("PoolOutputModule",
     ),
     fileName = cms.untracked.string('output.root'),
     outputCommands = cms.untracked.vstring(
-        "keep *_*_*_HChSignalAnalysis",
-        "drop *_counterNames_*_*",
-        "drop *_counterInstances_*_*",
-        "drop edmMergeableCounter_*_subcount*_*"
+        "drop *",
+        "keep double_*_*_HChSignalAnalysis",
+        "keep float_*_*_HChSignalAnalysis",
+        "keep int_*_*_HChSignalAnalysis",
+        "keep edmMergeableCounter_*_*_*",
+        "drop edmMergeableCounter_*_subcount*_HChSignalAnalysis"
+#         "keep *_*_*_HChSignalAnalysis",
+#         "drop *_counterNames_*_*",
+#         "drop *_counterInstances_*_*",
+#         "drop edmMergeableCounter_*_subcount*_*",
+#         "drop *PtrVector_*_*_*"
 #	"drop *",
 #        "keep edmMergeableCounter_*_*_*"
     )
