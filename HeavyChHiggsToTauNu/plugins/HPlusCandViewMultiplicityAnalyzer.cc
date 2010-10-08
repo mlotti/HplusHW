64bb67dc4088ee799378991a2eb7c1f2d7f783b2
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-10-08 13:10:49 +0300

    HPlusCandViewMultiplicityAnalyzer operates on multiple collections

--- HeavyChHiggsToTauNu/plugins/HPlusCandViewMultiplicityAnalyzer.cc
+++ HeavyChHiggsToTauNu/plugins/HPlusCandViewMultiplicityAnalyzer.cc
@@ -31,25 +31,36 @@ class HPlusCandViewMultiplicityAnalyzer: public edm::EDAnalyzer {
   /// Default EDAnalyzer method - called at the end of the job
   virtual void endJob();
 
-  edm::InputTag src_;
-  TH1 *histo_;
+  struct Data {
+    Data(edm::InputTag s, TH1 *h): src(s), histo(h) {}
+
+    edm::InputTag src;
+    TH1 *histo;
+  };
+
+  std::vector<Data> data_;
 };
 
 
-HPlusCandViewMultiplicityAnalyzer::HPlusCandViewMultiplicityAnalyzer(const edm::ParameterSet& iConfig):
-  src_(iConfig.getParameter<edm::InputTag>("src")),
-  histo_(0)
-{
+HPlusCandViewMultiplicityAnalyzer::HPlusCandViewMultiplicityAnalyzer(const edm::ParameterSet& iConfig) {
   edm::Service<TFileService> fs;
 
-  std::string title("multiplicity");
-  if(iConfig.exists("title"))
-    title = iConfig.getUntrackedParameter<std::string>("title");
-  histo_ = fs->make<TH1F>("multiplicity", title.c_str(),
-                          iConfig.getUntrackedParameter<int>("nbins"),
-                          iConfig.getUntrackedParameter<int>("min"),
-                          iConfig.getUntrackedParameter<int>("max"));
+  std::vector<std::string> names = iConfig.getParameterNamesForType<edm::ParameterSet>(false); // take only untracked parameters
+  data_.reserve(names.size());
 
+  for(size_t i=0; i<names.size(); ++i) {
+    edm::ParameterSet pset = iConfig.getUntrackedParameter<edm::ParameterSet>(names[i]);
+    
+    std::string title("multiplicity");
+    std::string name = names[i]+"_multiplicity";
+    if(iConfig.exists("title"))
+      title = pset.getUntrackedParameter<std::string>("title");
+    TH1 *histo = fs->make<TH1F>(name.c_str(), title.c_str(),
+                                pset.getUntrackedParameter<int>("nbins"),
+                                pset.getUntrackedParameter<int>("min"),
+                                pset.getUntrackedParameter<int>("max"));
+    data_.push_back(Data(pset.getParameter<edm::InputTag>("src"), histo));
+  }
 }
 
 HPlusCandViewMultiplicityAnalyzer::~HPlusCandViewMultiplicityAnalyzer() {}
@@ -57,11 +68,12 @@ HPlusCandViewMultiplicityAnalyzer::~HPlusCandViewMultiplicityAnalyzer() {}
 void HPlusCandViewMultiplicityAnalyzer::beginJob() {}
 
 void HPlusCandViewMultiplicityAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
-  edm::Handle<edm::View<reco::Candidate> > hcands;
-  iEvent.getByLabel(src_, hcands);
+  for(size_t i=0; i<data_.size(); ++i) {
+    edm::Handle<edm::View<reco::Candidate> > hcands;
+    iEvent.getByLabel(data_[i].src, hcands);
 
-  histo_->Fill(hcands->size());
-
+    data_[i].histo->Fill(hcands->size());
+  }
 }
 
 void HPlusCandViewMultiplicityAnalyzer::endJob() {}
