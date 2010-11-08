05a1fdc606c4bb7d01ba309f3699827643de94bf
Sami Lehti <sami.lehti@helsinki.fi>
2010-11-08 12:25:01 +0100

    Updated checkoutTags

--- HeavyChHiggsToTauNu/test/checkoutTags.sh
+++ HeavyChHiggsToTauNu/test/checkoutTags.sh
@@ -11,6 +11,7 @@
 # 21.10.2010/M.Kortelainen CMSSW_3_8_5_patch2 (Updated PatAlgos tag, added revision numbers for files)
 # 28.10.2010/M.Kortelainen CMSSW_3_8_5_patch3 (Electron ID and additional PAT tags from the release notes)
 # 2.11.2010/M.Kortelainen CMSSW_3_8_5_patch3 (tag for filterJSON.py etc. scripts) 
+# 8.11.2010/S.Lehti CMSSW_3_8_6 (New tau software and HPS + Tanc)
 
 
 #TARGET="analysis"
@@ -32,35 +33,10 @@ cvs co -r V01-04-00 FWCore/PythonUtilities
 #if [ "x$TARGET" = "xpattuple" ]; then
 cvs co -r V00-03-13 RecoEgamma/ElectronIdentification
 cvs co -r V00-02-01 ElectroWeakAnalysis/WENu
-cvs co -r V00-24-00 RecoTauTag/Configuration
-cvs co -r V00-24-00 RecoTauTag/RecoTau
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/CaloRecoTauDiscriminationByCharge.cc
-cvs co -r 1.2 RecoTauTag/RecoTau/plugins/CaloRecoTauDiscriminationByDeltaE.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/CaloRecoTauDiscriminationByFlightPathSignificance.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/CaloRecoTauDiscriminationByInvMass.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/CaloRecoTauDiscriminationByNProngs.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/CaloRecoTauDiscriminationByTauPolarization.cc
-cvs co -r 1.3 RecoTauTag/RecoTau/plugins/CaloRecoTauDiscriminationByIsolation.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByDeltaE.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByFlightPathSignificance.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByInvMass.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByNProngs.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByTauPolarization.cc
-cvs co -r 1.1 RecoTauTag/RecoTau/python/CaloRecoTauDiscriminationByCharge_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/CaloRecoTauDiscriminationByDeltaE_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/CaloRecoTauDiscriminationByFlightPathSignificance_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/CaloRecoTauDiscriminationByInvMass_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/CaloRecoTauDiscriminationByNProngs_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/CaloRecoTauDiscriminationByTauPolarization_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByDeltaE_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByFlightPathSignificance_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByInvMass_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByNProngs_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByTauPolarization_cfi.py
-cvs co -r 1.1 RecoTauTag/RecoTau/python/CaloRecoTauDiscriminationForChargedHiggs_cfi.py
-cvs co -r 1.2 RecoTauTag/RecoTau/python/PFRecoTauDiscriminationForChargedHiggs_cfi.py
-cvs co -r 1.3 RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByIsolationChargedPtSum_cfi.py
-cvs up -r 1.2 RecoTauTag/RecoTau/plugins/BuildFile.xml
+cvs co -r V01-00-00 DataFormats/TauReco
+cvs co -r V01-00-01 RecoTauTag/Configuration
+cvs co -r V01-00-05 RecoTauTag/RecoTau
+cvs co -r V01-00-00 RecoTauTag/TauTagTools
 cvs co HiggsAnalysis/Skimming
 rm HiggsAnalysis/Skimming/python/earlyDataInterestingEvents_cff.py
 #fi
