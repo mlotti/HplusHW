39b4f3355f7b23112305814c988b432f88daeed2
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-11-10 10:32:42 +0200

    Added embeddingAnalysis_cfg.py for analysing the tau embedding results

---
+++ HeavyChHiggsToTauNu/test/tauEmbedding/embeddingAnalysis_cfg.py
@@ -0,0 +1,118 @@
+import FWCore.ParameterSet.Config as cms
+from HiggsAnalysis.HeavyChHiggsToTauNu.HChOptions import getOptions
+from HiggsAnalysis.HeavyChHiggsToTauNu.HChDataVersion import DataVersion
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+#dataVersion = "36X"
+#dataVersion = "36Xspring10"
+#dataVersion = "37X"
+dataVersion = "38X"
+#dataVersion = "data" # this is for collision data 
+
+options = getOptions()
+if options.dataVersion != "":
+    dataVersion = options.dataVersion
+
+print "Assuming data is ", dataVersion
+dataVersion = DataVersion(dataVersion) # convert string to object
+
+process = cms.Process("TauEmbeddingAnalysis")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string(dataVersion.getGlobalTag())
+
+process.source = cms.Source('PoolSource',
+    duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+    fileNames = cms.untracked.vstring(
+        "file:embedded_RECO.root"
+  )
+)
+options.doPat = 1
+################################################################################
+
+process.load("HiggsAnalysis.HeavyChHiggsToTauNu.HChCommon_cfi")
+process.MessageLogger.categories.append("EventCounts")
+process.MessageLogger.cerr.FwkReport.reportEvery = 5000
+
+
+process.TFileService.fileName = "histograms.root"
+
+from HiggsAnalysis.HeavyChHiggsToTauNu.HChDataSelection import addDataSelection, dataSelectionCounters
+from HiggsAnalysis.HeavyChHiggsToTauNu.HChPatTuple import *
+from PhysicsTools.PatAlgos.tools.coreTools import removeSpecificPATObjects, removeCleaning, removeMCMatching
+process.patSequence = cms.Sequence()
+if options.doPat != 0:
+    print "Running PAT on the fly"
+
+    process.out = cms.OutputModule("PoolOutputModule",
+        fileName = cms.untracked.string('dummy.root'),
+        outputCommands = cms.untracked.vstring()
+    )
+
+
+    process.collisionDataSelection = cms.Sequence()
+    if dataVersion.isData():
+        process.collisionDataSelection = addDataSelection(process, dataVersion, trigger)
+
+    process.patPlainSequence = addPat(process, dataVersion, doPatTrigger=False, doTauHLTMatching=False,
+                                      doPatCalo=False, doPatMET=False, doPatElectronID=False)
+    process.patSequence = cms.Sequence(
+        process.collisionDataSelection *
+        process.patPlainSequence
+    )
+    removeSpecificPATObjects(process, ["Muons", "Electrons", "Photons"], False)
+    #removeSpecificPATObjects(process, ["Photons"], False)
+    removeCleaning(process, False)
+
+    del process.out
+
+process.configInfo = cms.EDAnalyzer("HPlusConfigInfoAnalyzer")
+if options.crossSection >= 0.:
+    process.configInfo.crossSection = cms.untracked.double(options.crossSection)
+    print "Dataset cross section has been set to %g pb" % options.crossSection
+if options.luminosity >= 0:
+    process.configInfo.luminosity = cms.untracked.double(options.luminosity)
+    print "Dataset integrated luminosity has been set to %g pb^-1" % options.luminosity
+
+process.commonSequence = cms.Sequence(
+    process.patSequence +
+    process.configInfo
+)
+
+################################################################################
+
+from HiggsAnalysis.HeavyChHiggsToTauNu.HChTools import *
+histoMuonPt = Histo("pt", "pt()", min=0., max=200., nbins=200, description="muon pt (GeV/c)")
+histoMuonEta = Histo("eta", "eta()", min=-3, max=3, nbins=60, description="muon eta")
+
+histoTauPt = Histo("pt", "pt()", min=0., max=200., nbins=200, description="tau pt (GeV/c)")
+histoTauEta = Histo("eta", "eta()", min=-3, max=3, nbins=60, description="tau eta")
+
+histoMet = Histo("et", "et()", min=0., max=300., nbins=300, description="MET (GeV)")
+
+
+muons = cms.InputTag("tightMuons")
+taus = cms.InputTag("selectedPatTaus")
+pfMET = cms.InputTag("pfMet")
+pfMETOriginal = cms.InputTag("pfMet", "", "RECO")
+
+
+counters = []
+if dataVersion.isData():
+    counters = dataSelectionCounters
+analysis = Analysis(process, "analysis", options, additionalCounters=counters)
+analysis.getCountAnalyzer().verbose = cms.untracked.bool(True)
+
+histoAnalyzer = analysis.addMultiHistoAnalyzer("All", [
+        ("muon_", muons, [histoMuonPt, histoMuonEta]),
+        ("tau_", taus, [histoTauPt, histoTauEta]),
+        ("pfmet_", pfMET, [histoMet]),
+        ("pfmetOriginal_", pfMETOriginal, [histoMet])])
+
+#process.analysisSequence = 
+process.analysisPath = cms.Path(
+    process.commonSequence *
+    analysis.getSequence()
+)
