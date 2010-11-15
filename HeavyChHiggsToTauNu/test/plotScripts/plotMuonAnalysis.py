a4c50875f0858566e94f12e52e27a25a441afe6d
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-11-15 13:52:38 +0100

    Updated plotMuonAnalysis.py

--- HeavyChHiggsToTauNu/test/plotScripts/plotMuonAnalysis.py
+++ HeavyChHiggsToTauNu/test/plotScripts/plotMuonAnalysis.py
@@ -9,43 +9,52 @@ import HiggsAnalysis.HeavyChHiggsToTauNu.tools.styles as styles
 
 legendLabels = {
     "Data":                  "Data",
-    "TTbar":                 "t#bar{t}",
-    "TTbarJets":             "t#bar{t}+jets",
+#    "TTbar":                 "t#bar{t}",
+#    "TTbarJets":             "t#bar{t}+jets",
+    "TTJets":                "t#bar{t}+jets",
     "WJets":                 "W+jets",
-    "ZJets":                 "Z+jets",
-    "QCD_Pt15to30_Fall10":   "QCD, 15 < #hat{p}_T < 30",
-    "QCD_Pt30to50_Fall10":   "QCD, 30 < #hat{p}_T < 50",
-    "QCD_Pt50to80_Fall10":   "QCD, 50 < #hat{p}_T < 80",
-    "QCD_Pt80to120_Fall10":  "QCD, 80 < #hat{p}_T < 120",
-    "QCD_Pt120to170_Fall10": "QCD, 120 < #hat{p}_T < 170",
-    "QCD_Pt170to300_Fall10": "QCD, 170 < #hat{p}_T < 300"}
+#    "ZJets":                 "Z+jets",
+    "DYJetsToLL":            "DY+jets",
+    "QCD_Pt15to30_Fall10":   "QCD, 15 < #hat{p}_{T} < 30",
+    "QCD_Pt30to50_Fall10":   "QCD, 30 < #hat{p}_{T} < 50",
+    "QCD_Pt50to80_Fall10":   "QCD, 50 < #hat{p}_{T} < 80",
+    "QCD_Pt80to120_Fall10":  "QCD, 80 < #hat{p}_{T} < 120",
+    "QCD_Pt120to170_Fall10": "QCD, 120 < #hat{p}_{T} < 170",
+    "QCD_Pt170to300_Fall10": "QCD, 170 < #hat{p}_{T} < 300",
+    "QCD_Pt20_MuEnriched": "QCD (#mu enrich.), #hat{p}_{T} > 20"
+}
 
 ROOT.gROOT.SetBatch(True)
 
 QCDdetails = False
 #QCDdetails = True
 
-#lastSelection = "h11_JetSelection"
-#lastMultip = "h11_Multiplicity"
+QCDmuEnriched = False
+#QCDmuEnriched = True
 
-lastSelection = "h12_METCut"
-lastMultip = "h12_Multiplicity"
+
+lastSelection = "h11_JetSelection"
+lastMultip = "h11_Multiplicity"
+lastSelectionOther = "afterOtherCutsAfterJetMultiplicityCut"
+
+#lastSelection = "h12_METCut"
+#lastMultip = "h12_Multiplicity"
+#lastSelectionOther = "afterOtherCutsAfterMETCut"
 
 
 
 style = TDRStyle()
 datasets = getDatasetsFromMulticrabCfg(counters="countAnalyzer")
 
-datasetsQCD = None
-if QCDdetails:
-    datasetsQCD = getDatasetsFromMulticrabCfg(counters="countAnalyzer")
-    datasetsQCD.selectAndReorder(["QCD_Pt15to30_Fall10",
-                                  "QCD_Pt30to50_Fall10",
-                                  "QCD_Pt50to80_Fall10",
-                                  "QCD_Pt80to120_Fall10",
-                                  "QCD_Pt120to170_Fall10",
-                                  "QCD_Pt170to300_Fall10"
-                                  ])
+datasetsQCD = datasets.shallowCopy()
+datasetsQCD.selectAndReorder([
+        #"QCD_Pt15to30_Fall10",
+        "QCD_Pt30to50_Fall10",
+        "QCD_Pt50to80_Fall10",
+        "QCD_Pt80to120_Fall10",
+        "QCD_Pt120to170_Fall10",
+        "QCD_Pt170to300_Fall10"
+])
 
 #datasets.remove(["SingleTop_sChannel", "SingleTop_tChannel", "SingleTop_tWChannel"])
 # datasets = getDatasetsFromCrabDirs(["Mu_140042-144114",
@@ -53,17 +62,36 @@ if QCDdetails:
 #                                     "QCD_Pt120to170_Fall10", "QCD_Pt170to300_Fall10",
 #                                     "QCD_Pt30to50_Fall10", "QCD_Pt50to80_Fall10",
 #                                     "QCD_Pt80to120_Fall10"], counters="countAnalyzer")
-#datasets.getDataset("Mu_140042-144114").setLuminosity(2126184.794/1e6) # ub^-1 -> pb^-1
-#datasets.getDataset("Mu_146240-147116").setLuminosity(4390660.197/1e6)
-#datasets.getDataset("Mu_147196-148058").setLuminosity(7618294.554/1e6)
-datasets.getDataset("Mu_135821-144114").setLuminosity(1763049.941/1e6) # ub^-1 -> pb^-1
+datasets.getDataset("Mu_135821-144114").setLuminosity(3051760.115/1e6) # ub^-1 -> pb^-1
 datasets.getDataset("Mu_146240-147116").setLuminosity(4390660.197/1e6)
-datasets.getDataset("Mu_147196-148058").setLuminosity(7618294.554/1e6)
+datasets.getDataset("Mu_147196-149442").setLuminosity(27384630.974/1e6)
 #datasets.remove(["Mu_146240-147116", "Mu_147196-148058"])
 datasets.mergeData()
+
+datasetsMC = datasets.deepCopy()
+datasetsMC.remove(["Data"])
+
 datasets.merge("QCD", ["QCD_Pt15to30_Fall10", "QCD_Pt30to50_Fall10", "QCD_Pt50to80_Fall10",
                        "QCD_Pt80to120_Fall10", "QCD_Pt120to170_Fall10", "QCD_Pt170to300_Fall10"])
-datasets.merge("Single t", ["SingleTop_sChannel", "SingleTop_tChannel", "SingleTop_tWChannel"])
+
+if QCDmuEnriched:
+    datasets.remove(["QCD"])
+    datasets.getDataset("QCD_Pt20_MuEnriched").setCrossSection(296600000.*0.0002855)
+    datasets.rename("QCD_Pt20_MuEnriched", "QCD")
+else:
+    datasets.remove(["QCD_Pt20_MuEnriched"])
+
+#datasets.merge("Single t", ["SingleTop_sChannel", "SingleTop_tChannel", "SingleTop_tWChannel"])
+datasets.merge("Single t", ["TToBLNu_s-channel", "TToBLNu_t-channel", "TToBLNu_tW-channel"])
+
+datasets.selectAndReorder([
+        "Data",
+        "WJets",
+        "TTJets",
+        "Single t",
+        "DYJetsToLL",
+        "QCD"
+])
 
 class Histo:
     def __init__(self, datasets, name, lumi=None):
@@ -107,7 +135,7 @@ class Histo:
 # After muon selection (h10_ElectronVeto)
 h = Histo(datasets, "h10_Multiplicity/jets_multiplicity")
 h.histos.stackMCHistograms()
-h.createFrame("njets", ymin=0.1, ymax=1e6)
+h.createFrame("njets_after10electronveto", ymin=0.1, ymax=1e6)
 h.frame.GetXaxis().SetTitle("Jet multiplicity")
 h.frame.GetYaxis().SetTitle("Number of events")
 h.addLegend(createLegend(0.7, 0.5, 0.9, 0.8))
@@ -121,7 +149,7 @@ h.save()
 # After muon selection + jet multip. cut (h11_JetSelection)
 h = Histo(datasets, lastMultip+"/jets_multiplicity")
 h.histos.stackMCHistograms()
-h.createFrame("njets_afternjetcut", xmin=3)
+h.createFrame("njets_after11njetcut", xmin=3)
 h.frame.GetXaxis().SetTitle("Jet multiplicity")
 h.frame.GetYaxis().SetTitle("Number of events")
 h.addLegend(createLegend(0.7, 0.5, 0.9, 0.8))
@@ -161,7 +189,6 @@ def muonPt(h, prefix=""):
     h.histos.addLuminosityText()
     h.save()
 
-    # h = Histo(datasets, "afterOtherCuts/pt")
     h.createFrame(prefix+"muon_pt_cut20", xmin=20, ymax=200)
     h.frame.GetXaxis().SetTitle(xlabel)
     h.frame.GetYaxis().SetTitle(ylabel)
@@ -171,9 +198,9 @@ def muonPt(h, prefix=""):
     h.histos.addLuminosityText()
     h.save()
 
-muonPt(Histo(datasets, "afterOtherCuts/pt"))
+muonPt(Histo(datasets, lastSelectionOther+"/pt"))
 if QCDdetails:
-    muonPt(Histo(datasetsQCD, "afterOtherCuts/pt", datasets.getDataset("Data").getLuminosity()), "qcd_")
+    muonPt(Histo(datasetsQCD, lastSelectionOther+"/pt", datasets.getDataset("Data").getLuminosity()), "qcd_")
 
 # Muon pt after all other cuts
 h = Histo(datasets, lastSelection+"/muon_pt")
@@ -292,24 +319,31 @@ eventCounter = EventCounter(datasets)
 eventCounter.normalizeMCByLuminosity()
 
 print "============================================================"
-print "Main counter (MC normalized by collision data luminosity)"
-eventCounter.getMainCounter().printCounter(FloatDecimalFormat(1))
+print "Main counter (%s)" % eventCounter.getNormalizationString()
+printCounter(eventCounter.getMainCounterData(), FloatDecimalFormat(1))
+print "------------------------------------------------------------"
+printCounterEfficiency(eventCounter.getMainCounterData(), FloatDecimalFormat(4))
 
+
+eventCounter = EventCounter(datasetsMC)
+print "============================================================"
+print "Main counter (%s)" % eventCounter.getNormalizationString()
+printCounter(eventCounter.getMainCounterData(), FloatDecimalFormat(0))
 
 if QCDdetails:
     print "============================================================"
     print "QCD dataset info: "
     datasetsQCD.printInfo()
 
+
     eventCounter = EventCounter(datasetsQCD)
     eventCounter.normalizeMCToLuminosity(datasets.getDataset("Data").getLuminosity())
-
     print "============================================================"
-    print "Main counter (MC normalized by collision data luminosity)"
+    print "Main counter (%s)" % eventCounter.getNormalizationString()
     eventCounter.getMainCounter().printCounter(FloatDecimalFormat(1))
 
-    eventCounter = EventCounter(datasetsQCD)
 
+    eventCounter = EventCounter(datasetsQCD)
     print "============================================================"
-    print "Main counter (MC events)"
+    print "Main counter (%s)" % eventCounter.getNormalizationString()
     eventCounter.getMainCounter().printCounter(FloatDecimalFormat(0))
