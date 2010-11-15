e3efddf693d402475eb064b34e92858c3dc102c3
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-11-15 10:01:30 +0200

    Readded tau embedding datasets

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasets.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasets.py
@@ -1,9 +1,12 @@
 import multicrabDatasetsCollisionData as collisionData
 import multicrabDatasetsMCSummer10 as mcSummer10
 import multicrabDatasetsMCFall10 as mcFall10
+import multicrabDatasetsTauEmbedding as tauEmbedding
 
 datasets = {}
 
 datasets.update(collisionData.datasets)
 datasets.update(mcSummer10.datasets)
 datasets.update(mcFall10.datasets)
+
+tauEmbedding.addTo(datasets)
