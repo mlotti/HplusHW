e080c31082e505e4fad01d8514fe8281c335a76f
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-11-22 15:53:52 +0200

    File rename

--- HeavyChHiggsToTauNu/plugins/HPlusPtrVertexZSelectors.cc
+++ HeavyChHiggsToTauNu/plugins/HPlusPtrVertexZSelectors.cc
@@ -1,7 +1,10 @@
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/ViewPtrVertexZSelector.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
 
 typedef ViewPtrVertexZSelector<reco::Candidate> HPlusCandViewPtrVertexZSelector;
+typedef ViewPtrVertexZSelector<pat::Muon> HPlusPATMuonViewPtrVertexZSelector;
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(HPlusCandViewPtrVertexZSelector);
+DEFINE_FWK_MODULE(HPlusPATMuonViewPtrVertexZSelector);
