66936e913aa63dc263920c5926fed7eb1813338d
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-12-09 15:27:39 +0100

    Updated the pattuple version to v7_test1

--- HeavyChHiggsToTauNu/test/pattuple/multicrabPatTuple.py
+++ HeavyChHiggsToTauNu/test/pattuple/multicrabPatTuple.py
@@ -69,7 +69,7 @@ def addOutputName(dataset):
     path = dataset.getDatasetPath().split("/")
     name = path[2].replace("-", "_")
     name += "_"+path[3]
-    name += "_pattuple_v6_4"
+    name += "_pattuple_v7_test1"
 
     dataset.addLine("USER.publish_data_name = "+name)
 multicrab.forEachDataset(addOutputName)
