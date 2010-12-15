e1df7bb23d4c4161d20515f5c2a795b4bd833756
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-12-15 08:21:43 +0100

    Added the v6_4 Jet PD pattuples

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData.py
@@ -200,6 +200,15 @@ datasets = {
                 "number_of_jobs": 100, # Adjusted for PATtuple
                 "lumiMaskRequired": True
             },
+            "pattuple_v6_4": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/JetMETTau/local-Run2010A_Nov4ReReco_v1_RECO_pattuple_v6_4-4c46839dc2dbfe33eac25fb0510aaca6/USER",
+                "luminosity": .284734728,
+                "number_of_jobs": 10 # 1.7M events
+            },
+            "pattuple_v6": {
+                "fallback": "pattuple_v6_4"
+            }
         }
     },
     "JetMET_141950-144114": {
@@ -212,6 +221,15 @@ datasets = {
                 "number_of_jobs": 300, # Adjusted for PATtuple
                 "lumiMaskRequired": True
             },
+            "pattuple_v6_4": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/JetMET/local-Run2010A_Nov4ReReco_v1_RECO_pattuple_v6_4-4c46839dc2dbfe33eac25fb0510aaca6/USER",
+                "luminosity": 2.895797321,
+                "number_of_jobs": 20 # 2.9M events
+            },
+            "pattuple_v6": {
+                "fallback": "pattuple_v6_4"
+            }
         }
     },
     "Jet_146240-148058": { # last run really 149442, but last run with HLT_Jet30U is this one
@@ -225,6 +243,15 @@ datasets = {
                 "number_of_jobs": 40, # Adjusted for PATtuple
                 "lumiMaskRequired": True
             },
+            "pattuple_v6_4": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/Jet/local-Run2010B_Nov4ReReco_v1_RECO_pattuple_v6_4-4c46839dc2dbfe33eac25fb0510aaca6/USER",
+                "luminosity": 14.527814041,
+                "number_of_jobs": 4
+            },
+            "pattuple_v6": {
+                "fallback": "pattuple_v6_4"
+            }
         }
     },
 
