63228d48fc197be60d1b09eac415c5059f58daf1
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2010-12-27 16:41:37 +0200

    Added option to print HLT paths to hltTableAnalyzer_cfg

--- HeavyChHiggsToTauNu/test/hltTableAnalyzer_cfg.py
+++ HeavyChHiggsToTauNu/test/hltTableAnalyzer_cfg.py
@@ -15,6 +15,11 @@ options.register("hltProcess",
                  options.multiplicity.singleton,
                  options.varType.string,
                  "HLT Process name")
+options.register("printPaths",
+                 0,
+                 options.multiplicity.singleton,
+                 options.varType.int,
+                 "Print HLT paths")
 options = getOptions(options)
 
 hltProcess = "HLT"
@@ -26,32 +31,46 @@ if options.hltProcess != "":
 print "Using HLT process name", hltProcess
 
 process = cms.Process("HLTTABLEANALYZER")
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+maxEv = -1
+if options.printPaths != 0:
+    maxEv = 10
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(maxEv) )
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring("ProductNotFound")
 )
 
 process.source = cms.Source('PoolSource',
     duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
-    processingMode = cms.untracked.string("Runs"),
     fileNames = cms.untracked.vstring(options.inputFiles)
 )
+if options.printPaths == 0:
+    process.source.processingMode = cms.untracked.string("Runs")
 
 #process.load("FWCore.MessageService.MessageLogger_cfi")
 #process.MessageLogger.categories = cms.untracked.vstring("HLTTableInfo")
 #del process.MessageLogger.statistics
-process.MessageLogger = cms.Service(
-    "MessageLogger",
+process.MessageLogger = cms.Service("MessageLogger",
     categories = cms.untracked.vstring("HLTTableInfo"),
     destinations = cms.untracked.vstring("cout"),
     cout = cms.untracked.PSet(
-        threshold = cms.untracked.string("INFO"),
+        threshold = cms.untracked.string("DEBUG"),
         default = cms.untracked.PSet(limit = cms.untracked.int32(0)),
-        HLTTableInfo = cms.untracked.PSet(limit = cms.untracked.int32(100))
+        HLTTableInfo = cms.untracked.PSet(limit = cms.untracked.int32(100)),
     )
 )
 
 process.load("HiggsAnalysis.HeavyChHiggsToTauNu.HPlusHLTTableAnalyzer_cfi")
 process.hltTableAnalyzer.hltProcessName = hltProcess
+
 process.path = cms.Path(process.hltTableAnalyzer)
 
+if options.printPaths != 0:
+    process.load("HLTrigger.HLTanalyzers.hlTrigReport_cfi")
+    process.hlTrigReport.HLTriggerResults.setProcessName(hltProcess)
+    process.path *= process.hlTrigReport
+
+    process.MessageLogger.categories.append("HLTrigReport")
+    process.MessageLogger.cout.HLTrigReport = cms.untracked.PSet(limit = cms.untracked.int32(1000000))
+
+
+print process.dumpPython()
