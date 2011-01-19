15bb75c96a77787f4cd4707a1a91c1ff1b6f56ba
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-01-19 09:02:55 +0100

    More tauembedding v5 skims ready

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsTauEmbedding.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsTauEmbedding.py
@@ -34,25 +34,57 @@ def addTo(datasets):
     }
 
 
+#    datasets[""]["data"]["tauembedding_skim_v5"] = {
+#        "dbs_url": common.pattuple_dbs,
+#        "datasetpath": "",
+#        "number_of_jobs":  # ~100 ev/job
+#    }
+
     datasets["Mu_135821-144114"]["data"]["tauembedding_skim_v5"] = {
         "dbs_url": common.pattuple_dbs,
-        "datasetpath": "/Mu/local-Run2010A_Nov4ReReco_v1_RECO_tauembedding_skim_v5-d9acfcd152bd91559f102d8a462838a1/USER",
-        "number_of_jobs": 7 # ~100 ev/job
+        "datasetpath": "/Mu/local-Run2010A_Nov4ReReco_v1_RECO_135821_tauembedding_skim_v5-d9acfcd152bd91559f102d8a462838a1/USER",
+        "number_of_jobs": 10 # ~100 ev/job
     }
-    datasets["Mu_146240-147116"]["data"]["tauembedding_skim_v5"] = {
+
+
+    datasets["TTJets_PU"]["data"]["tauembedding_skim_v5"] = {
         "dbs_url": common.pattuple_dbs,
-        "datasetpath": "/Mu/local-Run2010B_Nov4ReReco_v1_RECO_tauembedding_skim_v5-d9acfcd152bd91559f102d8a462838a1/USER",
-        "number_of_jobs": 12 # ~100 ev/job
+        "datasetpath": "/TTJets_TuneZ2_7TeV-madgraph-tauola/local-Fall10_E7TeV_ProbDist_2010Data_BX156_START38_V12_v1_GEN-SIM-RECO_tauembedding_skim_v5-f0f5761dbef0a56e664c9bfa3bb2c570/USER",
+        "number_of_jobs": 300 # ~500 ev/job
     }
-    datasets["Mu_147196-149442"]["data"]["tauembedding_skim_v5"] = {
+
+    datasets["WJets_Fall10_PU"]["data"]["tauembedding_skim_v5"] = {
         "dbs_url": common.pattuple_dbs,
-        "datasetpath": "/Mu/local-Run2010B_Nov4ReReco_v1_RECO_tauembedding_skim_v5-1b92bf46a5f97420ad6d4abe5c819f95/USER",
-        "number_of_jobs":  # ~100 ev/job
+        "datasetpath": "/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/local-Fall10_E7TeV_ProbDist_2010Data_BX156_START38_V12_v1_AODSIM_tauembedding_skim_v5-f0f5761dbef0a56e664c9bfa3bb2c570/USER",
+        "number_of_jobs": 50 # ~200 ev/job
+    }
+
+    datasets["TToBLNu_s-channel_PU"]["data"]["tauembedding_skim_v5"] = {
+        "dbs_url": common.pattuple_dbs,
+        "datasetpath": "/TToBLNu_TuneZ2_s-channel_7TeV-madgraph/local-Fall10_E7TeV_ProbDist_2010Data_BX156_START38_V12_v1_GEN-SIM-RECO_tauembedding_skim_v5b-f0f5761dbef0a56e664c9bfa3bb2c570/USER",
+        "number_of_jobs": 56 # ~100 ev/job
+    }
+
+    datasets["TToBLNu_t-channel_PU"]["data"]["tauembedding_skim_v5"] = {
+        "dbs_url": common.pattuple_dbs,
+        "datasetpath": "/TToBLNu_TuneZ2_t-channel_7TeV-madgraph/local-Fall10_E7TeV_ProbDist_2010Data_BX156_START38_V12_v1_GEN-SIM-RECO_tauembedding_skim_v5b-f0f5761dbef0a56e664c9bfa3bb2c570/USER",
+        "number_of_jobs": 95 # ~100 ev/job
+    }
+
+    datasets["TToBLNu_tW-channel_PU"]["data"]["tauembedding_skim_v5"] = {
+        "dbs_url": common.pattuple_dbs,
+        "datasetpath": "/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/local-Fall10_E7TeV_ProbDist_2010Data_BX156_START38_V12_v1_GEN-SIM-RECO_tauembedding_skim_v5b-f0f5761dbef0a56e664c9bfa3bb2c570/USER",
+        "number_of_jobs": 68 # ~500 ev/job
+    }
+
+    datasets["QCD_Pt20_MuEnriched_PU"]["data"]["tauembedding_skim_v5"] = {
+        "dbs_url": common.pattuple_dbs,
+        "datasetpath": "/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/local-Fall10_E7TeV_ProbDist_2010Data_BX156_START38_V12_v1_GEN-SIM-RECO_tauembedding_skim_v5-f0f5761dbef0a56e664c9bfa3bb2c570/USER",
+        "number_of_jobs": 140 # ~400 ev/job
     }
 
     datasets["DYJetsToLL_PU"]["data"]["tauembedding_skim_v5"] = {
         "dbs_url": common.pattuple_dbs,
         "datasetpath": "/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/local-Fall10_E7TeV_ProbDist_2010Data_BX156_START38_V12_v1_GEN-SIM-RECO_tauembedding_skim_v5-f0f5761dbef0a56e664c9bfa3bb2c570/USER",
-        "number_of_jobs": 60 # ~100 ev/job
+        "number_of_jobs": 30 # ~200 ev/job
     }
-
