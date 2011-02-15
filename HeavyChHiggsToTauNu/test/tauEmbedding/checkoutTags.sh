8d763b99c119561dc015b850df55455e8d1ee8b0
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-02-15 09:44:44 +0200

    Tau embedding checkoutTags.sh exits in first error

--- HeavyChHiggsToTauNu/test/tauEmbedding/checkoutTags.sh
+++ HeavyChHiggsToTauNu/test/tauEmbedding/checkoutTags.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 # Tag list modification history
 # 10.11.2010/M.Kortelainen CMSSW_3_8_6
 # 11.11.2010/M.Kortelainen the tag is needed to get the code to compile, moved one level up
