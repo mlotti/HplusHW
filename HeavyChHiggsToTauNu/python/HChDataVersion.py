38af3e6149b2ec4125be9cb5fae189ffcc8bad14
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-02-24 15:05:18 +0200

    Updated global tags in HChDataVersion

--- HeavyChHiggsToTauNu/python/HChDataVersion.py
+++ HeavyChHiggsToTauNu/python/HChDataVersion.py
@@ -92,12 +92,12 @@ class DataVersion:
         # Collision data
         if dataVersion in ["36Xdata", "38Xdata", "39Xdata"]:
             self.is_data = True
-            self.globalTag = "GR_R_39X_V5::All"
+            self.globalTag = "GR_R_311_V1::All"
 
         # MC
         else:
             self.is_data = False
-            self.globalTag = "START39_V8::All"
+            self.globalTag = "START311_V1::All"
 
             try:
                 self.signalTrigger = conf["signalTrigger"]
