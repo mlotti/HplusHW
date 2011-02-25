b3112debee2c419ba7fda1d48f7010de0da84a80
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-02-25 16:41:23 +0100

    Changed DY+jets plot style

--- HeavyChHiggsToTauNu/python/tools/styles.py
+++ HeavyChHiggsToTauNu/python/tools/styles.py
@@ -66,7 +66,8 @@ qcdStyle = Style(7, ROOT.kYellow+1)
 wStyle = Style(1, ROOT.kGreen+2)
 #ttStyle = Style(5, ROOT.kRed+1)
 ttStyle = Style(4, ROOT.kBlue)
-dyStyle = Style(12, ROOT.kBlue-3)
+dyStyle = Style(5, ROOT.kRed+1)
+#dyStyle = Style(12, ROOT.kBlue-3)
 stStyle = Style(2, ROOT.kMagenta)
 
 styles = [
