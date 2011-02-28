9111c9ebd1494b75e8c8391752eaad3b60037d50
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-02-28 08:30:46 +0200

    Added first Spring11 dataset

---
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsMCSpring11.py
@@ -0,0 +1,15 @@
+import multicrabDatasetsCommon as common
+
+datasets = {
+    # Testing
+    "WToTauNu_TuneZ2_Spring11": {
+        "dataVersion": "311Xredigi",
+        "crossSection": 7899,
+        "data": {
+            "AOD": {
+                "datasetpath": "/WToTauNu_TuneZ2_7TeV-pythia6-tauola/Spring11-E7TeV_FlatDist10_2011EarlyData_50ns_START311_V1G1-v1/AODSIM",
+                "number_of_jobs": 20,
+            },
+        }
+    },
+}
