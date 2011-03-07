a73fc1d214070ed73c084663c146521f1d78ea6b
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-03-07 09:34:11 +0200

    Adjusted doc generation output

--- HeavyChHiggsToTauNu/doc/createref.sh
+++ HeavyChHiggsToTauNu/doc/createref.sh
@@ -12,11 +12,13 @@ echo "Creating doxygen config file"
 sed "s?REPLACEME?$VERSION/$COMMIT?g" doxygen_py.config > doxyconfig_py.active
 sed "s?REPLACEME?$VERSION/$COMMIT?g" doxygen_cpp.config > doxyconfig_cpp.active
 
-echo "Generating reference manual"
 cd ..
+echo "Generating reference manual for C++"
 $DOXYGEN doc/doxyconfig_py.active
+echo "Generating reference manual for Python"
 $DOXYGEN doc/doxyconfig_cpp.active
 
 cd doc
 rm doxyconfig_py.active
 rm doxyconfig_cpp.active
+echo "Generating done"
