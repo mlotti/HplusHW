850ff56d973d461cf4a6cd91a87a4eb58d645336
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-03-10 09:27:38 +0200

    Integrated plotRtauEmbedding_afterTauID.py to plotTauEmbeddingSignalAnalysis.py

--- HeavyChHiggsToTauNu/test/plotScripts/plotTauEmbeddingSignalAnalysis.py
+++ HeavyChHiggsToTauNu/test/plotScripts/plotTauEmbeddingSignalAnalysis.py
@@ -26,29 +26,54 @@ analysis = "signalAnalysis"
 #analysis = "signalAnalysisTauSelectionHPSTightTauBased"
 counters = analysis+"Counters"
 
-# Read the datasets
-datasets = dataset.getDatasetsFromMulticrabCfg(counters=counters)
-datasets.remove(["WJets_TuneD6T_Winter10", "TTJets_TuneD6T_Winter10","TTToHplusBWB_M120_Winter10"])
-datasets.loadLuminosities()
-plots.mergeRenameReorderForDataMC(datasets)
+# main function
+def main():
+    # Read the datasets
+    datasets = dataset.getDatasetsFromMulticrabCfg(counters=counters)
+    datasets.remove(["WJets_TuneD6T_Winter10", "TTJets_TuneD6T_Winter10","TTToHplusBWB_M120_Winter10"])
+    datasets.loadLuminosities()
+    plots.mergeRenameReorderForDataMC(datasets)
 
-# Apply TDR style
-style = tdrstyle.TDRStyle()
+    # Apply TDR style
+    style = tdrstyle.TDRStyle()
 
+    # Create the plot objects and pass them to the formatting
+    # functions to be formatted, drawn and saved to files
+    deltaPhi(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_DeltaPhi"))
+    deltaPhi(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_DeltaPhiOriginal"))
+
+    leadingTrack(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_leadPFChargedHadrPt"))
+
+    met(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_begin_originalMet"), ratio=True)
+    met(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_originalMet"))
+    met(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_embeddingMet"))
+
+    muonPt(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_begin_originalMuonPt"), ratio=True)
+    muonPt(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_originalMuonPt"))
+    muonPt(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterMetCut_originalMuonPt"))
+
+    rtau(plots.DataMCPlot(datasets, analysis+"/TauID_RtauCut"), "TauIdRtau_afterTauId")
+
+
+# Helper function to flip the last two parts of the histogram name
+# e.g. ..._afterTauId_DeltaPhi -> DeltaPhi_afterTauId
 def flipName(name):
     tmp = name.split("_")
-    return tmp[-1] + "_" + tmp[-2] # ..._afterTauId_DeltaPhi -> DeltaPhi_afterTauId
+    return tmp[-1] + "_" + tmp[-2]
 
+# Common formatting
 def common(h, xlabel, ylabel):
     h.frame.GetXaxis().SetTitle(xlabel)
     h.frame.GetYaxis().SetTitle(ylabel)
-    h.setLegend(histograms.createLegend())
     h.draw()
     histograms.addCmsPreliminaryText()
     histograms.addEnergyText()
     h.addLuminosityText()
     h.save()
 
+# Functions below are for plot-specific formattings. They all take the
+# plot object as an argument, then apply some formatting to it, draw
+# it and finally save it to files.
 def deltaPhi(h, rebin=5):
     name = flipName(h.getRootHistoPath())
 
@@ -65,6 +90,7 @@ def deltaPhi(h, rebin=5):
 
     #h.createFrameFraction(name)
     h.createFrame(name)
+    h.setLegend(histograms.createLegend())
     common(h, xlabel, ylabel)
 
 def leadingTrack(h, rebin=5):
@@ -83,7 +109,23 @@ def leadingTrack(h, rebin=5):
     #h.createFrameFraction(name, opts=opts)
     h.createFrame(name, opts=opts)
     h.getPad().SetLogy(True)
+    h.setLegend(histograms.createLegend())
     common(h, xlabel, ylabel)
+
+def rtau(h, name, rebin=5):
+    #h.histoMgr.forEachHisto(lambda h: h.getRootHisto().Rebin(rebin))
+    xlabel = "R_{#tau}"
+    ylabel = "Events / %.2f" % h.binWidth()
+
+    h.stackMCHistograms()
+    h.addMCUncertainty()
+
+    opts = {"ymin": 0.01, "ymaxfactor": 10}
+    h.createFrame(name, opts=opts)
+    #h.createFrameFraction(name, opts=opts)
+    h.getPad().SetLogy(True)
+    #h.setLegend(histograms.createLegend())
+    common(h, xlabel, ylabel)    
 
 def met(h, rebin=5, ratio=False):
     name = flipName(h.getRootHistoPath())
@@ -108,6 +150,7 @@ def met(h, rebin=5, ratio=False):
     else:
         h.createFrame(name, opts=opts)
     h.getPad().SetLogy(True)
+    h.setLegend(histograms.createLegend())
     common(h, xlabel, ylabel)
 
 def muonPt(h, rebin=5, ratio=False):
@@ -129,18 +172,9 @@ def muonPt(h, rebin=5, ratio=False):
     else:
         h.createFrame(name, opts=opts)
     h.getPad().SetLogy(True)
+    h.setLegend(histograms.createLegend())
     common(h, xlabel, ylabel)
 
-
-deltaPhi(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_DeltaPhi"))
-deltaPhi(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_DeltaPhiOriginal"))
-
-leadingTrack(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_leadPFChargedHadrPt"))
-
-met(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_begin_originalMet"), ratio=True)
-met(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_originalMet"))
-met(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_embeddingMet"))
-
-muonPt(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_begin_originalMuonPt"), ratio=True)
-muonPt(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterTauId_originalMuonPt"))
-muonPt(plots.DataMCPlot(datasets, analysis+"/TauEmbeddingAnalysis_afterMetCut_originalMuonPt"))
+# Call the main function if the script is executed (i.e. not imported)
+if __name__ == "__main__":
+    main()
