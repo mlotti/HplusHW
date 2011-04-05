515e3820bfcf47cd39f77a5d337ed7f5f09ce5e8
Lauri Wendland <lauri.wendland@cern.ch>
2011-04-05 17:36:34 +0200

    Added latest P(MET) coefficients to QCD method3

--- HeavyChHiggsToTauNu/test/qcdMeasurementMethod3Part2_cfg.py
+++ HeavyChHiggsToTauNu/test/qcdMeasurementMethod3Part2_cfg.py
@@ -33,16 +33,16 @@ options, dataVersion = getOptionsDataVersion(dataVersion)
 # Define the process
 process = cms.Process("HChQCDMeasurementMethod3Part2")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5000) )
 
 process.source = cms.Source('PoolSource',
     duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
     fileNames = cms.untracked.vstring(
-    "file:test_pattuple_v9_JetMet2010A_86.root"
+    #"file:test_pattuple_v9_JetMet2010A_86.root"
     #"file:/media/disk/attikis/PATTuples/v9_1/test_pattuple_v9_qcd120170.root"
     #"file:/media/disk/attikis/PATTuples/v9_1/test_pattuple_v9_JetMet2010A_86.root"
     #"rfio:/castor/cern.ch/user/w/wendland/test_pattuple_v9_JetMet2010A_86.root"
-    #"rfio:/castor/cern.ch/user/w/wendland/test_pattuplev9_signalM120.root"
+    "rfio:/castor/cern.ch/user/w/wendland/test_pattuplev9_signalM120.root"
     #"rfio:/castor/cern.ch/user/w/wendland/test_pattuple_v9_qcd120170.root"
     #"file:/media/disk/attikis/tmp/pattuple_19_1_3id.root"
     #"rfio:/castor/cern.ch/user/w/wendland/test_pattuplev9_signalM120.root"
@@ -118,6 +118,11 @@ process.qcdMeasurementMethod3Part2 = cms.EDProducer("HPlusQCDMeasurementByMetFac
 import HiggsAnalysis.HeavyChHiggsToTauNu.HChMetTableFactorization_cfi as mettables
 import HiggsAnalysis.HeavyChHiggsToTauNu.METTableFactorization_NoFactorization_cfi as mettableCoeff
 #process.qcdMeasurementMethod3Part2.factorization = cms.untracked.PSet()
+mettableCoeff.METTableFactorizationCoefficients.METTables_Coefficients = cms.untracked.vdouble( *(
+0.0, 0.0, 0.0223602484, 0.0263059, 0.0210332103, 0.016273393, 0.018639329, 0.0176211454, 0.0183615819, 0.0159055926, 0.025789813, 0.0652346858
+) )
+mettableCoeff.METTableFactorizationCoefficients.factorizationSourceName = cms.untracked.string('PMET70_afterJetSelection_fromData_v3')
+
 process.qcdMeasurementMethod3Part2.factorization = mettables.METTableParameters
 process.qcdMeasurementMethod3Part2.factorization.factorizationTables = mettableCoeff.METTableFactorizationCoefficients
         
