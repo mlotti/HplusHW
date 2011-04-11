2c8a3b37f2b6eea91a4e519395f0cce67962f6fb
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-04-11 08:28:50 +0200

    Updated Dec22 JSON file from v2 to v4

--- HeavyChHiggsToTauNu/python/tools/certifiedLumi.py
+++ HeavyChHiggsToTauNu/python/tools/certifiedLumi.py
@@ -1,7 +1,7 @@
 
 files = {
     "Nov4ReReco": "Cert_136033-149442_7TeV_Nov4ReReco_Collisions10_JSON.txt",
-    "Dec22ReReco": "Cert_136033-149442_7TeV_Dec22ReReco_Collisions10_JSON_v2.txt",
+    "Dec22ReReco": "Cert_136033-149442_7TeV_Dec22ReReco_Collisions10_JSON_v4.txt",
     "PromptReco": "Cert_160404-161312_7TeV_PromptReco_Collisions11_JSON.txt",
     "DCSOnly": "json_DCSONLY.txt_160404-161312",
 }
