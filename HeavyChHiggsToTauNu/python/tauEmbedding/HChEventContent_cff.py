75bd97192ae217d01c8a94cc89437fac5bc12d65
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-04-15 08:35:16 +0300

    Keep goodJets in tau embedding event content

--- HeavyChHiggsToTauNu/python/tauEmbedding/HChEventContent_cff.py
+++ HeavyChHiggsToTauNu/python/tauEmbedding/HChEventContent_cff.py
@@ -4,11 +4,12 @@ HChEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring(
         "keep edmMergeableCounter_*_*_*", # in lumi block
         "keep *_selectedPatMuons_*_*",
+        "keep *_tightMuonsZ_*_*",
         "keep *_selectedPatPhotons_*_*",
         "keep *_selectedPatElectrons_*_*",
         "keep *_selectedPatJets_*_*",
+        "keep *_goodJets_*_*",
         "keep *_selectedPatTaus*_*_*",
-        "keep *_tightMuonsZ_*_*",
         "keep *_tauEmbeddingMuons_*_*",
         "keep *_patTriggerEvent_*_*",
         "keep *_patTrigger_*_*",
