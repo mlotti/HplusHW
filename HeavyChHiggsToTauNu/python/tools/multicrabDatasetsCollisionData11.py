bb0aaca54aaff4bc68ba67956f90d1e255888979
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-04-30 10:16:41 +0200

    Updated the JSON file

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData11.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData11.py
@@ -84,6 +84,32 @@ datasets = {
             },
         },
     },
+    "Tau_162803-163261_Prompt": {
+        "dataVersion": "41Xdata",
+        "trigger": "HLT_IsoPFTau35_Trk20_MET45_v2",
+        "runs": (162803, 163261), # This is prompt RECO, so check the run range again when running!
+        "data": {
+            "AOD": {
+                "datasetpath": "/Tau/Run2011A-PromptReco-v2/AOD",
+                "luminosity": 0,
+                "number_of_jobs": 80, # Adjusted for PATtuple file size
+                "lumiMask": "PromptReco"
+            },
+        },
+    },
+    "Tau_163270-163369_Prompt": {
+        "dataVersion": "41Xdata",
+        "trigger": "HLT_IsoPFTau35_Trk20_MET45_v4",
+        "runs": (163270, 163369), # This is prompt RECO, so check the run range again when running!
+        "data": {
+            "AOD": {
+                "datasetpath": "/Tau/Run2011A-PromptReco-v2/AOD",
+                "luminosity": 0,
+                "number_of_jobs": 80, # Adjusted for PATtuple file size
+                "lumiMask": "PromptReco"
+            },
+        },
+    },
 
     # Tau + jets
     "TauPlusX_160404-161312_Prompt": {
@@ -152,6 +178,32 @@ datasets = {
             },
             "pattuple_v10": {
                 "fallback": "pattuple_v10_2"
+            },
+        }
+    },
+    "TauPlusX_162803-163261_Prompt": {
+        "dataVersion": "41Xdata",
+        "trigger": "HLT_QuadJet40_IsoPFTau40_v1",
+        "runs": (162803, 163261), # This is prompt RECO, so check the run range again when running!
+        "data": {
+            "AOD": {
+                "datasetpath": "/TauPlusX/Run2011A-PromptReco-v2/AOD",
+                "luminosity": 0,
+                "number_of_jobs": 5, # Adjusted for PATtuple file size
+                "lumiMask": "PromptReco"
+            },
+        }
+    },
+    "TauPlusX_163270-163369_Prompt": {
+        "dataVersion": "41Xdata",
+        "trigger": "HLT_QuadJet40_IsoPFTau40_v3",
+        "runs": (163270, 163369), # This is prompt RECO, so check the run range again when running!
+        "data": {
+            "AOD": {
+                "datasetpath": "/TauPlusX/Run2011A-PromptReco-v2/AOD",
+                "luminosity": 0,
+                "number_of_jobs": 10, # Adjusted for PATtuple file size
+                "lumiMask": "PromptReco"
             },
         }
     },
