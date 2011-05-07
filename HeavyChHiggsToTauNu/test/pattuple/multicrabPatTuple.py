a5380b5a438a680e6b4be5f4ab2fc937d8feb9a5
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-05-07 09:10:27 +0300

    Bumped pattuple version to v11b

--- HeavyChHiggsToTauNu/test/pattuple/multicrabPatTuple.py
+++ HeavyChHiggsToTauNu/test/pattuple/multicrabPatTuple.py
@@ -192,7 +192,7 @@ def addOutputName(dataset):
     path = dataset.getDatasetPath().split("/")
     name = path[2].replace("-", "_")
     name += "_"+path[3]
-    name += "_pattuple_v11"
+    name += "_pattuple_v11b"
 
     # Add the begin run in the dataset name to the publish name in
     # order to distinguish pattuple datasets from the same PD
