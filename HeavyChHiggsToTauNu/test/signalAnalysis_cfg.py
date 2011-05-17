d433c7d8e2656b851b29693f0449de7c65d36080
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-05-17 20:30:30 +0300

    Added TauLike muon isolation as part of the signal analysis of embedded sample

--- HeavyChHiggsToTauNu/test/signalAnalysis_cfg.py
+++ HeavyChHiggsToTauNu/test/signalAnalysis_cfg.py
@@ -130,6 +130,7 @@ param.setAllTauSelectionSrcSelectedPatTaus()
 param.setTriggerVertexFor2011()
 
 if options.tauEmbeddingInput != 0:
+    tauEmbeddingCustomisations.addMuonIsolationEmbeddingForSignalAnalysis(process, process.commonSequence)
     tauEmbeddingCustomisations.customiseParamForTauEmbedding(param, dataVersion)
     if tauEmbeddingFinalizeMuonSelection:
         applyIsolation = not doTauEmbeddingMuonSelectionScan
