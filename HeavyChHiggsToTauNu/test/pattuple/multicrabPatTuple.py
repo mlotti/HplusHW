b54d3b26b0a8445e5a267d904870f38081c88839
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-05-24 07:38:32 +0200

    Updated crab configurations

--- HeavyChHiggsToTauNu/test/pattuple/multicrabPatTuple.py
+++ HeavyChHiggsToTauNu/test/pattuple/multicrabPatTuple.py
@@ -9,175 +9,33 @@ multicrab = Multicrab("crab_pat.cfg", lumiMaskDir="..")
 
 multicrab.extendDatasets(
     "AOD",
-#    "RECO",
     [
 ########
 #
-# 41X
+# 42X
 #
 ########
-    # Data
-# With DCSOnly json
-#    "Tau_160404-161176_Prompt", # HLT_IsoPFTau35_Trk20_MET45_v1
-#    "Tau_161216-161312_Prompt", # HLT_IsoPFTau35_Trk20_MET45_v2
-#    "TauPlusX_160404-161312_Prompt" # HLT_QuadJet40_IsoPFTau40_v1
-# With certified json
-#        "Tau_160431-161016_Prompt", # HLT_IsoPFTau35_Trk20_MET45_v1
-#        "Tau_162803-163261_Prompt", # HLT_IsoPFTau35_Trk20_MET45_v2
-#        "Tau_163270-163757_Prompt", # HLT_IsoPFTau35_Trk20_MET45_v4
-#        "Tau_163758-163869_Prompt", # HLT_IsoPFTau35_Trk20_MET45_v4
-#        "Tau_163758-163869_Prompt", # HLT_IsoPFTau35_Trk20_MET45_v4 (merge of the two above ones)
+        # Data 2010 (Apr21 ReReco)
+#        "JetMETTau_Tau_136035-139975_Apr21", # HLT_SingleLooseIsoTau20
+#        "JetMETTau_Tau_140058-141881_Apr21", # HLT_SingleLooseIsoTau20_Trk5
+#        "BTau_141956-144114_Apr21",          # HLT_SingleIsoTau20_Trk5
+#        "BTau_146428-148058_Apr21",          # HLT_SingleIsoTau20_Trk15_MET20
+#        "BTau_148822-149182_Apr21",          # HLT_SingleIsoTau20_Trk15_MET25_v3
+#        "BTau_149291-149294_Apr21",          # HLT_SingleIsoTau20_Trk15_MET25_v4
+        # Data 2011 (May10 ReReco)
+        # Data 2011 (PromptReco)
 
-#        "TauPlusX_160431-161016_Prompt", # HLT_QuadJet40_IsoPFTau40_v1
-#        "TauPlusX_162803-163261_Prompt", # HLT_QuadJet40_IsoPFTau40_v1
-#        "TauPlusX_163270-163869_Prompt", # HLT_QuadJet40_IsoPFTau40_v3
-
-########
-#
-# 311X
-#
-########
         # Signal MC (WH)
-#        "TTToHplusBWB_M80_Spring11",
-#        "TTToHplusBWB_M90_Spring11",
-#        "TTToHplusBWB_M100_Spring11",
-#        "TTToHplusBWB_M120_Spring11",
-#        "TTToHplusBWB_M140_Spring11",
-#        "TTToHplusBWB_M150_Spring11",
-#        "TTToHplusBWB_M155_Spring11",
-#        "TTToHplusBWB_M160_Spring11",
-        # (HH)
-#        "TTToHplusBHminusB_M80_Spring11",
-#        "TTToHplusBHminusB_M100_Spring11",
-#        "TTToHplusBHminusB_M120_Spring11",
-#        "TTToHplusBHminusB_M140_Spring11",
-#        "TTToHplusBHminusB_M150_Spring11",
-#        "TTToHplusBHminusB_M155_Spring11",
-#        "TTToHplusBHminusB_M160_Spring11",
+
+        # Signal MC (HH)
 
         # Background MC
-#        "QCD_Pt30to50_TuneZ2_Spring11",
-#        "QCD_Pt50to80_TuneZ2_Spring11",
-#        "QCD_Pt80to120_TuneZ2_Spring11",
-#        "QCD_Pt120to170_TuneZ2_Spring11",
-#        "QCD_Pt170to300_TuneZ2_Spring11",
-#        "QCD_Pt300to470_TuneZ2_Spring11",
-#        "TTJets_TuneZ2_Spring11",
-#        "WJets_TuneZ2_Spring11",
-#        "TToBLNu_s-channel_TuneZ2_Spring11",
-#        "TToBLNu_t-channel_TuneZ2_Spring11",
-#        "TToBLNu_tW-channel_TuneZ2_Spring11",
-#        "DYJetsToLL_M50_TuneZ2_Spring11",
-#        "WW_TuneZ2_Spring11",
-#        "WZ_TuneZ2_Spring11",
-#        "ZZ_TuneZ2_Spring11",
-
-########
-#
-# 39X
-#
-########
-        # Data (Tau)
-#        "JetMETTau_Tau_136035-139975_Dec22", # HLT_SingleLooseIsoTau20
-#        "JetMETTau_Tau_140058-141881_Dec22", # HLT_SingleLooseIsoTau20_Trk5
-#        "BTau_141956-144114_Dec22", # HLT_SingleIsoTau20_Trk5
-#        "BTau_146428-148058_Dec22", # HLT_SingleIsoTau20_Trk15_MET20
-#        "BTau_148822-149182_Dec22", # HLT_SingleIsoTau20_Trk15_MET25_v3
-#        "BTau_149291-149294_Dec22", # HLT_SingleIsoTau20_Trk15_MET25_v4
-
-        # Data (QuadJet)
-#        "JetMETTau_QuadJet_136035-141881_Dec22", # HLT_QuadJet15U
-#        "JetMET_QuadJet_141956-144114_Dec22",    # HLT_QuadJet15U
-#        "Jet_QuadJet_146428-147116_Dec22",       # HLT_QuadJet25U
-#        "MultiJet_QuadJet_147196-148058_Dec22",  # HLT_QuadJet25U_v2
-#        "MultiJet_QuadJet_148819-149442_Dec22",  # HLT_QuadJet25U_v3
-
-        # Data (Jet)
-#        "JetMETTau_Jet_136035-141881_Dec22", # HLT_Jet30U
-#        "JetMET_141956-144114_Dec22",        # HLT_Jet30U
-#        "Jet_146428-148058_Dec22",           # HLT_Jet30U
-#        "Jet_148822-149294_Dec22",           # HLT_Jet30U_v3
-
-        # Signal MC
-#        "TTToHplusBWB_M90_Winter10",
-#        "TTToHplusBWB_M100_Winter10",
-#        "TTToHplusBWB_M120_Winter10",
-#        "TTToHplusBWB_M140_Winter10",
-#        "TTToHplusBWB_M160_Winter10",
-
-        # Background MC
-#        "QCD_Pt30to50_TuneZ2_Winter10",
-#        "QCD_Pt50to80_TuneZ2_Winter10",
-#        "QCD_Pt80to120_TuneZ2_Winter10",
-#        "QCD_Pt120to170_TuneZ2_Winter10",
-#        "QCD_Pt170to300_TuneZ2_Winter10",
-#        "QCD_Pt300to470_TuneZ2_Winter10",
-#        "TTJets_TuneD6T_Winter10",
-#        "TTJets_TuneZ2_Winter10",
-#        "WJets_TuneD6T_Winter10",
-#        "WJets_TuneZ2_Winter10",
-#        "WJets_TuneZ2_Winter10_noPU",
-#        "DYJetsToLL_M10to50_TuneD6T_Winter10",
-#        "DYJetsToLL_M50_TuneD6T_Winter10",
-#        "DYJetsToLL_M50_TuneZ2_Winter10",
-#        "TToBLNu_s-channel_Winter10",
-#        "TToBLNu_t-channel_Winter10",
-#        "TToBLNu_tW-channel_Winter10"
-#        "W2Jets_ptW0to100_TuneZ2_Winter10",
-#        "W2Jets_ptW100to300_TuneZ2_Winter10",
-#        "W3Jets_ptW0to100_TuneZ2_Winter10",
-#        "W3Jets_ptW100to300_TuneZ2_Winter10",
-#        "W4Jets_ptW0to100_TuneZ2_Winter10",
-#        "W4Jets_ptW100to300_TuneZ2_Winter10",
-#        "WW_TuneZ2_Winter10",
-#        "WZ_TuneZ2_Winter10",
-#        "ZZ_TuneZ2_Winter10",
-          
-########
-#
-# 38X
-#
-########
-        # Data (Tau)
-#        "BTau_141950-144114_Nov4", # HLT_SingleIsoTau20_Trk5
-#        "BTau_146240-148107_Nov4", # HLT_SingleIsoTau20_Trk15_MET20
-#        "BTau_148108-149182_Nov4", # HLT_SingleIsoTau20_Trk15_MET25_v3
-#        "BTau_149291-149442_Nov4", # HLT_SingleIsoTau20_Trk15_MET25_v4
-        # Data (Jet)
-#        "JetMETTau_136033-141887_Nov4", # HLT_Jet30U
-#        "JetMET_141950-144114_Nov4",    # HLT_Jet30U
-#        "Jet_146240-148058_Nov4",       # HLT_Jet30U
-        # Signal Fall10 MC
-#        "TTToHplusBWB_M90_Fall10",
-#        "TTToHplusBWB_M100_Fall10",
-#        "TTToHplusBWB_M120_Fall10",
-#        "TTToHplusBWB_M140_Fall10",
-#        "TTToHplusBWB_M160_Fall10",
-        # Signal MC
-#        "TTToHpmToTauNu_M90_Spring10",
-#        "TTToHpmToTauNu_M100_Spring10",
-#        "TTToHpmToTauNu_M120_Spring10",
-#        "TTbar_Htaunu_M140_Spring10",
-#        "TTbar_Htaunu_M160_Spring10",
-        # Background Fall10 MC
-#        "QCD_Pt30to50_TuneZ2_Fall10",
-#        "QCD_Pt50to80_TuneZ2_Fall10",
-#        "QCD_Pt80to120_TuneZ2_Fall10",
-#        "QCD_Pt120to170_TuneZ2_Fall10",
-#        "QCD_Pt170to300_TuneZ2_Fall10",
-#        "TT_TuneZ2_Fall10",
-#        "TTJets_TuneZ2_Fall10",
-#        "WJets_TuneZ2_Fall10"
-        # Background MC
-#        "QCD_Pt30to50_Summer10",
-#        "QCD_Pt50to80_Summer10",
-#        "QCD_Pt80to120_Summer10",
-#        "QCD_Pt120to170_Summer10",
-#        "QCD_Pt170to230_Summer10",
-#        "QCD_Pt230to300_Summer10",
-#        "TTbar_Summer10",
-#        "TTbarJets_Summer10",
-#        "WJets_Summer10",
+#        "QCD_Pt15to30_TuneZ2_Summer11",
+#        "QCD_Pt30to50_TuneZ2_Summer11",
+#        "QCD_Pt50to80_TuneZ2_Summer11",
+#        "QCD_Pt80to120_TuneZ2_Summer11",
+#        "QCD_Pt120to170_TuneZ2_Summer11",
+#        "QCD_Pt170to300_TuneZ2_Summer11",
         ])
 
 # local_stage_out doesn't work due to denied permission because we're
@@ -194,7 +52,7 @@ def addOutputName(dataset):
     path = dataset.getDatasetPath().split("/")
     name = path[2].replace("-", "_")
     name += "_"+path[3]
-    name += "_pattuple_v11b"
+    name += "_pattuple_v12_test1"
 
     # Add the begin run in the dataset name to the publish name in
     # order to distinguish pattuple datasets from the same PD
