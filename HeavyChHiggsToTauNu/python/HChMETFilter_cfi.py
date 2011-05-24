ceeb5c493b0d8f4cf44c74652b9a37ae99f71b6c
Sami Lehti <sami.lehti@helsinki.fi>
2011-05-24 10:58:12 +0200

    TriggerEfficiency: bugfix

--- HeavyChHiggsToTauNu/python/HChMETFilter_cfi.py
+++ HeavyChHiggsToTauNu/python/HChMETFilter_cfi.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 from HiggsAnalysis.HeavyChHiggsToTauNu.HChSignalAnalysisParameters_cff import MET as defaultMETSelection
 hPlusMETPtrSelectorFilter = cms.EDFilter("HPlusMETPtrSelectorFilter",
-	metSelection = defaultMETSelection
+	MET = defaultMETSelection
 )
