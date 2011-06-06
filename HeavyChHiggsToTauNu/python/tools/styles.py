b3e4db36ed973c92bcee9b5e5c87d10bcc9d50b1
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-06-06 16:55:25 +0300

    Changes to signal plot style

--- HeavyChHiggsToTauNu/python/tools/styles.py
+++ HeavyChHiggsToTauNu/python/tools/styles.py
@@ -40,11 +40,13 @@ class StyleFill(StyleBase):
         h.SetFillStyle(self.fillStyle)
 
 class StyleLine(StyleBase):
-    def __init__(self, lineStyle=1):
+    def __init__(self, lineStyle=1, lineWidth=2):
         self.lineStyle = lineStyle
+        self.lineWidth = lineWidth
 
     def apply(self, h):
         h.SetLineStyle(self.lineStyle)
+        h.SetLinewidth(self.linewidth)
 
 class StyleMarker(StyleBase):
     def __init__(self, markerSize=1.2, markerColor=None):
@@ -80,7 +82,9 @@ errorStyle2 = StyleError(ROOT.kGray+2, 3354)
 errorStyle3 = StyleError(ROOT.kRed-10, 1001, linecolor=ROOT.kRed-10)
 
 mcStyle = Style(ROOT.kFullSquare, ROOT.kGreen-2)
-signalStyle = Style(34, ROOT.kPink-9)
+signalStyle = StyleCompound([Style(34, ROOT.kPink-9), 
+                             StyleLine(lineStyle=ROOT.kDashed, lineWidth=6)
+                             ])
 signal80Style =  signalStyle
 signal90Style =  signalStyle
 signal100Style = signalStyle
