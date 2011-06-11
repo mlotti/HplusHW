640f3c42c75aeb89941cf6df3e6bec3c3383bc8d
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-06-11 12:09:27 +0200

    Invalidated all test pattuples

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData11.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData11.py
@@ -7,12 +7,6 @@ datasets = {
         "trigger": "HLT_IsoPFTau35_Trk20_MET45_v1",
         "runs": (160404, 161176), # This is prompt RECO, so check the run range again when running!
         "data": {
-            "pattuple_v10_test3": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/Tau/local-Run2011A_PromptReco_v1_AOD_160404_pattuple_v10_test3-0afb79e1b4a9efb21952e294fbf97113/USER",
-                "luminosity": 10.589643,
-                "number_of_jobs": 2
-            },
         }
     },
     "Tau_161216-161312_Prompt": {
@@ -20,12 +14,6 @@ datasets = {
         "trigger": "HLT_IsoPFTau35_Trk20_MET45_v2",
         "runs": (161216, 161312), # This is prompt RECO, so check the run range again when running!
         "data": {
-            "pattuple_v10_test3": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/Tau/local-Run2011A_PromptReco_v1_AOD_161216_pattuple_v10_test3-85ec5544f54122dfd23b7a7442771d8e/USER",
-                "luminosity": 12.291216,
-                "number_of_jobs": 2
-            },
         }
     },
     "Tau_160431-161016_Prompt": {
@@ -194,12 +182,6 @@ datasets = {
         "trigger": "HLT_QuadJet40_IsoPFTau40_v1",
         "runs": (160404, 161312), # This is prompt RECO, so check the run range again when running!
         "data": {
-            "pattuple_v10_test3": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/TauPlusX/local-Run2011A_PromptReco_v1_AOD_160404_pattuple_v10_test3-7ca7b85da23747106d19bca56315fb4a/USER",
-                "luminosity": 22.880859,
-                "number_of_jobs": 1
-            },
         }
     },
     "TauPlusX_160431-161016_Prompt": {
