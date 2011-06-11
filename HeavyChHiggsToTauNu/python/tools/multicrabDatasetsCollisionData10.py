2c76094868d2d9beee3ef1d3a03e01e57a9238c2
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-06-11 11:26:33 +0200

    Invalidated v9 pattuples (39X)

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData10.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData10.py
@@ -10,15 +10,6 @@ datasets = {
         "trigger": "HLT_SingleLooseIsoTau20",
         "runs": (136035, 139975), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/JetMETTau/local-Run2010A_Dec22ReReco_v1_AOD_Tau_pattuple_v9_1-4e2fa0cbfe0cf1221a0673f448298393/USER",
-                "luminosity": 0.085524691,
-                "number_of_jobs": 4
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            }
         }
     },
     "JetMETTau_Tau_140058-141881_Dec22": {
@@ -26,15 +17,6 @@ datasets = {
         "trigger": "HLT_SingleLooseIsoTau20_Trk5",
         "runs": (140058, 141881), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/JetMETTau/local-Run2010A_Dec22ReReco_v1_AOD_Tau_pattuple_v9_1-7b621476048eefc450e74b1a3fe9ad51/USER",
-                "luminosity": 0.198967175,
-                "number_of_jobs": 6
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            }
         }
     },
     "BTau_141956-144114_Dec22": {
@@ -42,15 +24,6 @@ datasets = {
         "trigger": "HLT_SingleIsoTau20_Trk5",
         "runs": (141956, 144114), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/BTau/local-Run2010A_Dec22ReReco_v1_AOD_141956_pattuple_v9_1-8fa92c7e2c071a3e7e6f47b50c02a015/USER",
-                "luminosity": 2.846183931,
-                "number_of_jobs": 30
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            },
             "pattuple_v11": {
                 "dbs_url": common.pattuple_dbs,
                 "datasetpath": "/BTau/local-Run2010A_Dec22ReReco_v1_AOD_141956_pattuple_v11b-b20aa4dd1fd1c459fb67271e0123029e/USER",
@@ -64,15 +37,6 @@ datasets = {
         "trigger": "HLT_SingleIsoTau20_Trk15_MET20",
         "runs": (146428, 148058), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/BTau/local-Run2010B_Dec22ReReco_v1_AOD_146428_pattuple_v9-3a013f41a0c4ff6fc1143af634d9ef19/USER",
-                "luminosity": 14.484590862,
-                "number_of_jobs": 20
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            },
             "pattuple_v10_1": {
                 "dbs_url": common.pattuple_dbs,
                 "datasetpath": "/BTau/local-Run2010B_Dec22ReReco_v1_AOD_146428_pattuple_v10_1b-c4ea815147b859c202ad50a9425ec5e1/USER",
@@ -95,15 +59,6 @@ datasets = {
         "trigger": "HLT_SingleIsoTau20_Trk15_MET25_v3",
         "runs": (148822, 149182), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/BTau/local-Run2010B_Dec22ReReco_v1_AOD_148822_pattuple_v9-9bdd93eeac3a09280bc73f406326390f/USER",
-                "luminosity": 16.084022481,
-                "number_of_jobs": 10
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            },
             "pattuple_v10_1": {
                 "dbs_url": common.pattuple_dbs,
                 "datasetpath": "/BTau/local-Run2010B_Dec22ReReco_v1_AOD_148822_pattuple_v10_1b-825f9f3a2228e308bb923d3925115592/USER",
@@ -126,15 +81,6 @@ datasets = {
         "trigger": "HLT_SingleIsoTau20_Trk15_MET25_v4",
         "runs": (149291, 149294), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/BTau/local-Run2010B_Dec22ReReco_v1_AOD_149291_pattuple_v9-46b2fb8e054eedee742fb578c3681cd6/USER",
-                "luminosity": 2.270373344,
-                "number_of_jobs": 2
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            },
             "pattuple_v10_1": {
                 "dbs_url": common.pattuple_dbs,
                 "datasetpath": "/BTau/local-Run2010B_Dec22ReReco_v1_AOD_149291_pattuple_v10_1b-79566228a8278608a71cc48012464cce/USER",
@@ -165,15 +111,6 @@ datasets = {
                 "number_of_jobs": 100, # Adjusted for PATtuple
                 "lumiMask": "Dec22ReReco"
             },
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/JetMETTau/local-Run2010A_Dec22ReReco_v1_AOD_Jet_pattuple_v9_1-7530ef6ad2e2f18191ca644614b1b027/USER",
-                "luminosity": .284491865,
-                "number_of_jobs": 10
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            }
         }
     },
     "JetMET_141956-144114_Dec22": {
@@ -181,15 +118,6 @@ datasets = {
         "trigger": "HLT_Jet30U",
         "runs": (141956, 144114), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/JetMET/local-Run2010A_Dec22ReReco_v1_AOD_141956_pattuple_v9_1-7530ef6ad2e2f18191ca644614b1b027/USER",
-                "luminosity": 2.846183931,
-                "number_of_jobs": 20
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            }
         }
     },
     "Jet_146428-148058_Dec22": {
@@ -197,15 +125,6 @@ datasets = {
         "trigger": "HLT_Jet30U",
         "runs": (146428, 148058), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/Jet/local-Run2010B_Dec22ReReco_v1_AOD_146428_pattuple_v9_1-7530ef6ad2e2f18191ca644614b1b027/USER",
-                "luminosity": 14.517746074,
-                "number_of_jobs": 2
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            }
         }
     },
     "Jet_148822-149294_Dec22": {
@@ -213,15 +132,6 @@ datasets = {
         "trigger": "HLT_Jet30U_v3",
         "runs": (148822, 149294), # The real range for this trigger (from Run Registry and the Dec22 JSON file)
         "data": {
-            "pattuple_v9_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/Jet/local-Run2010B_Dec22ReReco_v1_AOD_148822_pattuple_v9_1-f27709779cfd23143641ea67ab8bfc68/USER",
-                "luminosity": 18.435676673,
-                "number_of_jobs": 1
-            },
-            "pattuple_v9": {
-                "fallback": "pattuple_v9_1"
-            }
         }
     },
 
--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsTauEmbedding.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsTauEmbedding.py
@@ -259,28 +259,6 @@ def addTo(datasets):
             }
             })
 
-    datasets["TTToHplusBWB_M120_Winter10"]["data"].update({
-            "tauembedding_skim_v6": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/TTToHplusBWB_M-120_7TeV-pythia6-tauola/local-Winter10_E7TeV_ProbDist_2010Data_BX156_START39_V8_v1_AODSIM_tauembedding_skim_v6-356976479e058d2236b19259096d6dbe/USER",
-                "number_of_jobs": 15 # ~700 ev/job
-            },
-            "tauembedding_generation_v6_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/TTToHplusBWB_M-120_7TeV-pythia6-tauola/local-Winter10_E7TeV_ProbDist_2010Data_BX156_START39_V8_v1_AODSIM_tauembedding_generation_v6_1-e186a9fcb0f6b229006bacc604d044d9/USER",
-                "number_of_jobs": 10 # ~700 ev/job
-            },
-            "tauembedding_embedding_v6_1": {
-                "dbs_url": common.pattuple_dbs,
-                "datasetpath": "/TTToHplusBWB_M-120_7TeV-pythia6-tauola/local-Winter10_E7TeV_ProbDist_2010Data_BX156_START39_V8_v1_AODSIM_tauembedding_embedding_v6_1-105b277d7ebabf8cba6c221de6c7ed8a/USER",
-                "number_of_jobs": 3 # ~2000 ev/job
-            },
-            # Fake the v6_2 for MC for time being
-            "tauembedding_embedding_v6_2": {
-                "fallback": "tauembedding_embedding_v6_1"
-            }
-            })
-
 ################################################################################
 
     datasets["Mu_136035-144114_Dec22"]["data"].update({
