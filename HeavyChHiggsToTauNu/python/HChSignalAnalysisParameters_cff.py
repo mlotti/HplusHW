62b59bd5c5ecf74d09415b62048a2228e6189c4c
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-07-05 22:10:43 +0300

    Increased HLT MET cut to 60

--- HeavyChHiggsToTauNu/python/HChSignalAnalysisParameters_cff.py
+++ HeavyChHiggsToTauNu/python/HChSignalAnalysisParameters_cff.py
@@ -16,7 +16,7 @@ trigger = cms.untracked.PSet(
                                      "HLT_IsoPFTau35_Trk20_MET45_v6",
                                      "HLT_IsoPFTau35_Trk20_MET60_v2",
     ),
-    hltMetCut = cms.untracked.double(45.0),
+    hltMetCut = cms.untracked.double(60.0),
     selectionType = cms.untracked.string("byTriggerBit"), # Default byTriggerBit, other options byParametrisation, disabled
     triggerTauSelection = cms.untracked.PSet(),
     triggerMETSelection = cms.untracked.PSet(),
