a3ddde4eb278465f1069b9515a3ae001211501fa
Lauri.A Wendland <wendland@jade.hip.fi>
2011-07-06 12:56:30 +0300

    bug fix for merge

--- HeavyChHiggsToTauNu/interface/SignalAnalysis.h
+++ HeavyChHiggsToTauNu/interface/SignalAnalysis.h
@@ -182,7 +182,6 @@ namespace HPlus {
     TH1 *hSelectedTauEtMetCut;
     TH1 *hSelectedTauEtaMetCut;
     TH1 *hSelectedTauPhiMetCut;
-    TH1 *hMETBeforeTauId;
     TH1 *hSelectedTauEtAfterCuts;
     TH1 *hSelectedTauEtaAfterCuts;
     TH1 *hMetAfterCuts;
