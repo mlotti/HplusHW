6512a7f6f4a8864405b5f8631679698d16709e51
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-07-07 08:27:35 +0200

    More v17 pattuples

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData11May10.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCollisionData11May10.py
@@ -37,6 +37,12 @@ datasets = {
                 "luminosity": 5.884518,
                 "number_of_jobs": 1
             },
+            "pattuple_v17": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/Tau/local-Run2011A_May10ReReco_v1_AOD_160431_pattuple_v17-377a23a99017553e73fe517f9c607b59/USER",
+                "luminosity": 2.296864,
+                "number_of_jobs": 1
+            },
         }
     },
     "Tau_161217-163261_May10": {
@@ -68,6 +74,12 @@ datasets = {
                 "luminosity": 38.518306,
                 "number_of_jobs": 2
             },
+            "pattuple_v17": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/Tau/local-Run2011A_May10ReReco_v1_AOD_161217_pattuple_v17-e4cfe64c6b123ecde897f0b19cc05328/USER",
+                "luminosity": 25.267523,
+                "number_of_jobs": 2
+            },
         },
     },
     "Tau_163270-163869_May10": {
@@ -91,6 +103,12 @@ datasets = {
                 "dbs_url": common.pattuple_dbs,
                 "datasetpath": "/Tau/local-Run2011A_May10ReReco_v1_AOD_163270_pattuple_v16-8681d17f150ac7e8fcb47588faf5fa09/USER",
                 "luminosity": 159.758105,
+                "number_of_jobs": 6
+            },
+            "pattuple_v17": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/Tau/local-Run2011A_May10ReReco_v1_AOD_163270_pattuple_v17-6d098da292fab19f3d03a84563841e91/USER",
+                "luminosity": 16.940907,
                 "number_of_jobs": 6
             },
         }
