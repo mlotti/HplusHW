543be327c44f66d96426e6e974695b2e5d9b2e5b
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-09-07 09:09:32 +0300

    Refactoring crosssection.py

--- HeavyChHiggsToTauNu/python/tools/crosssection.py
+++ HeavyChHiggsToTauNu/python/tools/crosssection.py
@@ -5,21 +5,6 @@ ttCrossSection = 165.0
 defaultMu = 200
 
 whDatasetMass = {
-    # Spring10
-    "TTbar_Htaunu_M80":    80,
-    "TTToHpmToTauNu_M90":  90,
-    "TTToHpmToTauNu_M100": 100,
-    "TTToHpmToTauNu_M120": 120,
-    "TTbar_Htaunu_M140":   140,
-    "TTbar_Htaunu_M160":   160,
-
-    # Winter10
-    "TTToHplusBWB_M90_Winter10": 90,
-    "TTToHplusBWB_M100_Winter10": 100,
-    "TTToHplusBWB_M120_Winter10": 120,
-    "TTToHplusBWB_M140_Winter10": 140,
-    "TTToHplusBWB_M160_Winter10": 160,
-
     # Logical names (see plots.py)
     "TTToHplusBWB_M80": 80,
     "TTToHplusBWB_M90": 90,
@@ -34,6 +19,7 @@ whDatasetMass = {
 
 hhDatasetMass = {
     "TTToHplusBHminusB_M80": 80,
+    "TTToHplusBHminusB_M90": 90,
     "TTToHplusBHminusB_M100": 100,
     "TTToHplusBHminusB_M120": 120,
     "TTToHplusBHminusB_M140": 140,
@@ -59,58 +45,32 @@ def hhTauNuCrossSectionMSSM(mass, tanbeta, mu):
 def hhTauNuCrossSection(br_tH, br_Htaunu):
     return ttCrossSection * br_tH*br_tH * br_Htaunu*br_Htaunu
 
+
+def _setHplusCrossSectionsHelper(massList, datasets, function):
+    for name, mass in massList:
+        if not datasets.hasDataset(name):
+            continue
+        crossSection = function(mass)
+        datasets.getDataset(name).setCrossSection(crossSection)
+        print "Setting %s cross section to %f pb" % (name, crossSection)
+
+def _setHplusCrossSections(datasets, whFunction, hhFunction):
+    _setHplusCrossSectionsHelper(whDatasetMass.iteritems(), datasets, whFunction)
+    _setHplusCrossSectionsHelper(hhDatasetMass.iteritems(), datasets, hhFunction)
+
 def setHplusCrossSectionsToTop(datasets):
-    for name, mass in whDatasetMass.iteritems():
-        if not datasets.hasDataset(name):
-            continue
-
-        crossSection = ttCrossSection
-        datasets.getDataset(name).setCrossSection(crossSection)
-        print "Setting %s cross section to %f pb" % (name, crossSection)
-
-    for name, mass in hhDatasetMass.iteritems():
-        if not datasets.hasDataset(name):
-            continue
-
-        crossSection = ttCrossSection
-        datasets.getDataset(name).setCrossSection(crossSection)
-        print "Setting %s cross section to %f pb" % (name, crossSection)
-
+    function = lambda mass: ttCrossSection
+    _setHplusCrossSections(datasets, function, function)
 
 def setHplusCrossSectionsToMSSM(datasets, tanbeta=20, mu=defaultMu):
-    for name, mass in whDatasetMass.iteritems():
-        if not datasets.hasDataset(name):
-            continue
-
-        crossSection = whTauNuCrossSectionMSSM(mass, tanbeta, mu)
-        datasets.getDataset(name).setCrossSection(crossSection)
-        print "Setting %s cross section to %f pb" % (name, crossSection)
-
-    for name, mass in hhDatasetMass.iteritems():
-        if not datasets.hasDataset(name):
-            continue
-
-        crossSection = hhTauNuCrossSectionMSSM(mass, tanbeta, mu)
-        datasets.getDataset(name).setCrossSection(crossSection)
-        print "Setting %s cross section to %f pb" % (name, crossSection)
+    _setHplusCrossSections(datasets,
+                           lambda mass: whTauNuCrossSectionMSSM(mass, tanbeta, mu),
+                           lambda mass: hhTauNuCrossSectionMSSM(mass, tanbeta, mu))
 
 def setHplusCrossSectionsToBR(datasets, br_tH, br_Htaunu):
-    for name, mass in whDatasetMass.iteritems():
-        if not datasets.hasDataset(name):
-            continue
-
-        crossSection = whTauNuCrossSection(br_tH, br_Htaunu)
-        datasets.getDataset(name).setCrossSection(crossSection)
-        print "Setting %s cross section to %f pb" % (name, crossSection)
-
-    for name, mass in hhDatasetMass.iteritems():
-        if not datasets.hasDataset(name):
-            continue
-
-        crossSection = hhTauNuCrossSection(br_tH, br_Htaunu)
-        datasets.getDataset(name).setCrossSection(crossSection)
-        print "Setting %s cross section to %f pb" % (name, crossSection)
-
+    _setHplusCrossSections(datasets,
+                           lambda mass: whTauNuCrossSection(br_tH, br_Htaunu),
+                           lambda mass: hhTauNuCrossSection(br_tH, br_Htaunu))
 
 def setHplusCrossSections(datasets, tanbeta=20, mu=defaultMu, toTop=False):
     if toTop:
