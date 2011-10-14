f4f86f43e0d8a4b8378ac0545d9b6d3bf5b568a6
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-10-14 12:52:46 +0300

    Generalized HPlusSelectFirstVertex to generic first object selector

--- HeavyChHiggsToTauNu/test/tauEmbedding/muonDebug_cfg.py
+++ HeavyChHiggsToTauNu/test/tauEmbedding/muonDebug_cfg.py
@@ -121,7 +121,7 @@ process.infoPath = addConfigInfo(process, options, dataVersion)
 
 ################################################################################
 
-process.firstPrimaryVertex = cms.EDProducer("HPlusSelectFirstVertex",
+process.firstPrimaryVertex = cms.EDProducer("HPlusFirstVertexSelector",
     src = cms.InputTag("offlinePrimaryVertices")
 )
 process.commonSequence *= process.firstPrimaryVertex
