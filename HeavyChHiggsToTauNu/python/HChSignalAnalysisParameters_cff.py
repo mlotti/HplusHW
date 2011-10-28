46c5d1df807ffa89c93a53bb102a64273751cd2c
Lauri A. Wendland <lauri.wendland@cern.ch>
2011-10-28 16:55:32 +0300

    default params now MET>40 and Rtau>0.7

--- HeavyChHiggsToTauNu/python/HChSignalAnalysisParameters_cff.py
+++ HeavyChHiggsToTauNu/python/HChSignalAnalysisParameters_cff.py
@@ -49,7 +49,7 @@ tauSelectionBase = cms.untracked.PSet(
     ptCut = cms.untracked.double(40), # jet pt > value
     etaCut = cms.untracked.double(2.1), # jet |eta| < value
     leadingTrackPtCut = cms.untracked.double(20.0), # ldg. track > value
-    rtauCut = cms.untracked.double(0.0), # rtau > value
+    rtauCut = cms.untracked.double(0.7), # rtau > value
     antiRtauCut = cms.untracked.double(0.0), # rtau < value
     invMassCut = cms.untracked.double(999.), # m(vis.tau) < value; FIXME has no effect in TauSelection.cc 
     nprongs = cms.untracked.uint32(1) # not used at the moment FIXME: has no effect in TauSelection.cc
@@ -149,7 +149,7 @@ MET = cms.untracked.PSet(
     caloSrc = cms.untracked.InputTag("patMETs"),
     tcSrc = cms.untracked.InputTag("patMETsTC"),
     select = cms.untracked.string("raw"), # raw, type1, type2
-    METCut = cms.untracked.double(70.0)
+    METCut = cms.untracked.double(40.0)
 )
 
 bTagging = cms.untracked.PSet(
