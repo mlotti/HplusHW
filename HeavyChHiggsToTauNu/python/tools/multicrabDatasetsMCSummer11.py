8c13e5d427d07da68abcb21448af62c228b544ef
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-10-31 08:22:04 +0100

    Added first v19 pattuple

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsMCSummer11.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsMCSummer11.py
@@ -65,6 +65,11 @@ datasets = {
                 "datasetpath": "/TTToHplusBWB_M-120_7TeV-pythia6-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_pattuple_v18-8eea754df021b160abed50fa738aa521/USER",
                 "number_of_jobs": 1
             },
+            "pattuple_v19": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/TTToHplusBWB_M-120_7TeV-pythia6-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_pattuple_v19b-9436cd413e1f831f4594f528a53faac6/USER",
+                "number_of_jobs": 1
+            },
         }
     },
     "TTToHplusBWB_M140_Summer11": {
