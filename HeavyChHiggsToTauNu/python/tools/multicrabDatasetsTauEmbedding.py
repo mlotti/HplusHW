c8cfb649f1d941e4abe0825ebc1a3101712c2cd6
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-11-21 17:12:07 +0200

    Added v31_1 vispt30 embedded datasets

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsTauEmbedding.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsTauEmbedding.py
@@ -154,6 +154,32 @@ def addTo(datasets):
         "QCD_Pt20_MuEnriched_TuneZ2_Summer11": "/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt20-22559ec2c5e66c0c33625ecb67add84e/USER",
         })
 
+    add("embedding", "v13_1_vispt20", {
+        "SingleMu_Mu_160431-163261_May10":     "/SingleMu/local-May10ReReco_v1_AOD_160431_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_163270-163869_May10":     "/SingleMu/local-May10ReReco_v1_AOD_163270_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_165088-166150_Prompt":    "/SingleMu/local-PromptReco_v4_AOD_165088_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_166161-166164_Prompt":    "/SingleMu/local-PromptReco_v4_AOD_166161_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_166346-166346_Prompt":    "/SingleMu/local-PromptReco_v4_AOD_166346_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_166374-167043_Prompt":    "/SingleMu/local-PromptReco_v4_AOD_166374_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_167078-167913_Prompt":    "/SingleMu/local-PromptReco_v4_AOD_167078_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_170722-172619_Aug05":     "/SingleMu/local-05Aug2011_v1_AOD_170722_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_172620-173198_Prompt":    "/SingleMu/local-PromptReco_v6_AOD_172620_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "SingleMu_Mu_173236-173692_Prompt":    "/SingleMu/local-PromptReco_v6_AOD_173236_tauembedding_embedding_v13_1_vispt30-947a4a88c33687e763c591af079fc279/USER",
+        "TTJets_TuneZ2_Summer11":              "/TTJets_TuneZ2_7TeV-madgraph-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "WJets_TuneZ2_Summer11":               "/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "DYJetsToLL_M50_TuneZ2_Summer11":      "/DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "T_t-channel_TuneZ2_Summer11":         "/T_TuneZ2_t-channel_7TeV-powheg-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "Tbar_t-channel_TuneZ2_Summer11":      "/Tbar_TuneZ2_t-channel_7TeV-powheg-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "T_tW-channel_TuneZ2_Summer11":        "/T_TuneZ2_tW-channel-DR_7TeV-powheg-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "Tbar_tW-channel_TuneZ2_Summer11":     "/Tbar_TuneZ2_tW-channel-DR_7TeV-powheg-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "T_s-channel_TuneZ2_Summer11":         "/T_TuneZ2_s-channel_7TeV-powheg-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "Tbar_s-channel_TuneZ2_Summer11":      "/Tbar_TuneZ2_s-channel_7TeV-powheg-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "WW_TuneZ2_Summer11":                  "/WW_TuneZ2_7TeV_pythia6_tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "WZ_TuneZ2_Summer11":                  "/WZ_TuneZ2_7TeV_pythia6_tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "ZZ_TuneZ2_Summer11":                  "/ZZ_TuneZ2_7TeV_pythia6_tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        "QCD_Pt20_MuEnriched_TuneZ2_Summer11": "/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/local-Summer11_PU_S4_START42_V11_v1_AODSIM_tauembedding_embedding_v13_1_vispt30-22559ec2c5e66c0c33625ecb67add84e/USER",
+        })
+
     # add("embedding", "", {
     #     "SingleMu_Mu_160431-163261_May10":     ,
     #     "SingleMu_Mu_163270-163869_May10":     ,
