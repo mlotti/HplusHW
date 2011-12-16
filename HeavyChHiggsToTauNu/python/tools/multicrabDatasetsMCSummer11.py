3f2792b52b29e39c95ff654360c6a1415b37caed
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2011-12-16 17:56:22 +0200

    Added TTJets v19 pattuple

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsMCSummer11.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsMCSummer11.py
@@ -684,6 +684,11 @@ datasets = {
                 "datasetpath": "/TTJets_TuneZ2_7TeV-madgraph-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_pattuple_v18-8eea754df021b160abed50fa738aa521/USER",
                 "number_of_jobs": 4
             },
+            "pattuple_v19": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/TTJets_TuneZ2_7TeV-madgraph-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_pattuple_v19-9436cd413e1f831f4594f528a53faac6/USER",
+                "number_of_jobs": 4
+            },
         },
     },
     "WJets_TuneZ2_Summer11": {
