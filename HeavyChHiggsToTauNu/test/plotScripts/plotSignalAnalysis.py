4716fe6f8f5ac7eaaca7613aa2d624f407b8b06b
Sami Lehti <sami.lehti@helsinki.fi>
2011-12-20 10:26:10 +0100

    Added int.lumi print in plotSignalAnalysis*

--- HeavyChHiggsToTauNu/test/plotScripts/plotSignalAnalysis.py
+++ HeavyChHiggsToTauNu/test/plotScripts/plotSignalAnalysis.py
@@ -67,6 +67,8 @@ def main():
 
     plots.mergeRenameReorderForDataMC(datasets)
 
+    print "Int.Lumi",datasets.getDataset("Data").getLuminosity()
+
     # Remove signals other than M120
     #datasets.remove(filter(lambda name: "TTToHplus" in name, datasets.getAllDatasetNames()))
     datasets.remove(filter(lambda name: "TTToHplus" in name and not "M120" in name, datasets.getAllDatasetNames()))
