4fb1b6ac8f29b064480c7aeef0c85b8a64007f1f
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-01-26 09:34:33 +0100

    Added v19 pattuple for W+3jets sample

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsMCSummer11.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsMCSummer11.py
@@ -721,6 +721,11 @@ datasets = {
                 "datasetpath": "/W3Jets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM",
                 "number_of_jobs": 490, # Adjusted for PATtuple file size.
             },
+            "pattuple_v19": {
+                "dbs_url": common.pattuple_dbs,
+                "datasetpath": "/W3Jets_TuneZ2_7TeV-madgraph-tauola/local-Summer11_PU_S4_START42_V11_v1_AODSIM_pattuple_v19-9436cd413e1f831f4594f528a53faac6/USER",
+                "number_of_jobs": 16
+            },
         },
     },
     "DYJetsToLL_M50_TuneZ2_Summer11": { # Z+jets
