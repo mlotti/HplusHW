1f0cf37a39955f590dfe9e62df505391f9a1886b
Lauri A. Wendland <lauri.wendland@cern.ch>
2012-01-31 09:51:26 +0200

    updated mTplot generator script

--- HeavyChHiggsToTauNu/test/datacardGenerator/combineMt.C
+++ HeavyChHiggsToTauNu/test/datacardGenerator/combineMt.C
@@ -1,19 +1,40 @@
+#include <TCanvas.h>
+#include <TH1.h>
+#include <TROOT.h>
+#include <TStyle.h>
+#include <TFile.h>
+#include <TColor.h>
+#include <THStack.h>
+#include <TMath.h>
+#include <TLatex.h>
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
 
 void plot(int mass) {
   double myQCDRelUncert = 0.038;
   double myEWKRelUncert = 0.131;
   double myFakesRelUncert = 0.238;
-  double delta = 1.5;
+  double delta = 1.4;
   double br = 0.05;
-
+  bool debug = false;
   bool log = false;
   double ymin = 0.001;
-  double ymax = 160;
+  double ymax = 48;
   
-  cout << "Values used as relative uncertainty (please check):" << endl;
-  cout << "  QCD: " << myQCDRelUncert << endl;
-  cout << "  EWK genuine tau: " << myEWKRelUncert << endl;
-  cout << "  EWK fake tau: " << myFakesRelUncert << endl << endl;
+  static bool bMessage = false;
+  if (!bMessage) {
+    cout << "Values used as relative uncertainty (please check):" << endl;
+    cout << "  QCD: " << myQCDRelUncert << endl;
+    cout << "  EWK genuine tau: " << myEWKRelUncert << endl;
+    cout << "  EWK fake tau: " << myFakesRelUncert << endl << endl;
+    bMessage = true;
+  }
+  cout << "Processing mass point: " << mass << " GeV/c2" << endl;
   
   gStyle->SetOptFit(1);
   gStyle->SetOptStat(0);
@@ -30,6 +51,26 @@ void plot(int mass) {
 
   std::string infile = s.str();
   
+ // Canvas
+  TCanvas *myCanvas = new TCanvas("myCanvas", "",0,0,600,600);
+  myCanvas->SetHighLightColor(2);
+  myCanvas->Range(0,0,1,1);
+  myCanvas->SetFillColor(0);
+  myCanvas->SetBorderMode(0);
+  myCanvas->SetBorderSize(2);
+  if (log)
+    myCanvas->SetLogy();
+  myCanvas->SetTickx(1);
+  myCanvas->SetTicky(1);
+  myCanvas->SetLeftMargin(0.16);
+  myCanvas->SetRightMargin(0.05);
+  myCanvas->SetTopMargin(0.05);
+  myCanvas->SetBottomMargin(0.08);
+  myCanvas->SetFrameFillStyle(0);
+  myCanvas->SetFrameBorderMode(0);
+  myCanvas->SetFrameFillStyle(0);
+  myCanvas->SetFrameBorderMode(0);
+  myCanvas->cd();
 
   Int_t ci;
 
@@ -69,30 +110,6 @@ void plot(int mass) {
   fakeW->SetLineWidth(0);
   TH1* faket = (TH1*)f->Get("fake_t");
 
-  // Frame
-  TCanvas *transverseMass = new TCanvas("transverseMass", "",0,0,600,600);
-  gStyle->SetOptFit(1);
-  gStyle->SetOptStat(0);
-  gStyle->SetOptTitle(0);
-  transverseMass->SetHighLightColor(2);
-  transverseMass->Range(0,0,1,1);
-  transverseMass->SetFillColor(0);
-  transverseMass->SetBorderMode(0);
-  transverseMass->SetBorderSize(2);
-  if (log)
-    transverseMass->SetLogy();
-  transverseMass->SetTickx(1);
-  transverseMass->SetTicky(1);
-  transverseMass->SetLeftMargin(0.16);
-  transverseMass->SetRightMargin(0.05);
-  transverseMass->SetTopMargin(0.05);
-  transverseMass->SetBottomMargin(0.08);
-  transverseMass->SetFrameFillStyle(0);
-  transverseMass->SetFrameBorderMode(0);
-  transverseMass->SetFrameFillStyle(0);
-  transverseMass->SetFrameBorderMode(0);
-  transverseMass->cd();
-  
   TH1F *hFrame = new TH1F("hFrame","",20,0,400);
   hFrame->SetMinimum(ymin);
   if (log)
@@ -110,12 +127,7 @@ void plot(int mass) {
   hFrame->GetYaxis()->SetTitleFont(43);
   hFrame->GetYaxis()->SetTitleSize(27);
   hFrame->GetYaxis()->SetTitleOffset(1.3);
-
-/*  hFrame->GetXaxis()->SetLabelSize(0.05);
-  hFrame->GetXaxis()->SetTitleSize(0.05);
-  hFrame->GetYaxis()->SetLabelSize(0.05);
-  hFrame->GetYaxis()->SetTitleSize(0.05);
-  hFrame->GetYaxis()->SetTitleOffset(1.3);*/
+  
 
   // signal
   hh->Scale(br*br);
@@ -152,24 +164,26 @@ void plot(int mass) {
   uncert->Add(ewktau);
   uncert->Add(qcd);
   uncert->SetFillColor(1);
-  uncert->SetFillStyle(3354);
+  uncert->SetFillStyle(3344);
   uncert->SetLineColor(0);
   uncert->SetLineStyle(0);
   uncert->SetLineWidth(0);
 
-  TH1* hExpBkgTotalUncert = (TH1*)hExpBkg->Clone();
+  TH1* hExpBkgTotalUncert = (TH1*)uncert->Clone();
+  hExpBkgTotalUncert->SetFillStyle(3354);
 
   TH1* hAgreement = (TH1*)data->Clone();
   hAgreement->Divide(hExpBkg);
   TGraphErrors* hAgreementRelUncert = new TGraphErrors(hAgreement->GetNbinsX());
   hAgreementRelUncert->SetLineWidth(2);
+  hAgreementRelUncert->SetLineColor(kBlack);
   for (int i = 1; i <= hFrame->GetNbinsX(); ++i) {
     double myQCDTotalUncert = TMath::Power(qcd->GetBinError(i), 2)
-      + TMath::Power(qcd->GetBinContent(i)*myQCDRelUncert[i-1], 2);
+      + TMath::Power(qcd->GetBinContent(i)*myQCDRelUncert, 2);
     double myEWKTotalUncert = TMath::Power(ewktau->GetBinError(i), 2)
-      + TMath::Power(ewktau->GetBinContent(i)*myEWKRelUncert[i-1], 2);
+      + TMath::Power(ewktau->GetBinContent(i)*myEWKRelUncert, 2);
     double myFakesTotalUncert = TMath::Power(fakes->GetBinError(i), 2)
-      + TMath::Power(fakes->GetBinContent(i)*myFakesRelUncert[i-1], 2);
+      + TMath::Power(fakes->GetBinContent(i)*myFakesRelUncert, 2);
     hExpBkgTotalUncert->SetBinError(i, TMath::Sqrt(myQCDTotalUncert + myEWKTotalUncert + myFakesTotalUncert));
 
     if (hExpBkg->GetBinContent(i) > 0) {
@@ -183,11 +197,15 @@ void plot(int mass) {
       hAgreementRelUncert->SetPoint(i-1, hExpBkg->GetBinCenter(i), 0);
       hAgreementRelUncert->SetPointError(i-1, 0, 0);
     }
-    cout << "point: " << hAgreementRelUncert->GetX()[i-1] << ", " << hAgreementRelUncert->GetY()[i-1] << ", uncert: " << hAgreementRelUncert->GetErrorX(i-1) << ", " << hAgreementRelUncert->GetErrorY(i-1) << endl;
-    cout << "  bkg. stat. uncert. " << hExpBkg->GetBinError(i) << ", stat+syst uncert. " << TMath::Sqrt(myQCDTotalUncert + myEWKTotalUncert + myFakesTotalUncert) << endl;
+    if (debug) {
+      cout << "Point: " << hAgreementRelUncert->GetX()[i-1]-10 << "-" << hAgreementRelUncert->GetX()[i-1]+10
+          << " GeV/c2, agreement: " << hAgreementRelUncert->GetY()[i-1] << ", uncert: " << hAgreement->GetBinError(i) << ", " << hAgreementRelUncert->GetErrorY(i-1) << endl;
+      cout << "  bkg. stat. uncert. " << hExpBkg->GetBinError(i) << " (i.e. " << hExpBkg->GetBinError(i) / hExpBkg->GetBinContent(i) * 100.0 << " %)"
+          << ", stat+syst uncert. " << TMath::Sqrt(myQCDTotalUncert + myEWKTotalUncert + myFakesTotalUncert) 
+          << " (i.e. " << TMath::Sqrt(myQCDTotalUncert + myEWKTotalUncert + myFakesTotalUncert) / hExpBkg->GetBinContent(i) * 100.0 << " %)" << endl;
+    }
   }
 
-  /*
   // Agreement pad
   TPad* pad = new TPad("ratiopad","ratiopad",0.,0.,1.,.3);
   pad->Draw();
@@ -203,8 +221,6 @@ void plot(int mass) {
   pad->SetRightMargin(0.05);
   pad->SetTopMargin(0);
   pad->SetBottomMargin(0.34);
-  pad->SetFrameFillStyle(0);
-  pad->SetFrameBorderMode(0);
   pad->SetFrameFillStyle(0);
   pad->SetFrameBorderMode(0);
   // Plot here ratio
@@ -242,9 +258,8 @@ void plot(int mass) {
   hAgreementRelUncert->Draw("[]");
   pad->RedrawAxis();
 
-  transverseMass->cd();
-  */
-  // plot pad
+  myCanvas->cd();
+  
   TPad* plotpad = new TPad("plotpad", "plotpad",0,0.3,1.,1.);
   plotpad->Draw();
   plotpad->cd();
@@ -263,8 +278,7 @@ void plot(int mass) {
   plotpad->SetBottomMargin(0.0);
   plotpad->SetFrameFillStyle(0);
   plotpad->SetFrameBorderMode(0);
-  plotpad->SetFrameFillStyle(0);
-  plotpad->SetFrameBorderMode(0);
+  
   hFrame->GetXaxis()->SetTitleSize(0);
   hFrame->GetXaxis()->SetLabelSize(0);
   hFrame->GetYaxis()->SetTitleFont(43);
@@ -275,67 +289,90 @@ void plot(int mass) {
   hFrame->Draw();
   exp->Draw("hist same");
   uncert->Draw("E2 same");
-
+  hExpBkgTotalUncert->Draw("E2 same");
   // Data
   data->Draw("same");
   
   //signal->Draw("same");
-
-
-  TLegend *leg = new TLegend(0.55,0.68,0.9,0.93,NULL,"brNDC");
+  TLegend *leg = new TLegend(0.53,0.6,0.87,0.91,NULL,"brNDC");
   leg->SetBorderSize(0);
-  leg->SetTextFont(64);
+  leg->SetTextFont(63);
   leg->SetTextSize(18);
   leg->SetLineColor(1);
   leg->SetLineStyle(1);
   leg->SetLineWidth(1);
-  leg->SetFillColor(0);
-  leg->SetFillStyle(4000);
+  leg->SetFillColor(kWhite);
+  //leg->SetFillStyle(4000); // enabling this will cause the plot to be erased from the pad
   TLegendEntry* entry = leg->AddEntry(data, "Data", "P");
   s.str("");
-  s << "HH+HW(" << mass << "), br=" << br;
+  s << "with H^{#pm}#rightarrow#tau^{#pm}#nu";
   entry = leg->AddEntry(signal, s.str().c_str(), "L");
   entry = leg->AddEntry(qcd, "QCD (meas.)", "F");
-  entry = leg->AddEntry(ewktau, "EWK w. taus (meas.)", "F");
-  entry = leg->AddEntry(fakes, "EWK fake taus (MC)", "F");
+  entry = leg->AddEntry(ewktau, "EWK genuine #tau (meas.)", "F");
+  entry = leg->AddEntry(fakes, "EWK fake #tau (MC)", "F");
+  entry = leg->AddEntry(uncert, "stat. uncert.", "F");
+  entry = leg->AddEntry(hExpBkgTotalUncert, "stat. #oplus syst. uncert.", "F");
   leg->Draw();
+  
+  TLatex *tex = new TLatex(0.62,0.945,"CMS Preliminary");
+  tex->SetNDC();
+  tex->SetTextFont(43);
+  tex->SetTextSize(27);
+  tex->SetLineWidth(2);
+  tex->Draw();
+  tex = new TLatex(0.2,0.945,"#sqrt{s} = 7 TeV");
+  tex->SetNDC();
+  tex->SetTextFont(43);
+  tex->SetTextSize(27);
+  tex->SetLineWidth(2);
+  tex->Draw();
+  tex = new TLatex(0.43,0.945,"2.2 fb^{-1}");
+  tex->SetNDC();
+  tex->SetTextFont(43);
+  tex->SetTextSize(27);
+  tex->SetLineWidth(2);
+  tex->Draw();
 
+  s.str("");
+  s << "m_{H^{#pm}} = " << mass << " GeV/c^{2}";
+  tex = new TLatex(0.28,0.865,s.str().c_str());
+  tex->SetNDC();
+  tex->SetTextFont(63);
+  tex->SetTextSize(20);
+  tex->SetLineWidth(2);
+  tex->Draw();
+  s.str("");
+  s << "BR(t#rightarrowbH^{#pm})=" << setprecision(2) << br;
+  tex = new TLatex(0.28,0.805,s.str().c_str());
+  tex->SetNDC();
+  tex->SetTextFont(63);
+  tex->SetTextSize(20);
+  tex->SetLineWidth(2);
+  tex->Draw();
 
-   TLatex *   tex = new TLatex(0.62,0.945,"CMS Preliminary");
-tex->SetNDC();
-   tex->SetTextFont(43);
-   tex->SetTextSize(27);
-   tex->SetLineWidth(2);
-   tex->Draw();
-      tex = new TLatex(0.2,0.945,"#sqrt{s} = 7 TeV");
-tex->SetNDC();
-   tex->SetTextFont(43);
-   tex->SetTextSize(27);
-   tex->SetLineWidth(2);
-   tex->Draw();
-      tex = new TLatex(0.43,0.945,"2.2 fb^{-1}");
-tex->SetNDC();
-   tex->SetTextFont(43);
-   tex->SetTextSize(27);
-   tex->SetLineWidth(2);
-   tex->Draw();
+  
+  plotpad->RedrawAxis();
+  plotpad->Modified();
 
-   plotpad->RedrawAxis();
-   plotpad->Modified();
-
-   s.str("");
-   s << "mT_datadriven_m" << mass << ".png";
-   transverseMass->Print(s.str().c_str());
+  s.str("");
+  s << "mT_datadriven_m" << mass << ".png";
+  myCanvas->Print(s.str().c_str());
+  s.str("");
+  s << "mT_datadriven_m" << mass << ".C";
+  myCanvas->Print(s.str().c_str());
+  s.str("");
+  s << "mT_datadriven_m" << mass << ".eps";
+  myCanvas->Print(s.str().c_str());
    
 
 }
 
 void combineMt(){
-  /*plot(80);
+  plot(80);
   plot(100);
   plot(120);
   plot(140);
   plot(150);
-  plot(155);*/
+  plot(155);
   plot(160);
 }
