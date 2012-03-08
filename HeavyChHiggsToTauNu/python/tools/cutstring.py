8fa3b551c53ed85522306dc8133e78dc2a09bda9
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-03-08 10:37:06 +0200

    Documented cutstring module

--- HeavyChHiggsToTauNu/python/tools/cutstring.py
+++ HeavyChHiggsToTauNu/python/tools/cutstring.py
@@ -1,16 +1,44 @@
+## \package cutstring
+# Logical operations for cut strings (for TTree::Draw)
+#
+# TCut has quite cumbersome syntax in PyROOT, and operating with
+# python strings is very straightforward in the end
+
+## Is the string empty?
+#
+# Intended for internal use
 def _isNotEmpty(string):
     return string != ""
 
+## Construct 'and' of the argument strings
+#
+# \param args   String arguments
+#
+# \return 'and' of arguments
+#
+# Empty arguments are ignored
 def And(*args):
     lst = filter(_isNotEmpty, args)
     return "("+"&&".join(lst)+")"
 
+## Construct 'not' of the argument string
+#
+# \param arg  String
+#
+# \return 'not' of argument. If argument is empty, empty string is returned.
 def Not(arg):
     if _isNotEmpty(arg):
         return "(!(%s))"%arg
     else:
         return ""
 
+## Construct 'or' of the argument strings
+#
+# \param args   String arguments
+#
+# \return 'or' of arguments
+#
+# Empty arguments are ignored
 def Or(*args):
     lst = filter(_isNotEmpty, args)
     return "("+"||".join(lst)+")"
