b9910176715b7d5089cb4597b16ea0237f58342f
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-03-08 09:08:13 +0200

    Bumped pattuple version to v22

--- HeavyChHiggsToTauNu/test/pattuple/multicrabPatTuple.py
+++ HeavyChHiggsToTauNu/test/pattuple/multicrabPatTuple.py
@@ -166,7 +166,7 @@ def addOutputName(dataset):
     path = dataset.getDatasetPath().split("/")
     name = path[2].replace("-", "_")
     name += "_"+path[3]
-    name += "_pattuple_v20_test1"
+    name += "_pattuple_v22"
 
     # Add the begin run in the dataset name to the publish name in
     # order to distinguish pattuple datasets from the same PD
