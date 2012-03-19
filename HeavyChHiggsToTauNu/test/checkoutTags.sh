c2b718ec62fff4809b68a388ae11f8ffa21c9796
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-03-19 09:39:54 +0200

    Updated lumi tag to include pixel lumi calculation

--- HeavyChHiggsToTauNu/test/checkoutTags.sh
+++ HeavyChHiggsToTauNu/test/checkoutTags.sh
@@ -56,6 +56,7 @@ set -e
 # 6.10.2011/M.Kortelainen CMSSW_4_2_8_patch2 Another bugfix from Christian for type I/II MEt
 # 17.10.2011/M.Kortelainen CMSSW_4_2_8_patch2 Updated PU reweight tag for the updated recipe, lumi tag for minor bugfix (which is probably not relevant to us)
 # 29.12.2011/S.Lehti       CMSSW_4_2_8_patch2 Commented removal of HiggsAnalysis/Skimming/python/earlyDataInterestingEvents_cff.py
+# 19.3.2012/M.Kortelainen CMSSW_4_2_8_patch2 Updated lumi tag to include the pixel lumi
 
 # addpkg requires cmsenv
 eval $(scram runtime -sh)
@@ -102,7 +103,7 @@ cvs up -r 1.6 JetMETCorrections/Modules/interface/JetCorrectionProducer.h
 
 # Luminosity
 # https://twiki.cern.ch/twiki/bin/view/CMS/LumiCalc
-addpkg RecoLuminosity/LumiDB      V03-03-02
+addpkg RecoLuminosity/LumiDB      V03-04-02
 
 # Electron ID
 # https://twiki.cern.ch/twiki/bin/view/CMS/SimpleCutBasedEleID
