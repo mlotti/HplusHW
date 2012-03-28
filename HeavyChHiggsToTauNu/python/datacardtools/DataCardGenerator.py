b26516204a727b8de581b6fe9c4a6c7c19216fad
Sami Lehti <sami.lehti@helsinki.fi>
2012-03-28 22:25:09 +0200

    datacardGenerator.py: updated inputfile

--- HeavyChHiggsToTauNu/python/datacardtools/DataCardGenerator.py
+++ HeavyChHiggsToTauNu/python/datacardtools/DataCardGenerator.py
@@ -10,19 +10,49 @@ import HiggsAnalysis.HeavyChHiggsToTauNu.tools.styles as styles
 import HiggsAnalysis.HeavyChHiggsToTauNu.tools.plots as plots
 import HiggsAnalysis.HeavyChHiggsToTauNu.tools.crosssection as xsect
 
+import HiggsAnalysis.HeavyChHiggsToTauNu.datacardtools.MulticrabPathFinder as PathFinder
+
+from HiggsAnalysis.HeavyChHiggsToTauNu.tools.aux import load_module,sort
+
 class DataCardGenerator:
-    def __init__(self, config):
+    def __init__(self, cfgFileName):
+	config = load_module(cfgFileName)
+	config.DataGroups.Print()
+	config.Nuisances.Print()
+
+	self.reportUnusedNuisances(config)
+
 	self.config = config
-#        self.name = config.DataCardName
-#        self.massPoints = config.MassPoints
-        
-    def generate(self,dirs):
+
+    def reportUnusedNuisances(self,config):
+	usedNuisances = []
+        for nuisance in config.Nuisances.nuisances.keys():
+	    for datagroup in config.DataGroups.datagroups.keys():
+		for usedNuisance in config.DataGroups.get(datagroup).nuisances:
+		    if usedNuisance == nuisance:
+			usedNuisances.append(nuisance)
+	usedNuisances = self.rmDuplicates(usedNuisances)
+	unUsedNuisances = []
+	for nuisance in config.Nuisances.nuisances.keys():
+	    if nuisance not in usedNuisances:
+		#print "UNUSED NUISANCE"
+		#config.Nuisances.get(nuisance).Print
+		unUsedNuisances.append(nuisance)
+	print "Unused nuisances",sort(unUsedNuisances)
+
+    def rmDuplicates(self,list):
+	retlist = []
+	for element in list:
+	    if element not in retlist:
+		retlist.append(element)
+	return retlist
+
+    def generate(self):
 	signalDir = []
-	signalDir.append(dirs[0])
+	signalDir.append(self.config.multicrabPaths.getSignalPath())
 	datasets = dataset.getDatasetsFromMulticrabDirs(signalDir,counters=self.config.CounterDir)
 	datasets.loadLuminosities()
 	plots.mergeRenameReorderForDataMC(datasets)
-#	xsect.setHplusCrossSectionsToBR(datasets, br_tH=0.05, br_Htaunu=1)
-#	plots.mergeWHandHH(datasets)
 	luminosity = datasets.getDataset("Data").getLuminosity()
         print "Luminosity = ",luminosity
+
--- HeavyChHiggsToTauNu/python/tools/aux.py
+++ HeavyChHiggsToTauNu/python/tools/aux.py
@@ -4,6 +4,7 @@ import sys
 import os
 import hashlib
 import imp
+import re
 
 def execute(cmd):
     f = os.popen(cmd)
@@ -27,4 +28,22 @@ def load_module(code_path):
             except: pass
     except:
 	print "Problem importing file",code_path
+	print "check the file with 'python",code_path,"'"
 	sys.exit()
+
+def sort(list):
+    value_re = re.compile("(^\d+)(\D*$)")
+    for t in list:
+        i = len(list)
+        while i > 1:
+            match1 = value_re.search(list[i-1])
+            match2 = value_re.search(list[i-2])  
+            if int(match1.group(1)) < int(match2.group(1)):
+                 swap(list,i-1,i-2)
+            i = i - 1
+    return list
+        
+def swap(list,n1,n2):
+    tmp = list[n1]
+    list[n1] = list[n2]
+    list[n2] = tmp
