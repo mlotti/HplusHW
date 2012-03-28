f794888a4cb8e33057f591d0837533196ff7d5ad
Lauri A. Wendland <lauri.wendland@cern.ch>
2012-03-28 10:39:28 +0300

    added v25 test files

--- HeavyChHiggsToTauNu/python/HChDataVersion.py
+++ HeavyChHiggsToTauNu/python/HChDataVersion.py
@@ -52,7 +52,8 @@ config = {
         "triggerProcess": "HLT",
         "recoProcess": "RECO",
         "signalTrigger": "HLT_MediumIsoPFTau35_Trk20_MET60_v1",
-        "patMadhatter": "file:/mnt/flustre/mkortela/data/TTToHplusBWB_M-120_7TeV-pythia6-tauola/Fall11-PU_S6_START44_V9B-v1/AODSIM/28ACFF78-0237-E111-97C7-00261894397B.root"
+        "patMadhatter": "file:/mnt/flustre/mkortela/data/TTToHplusBWB_M-120_7TeV-pythia6-tauola/Fall11-PU_S6_START44_V9B-v1/AODSIM/28ACFF78-0237-E111-97C7-00261894397B.root",
+        "analysisMadhatter": "/store/group/local/HiggsChToTauNuFullyHadronic/pattuples/CMSSW_4_4_X/TTToHplusBWB_M120_Fall11/TTToHplusBWB_M-120_7TeV-pythia6-tauola/Spring10_START3X_V26_v1_GEN-SIM-RECO-pattuple_v3_test2_TTToHplusBWB_M120_Fall11//b907d114bdb314991aecc34de5a9eb36/pattuple_10_4_2mj.root",
         #"patMadhatter": "file:/mnt/flustre/mkortela/data//QCD_Pt-170to300_TuneZ2_7TeV_pythia6/Fall11-PU_S6_START44_V9B-v1/AODSIM/F468E7CF-C029-E111-BDA8-003048D47750.root"
     },
     "44XmcAve32": {
