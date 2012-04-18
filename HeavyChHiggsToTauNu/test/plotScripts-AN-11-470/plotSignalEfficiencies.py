d92feaa8a4196b946d4924e08639be1a6e086c01
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-04-18 14:59:00 +0300

    Enhanced signal efficiency plot

--- HeavyChHiggsToTauNu/test/plotScripts-AN-11-470/plotSignalEfficiencies.py
+++ HeavyChHiggsToTauNu/test/plotScripts-AN-11-470/plotSignalEfficiencies.py
@@ -60,7 +60,7 @@ def main():
     eventCounter.normalizeMCByCrossSection()
     mainTable = eventCounter.getMainCounterTable()
 
-    signalDatasets = [
+    wh = [
         "TTToHplusBWB_M80",
         "TTToHplusBWB_M90",
         "TTToHplusBWB_M100",
@@ -70,8 +70,24 @@ def main():
         "TTToHplusBWB_M155",
         "TTToHplusBWB_M160",
         ]
-    allName = "All events"
+    hh = [
+        "TTToHplusBHminusB_M80",
+        "TTToHplusBHminusB_M90",
+        "TTToHplusBHminusB_M100",
+        "TTToHplusBHminusB_M120",
+        "TTToHplusBHminusB_M140",
+        "TTToHplusBHminusB_M150",
+        "TTToHplusBHminusB_M155",
+        "TTToHplusBHminusB_M160",
+        ]
 
+    doPlots(mainTable, wh, "HW", True)
+    doPlots(mainTable, wh, "HW", False)
+    doPlots(mainTable, hh, "HH", True)
+    doPlots(mainTable, hh, "HH", False)
+
+def doPlots(mainTable, signalDatasets, signalPostfix, consequtive=True):
+    allName = "All events"
     cuts = [
         "Trigger and HLT_MET cut",
         "primary vertex",
@@ -98,11 +114,16 @@ def main():
         # Get the counts (returned objects are of type dataset.Count,
         # and have both value and uncertainty
         allCount = column.getCount(column.getRowNames().index("All events"))
+        prevCount = allCount
 
         for cut in cuts:
             cutCount = column.getCount(column.getRowNames().index(cut))
             eff = cutCount.clone()
-            eff.divide(allCount) # N(cut) / N(all)
+            if consequtive:  
+                eff.divide(prevCount)
+                prevCount = cutCount
+            else:
+                eff.divide(allCount) # N(cut) / N(all)
         
             yvalues[cut].append(eff.value())
             yerrs[cut].append(eff.uncertainty())
@@ -112,68 +133,81 @@ def main():
                                array.array("d", xerrs), array.array("d", yerrs[cutname]))
         gr.SetMarkerStyle(24)
         gr.SetMarkerColor(2)
-        gr.SetMarkerSize(0.9)
+        gr.SetMarkerSize(2)
         gr.SetLineStyle(1)
-        gr.SetLineWidth(2)
+        gr.SetLineWidth(4)
         return gr
 
-    gtrig = createErrors("Trigger and HLT_MET cut")
-    gtrig.SetLineColor(38)
-    gtrig.SetMarkerColor(38)
-    gtrig.SetLineStyle(2) 
-    gtau = createErrors("taus == 1")
-    gtau.SetLineColor(2)
-    gtau.SetMarkerColor(2)
-    gtau.SetLineStyle(3)  
-    #gtau = createErrors("trigger scale factor")
-    gveto = createErrors("muon veto")
-    gveto.SetLineColor(1)
-    gveto.SetMarkerColor(1)
-    gveto.SetLineStyle(4) 
-    gjets = createErrors("njets")
-    gjets.SetLineColor(4)
-    gjets.SetMarkerColor(4)
-    gjets.SetLineStyle(1) 
-    gmet = createErrors("MET")
-    gmet.SetLineColor(2)
-    gmet.SetMarkerColor(2)
-    gmet.SetLineStyle(5) 
-    gbtag = createErrors("btagging")
-    gbtag.SetLineColor(1)
-    gbtag.SetMarkerColor(1)
-    gbtag.SetLineStyle(6) 
-    #gtau = createErrors("trigger scale factor")
+    def setStyle(gr, lc, ls, ms):
+        gr.SetLineColor(lc)
+        gr.SetLineStyle(ls)
+        gr.SetMarkerColor(lc)
+        gr.SetMarkerStyle(ms)
 
+    gtrig = createErrors("Trigger and HLT_MET cut")
+    setStyle(gtrig, lc=38, ls=8, ms=20)
+
+    #gtau = createErrors("trigger scale factor")
+    gtau = createErrors("taus == 1")
+    setStyle(gtau, lc=2, ls=3, ms=21)
+
+    gveto = createErrors("muon veto")
+    setStyle(gveto, lc=1, ls=5, ms=22)
+
+    gjets = createErrors("njets")
+    setStyle(gjets, lc=4, ls=1, ms=23)
+
+    gmet = createErrors("MET")
+    setStyle(gmet, lc=2, ls=2, ms=24)
+
+    gbtag = createErrors("btagging")
+    setStyle(gbtag, lc=1, ls=6, ms=24)
                         
     glist = [gtrig, gtau, gveto, gjets, gmet, gbtag]
     
-    opts = {"xmin": 75, "xmax": 165, "ymin": 0.001}
-    canvasFrame = histograms.CanvasFrame([histograms.HistoGraph(g, "", "") for g in glist], "SignalEfficiency", **opts)
+    #opts = {"xmin": 75, "xmax": 165, "ymin": 0.001}
+    opts = {"xmin": 75, "xmax": 165, "ymin": 7e-4, "ymax": 2e-1}
+    name = "SignalEfficiency"
+    if consequtive:
+        opts.update({"ymin": 2.5e-2, "ymax": 1.15})
+        name += "Conseq"
+    name += "_"+signalPostfix
+
+    canvasFrame = histograms.CanvasFrame([histograms.HistoGraph(g, "", "") for g in glist], name, **opts)
     canvasFrame.frame.GetYaxis().SetTitle("Selection efficiency")
     canvasFrame.frame.GetXaxis().SetTitle("m_{H^{#pm}} (GeV/c^{2})")
     canvasFrame.canvas.SetLogy(True)
     canvasFrame.frame.Draw()
 
     for gr in glist:
-        gr.Draw("PC same")
+        gr.Draw("PL same")
     
     histograms.addEnergyText()
     histograms.addCmsPreliminaryText()
 
     legend = histograms.createLegend(x1=0.5, y1=0.53, x2=0.85, y2=0.75)
+    legend = histograms.moveLegend(legend, dx=-0.3, dy=-0.04)
 
-    legend.AddEntry(gtrig,"Trigger", "l"); 
-    legend.AddEntry(gtau, "#tau identification", "l"); 
-    legend.AddEntry(gveto ,"lepton vetoes", "l"); 
-    legend.AddEntry(gjets ,"3 jets", "l"); 
-    legend.AddEntry(gmet,"MET ", "l")
-    legend.AddEntry(gbtag,"b tagging ", "l")
+    legend.AddEntry(gtrig,"Trigger", "lp"); 
+    legend.AddEntry(gtau, "#tau-jet identification", "lp"); 
+    legend.AddEntry(gveto ,"lepton vetoes", "lp"); 
+    legend.AddEntry(gjets ,"3 jets", "lp"); 
+    legend.AddEntry(gmet,"E_{T}^{miss} ", "lp")
+    legend.AddEntry(gbtag,"b tagging ", "lp")
     legend.Draw()
-    
+
+    process = {    
+        "HW": "t#bar{t} #rightarrow W^{+}bH^{-}#bar{b}",
+        "HH": "t#bar{t} #rightarrow H^{+}bH^{-}#bar{b}"
+        }[signalPostfix]
+
+    histograms.addText(x=legend.GetX1()+0.02, y=legend.GetY2()+0.01,
+                       text=process, size=17)
+
     canvasFrame.canvas.SaveAs(".png")
     canvasFrame.canvas.SaveAs(".C")
     canvasFrame.canvas.SaveAs(".eps")
-    
+
 # Call the main function if the script is executed (i.e. not imported)
 if __name__ == "__main__":
     main()
