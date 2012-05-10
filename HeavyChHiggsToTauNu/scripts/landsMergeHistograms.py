26acebdc317f39ed3e11253709a74e1229e52dcc
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-05-10 09:02:57 +0300

    Proper python implementation of landsMergeHistograms, forward all arguments to hplusMergeHistograms

--- HeavyChHiggsToTauNu/scripts/landsMergeHistograms.py
+++ HeavyChHiggsToTauNu/scripts/landsMergeHistograms.py
@@ -1,3 +1,17 @@
-#hplusMergeHistograms.py -i "lands_datacard_hplushadronic_m\S*\.txt_Hybrid_limits_tree\S*\.root"
-#hplusMergeHistograms.py -i "datacard\S+\.txt_Hybrid_limits_tree\S*\.root"
-hplusMergeHistograms.py -i "split\S+_limits_tree\S*\.root"
+#!/usr/bin/env python
+
+import sys
+import subprocess
+
+def main():
+    args = sys.argv[1:]
+
+    command = ["hplusMergeHistograms.py", "-i", "split\S+_limits_tree\S*\.root"]+args
+    ret = subprocess.call(command)
+    if ret != 0:
+        raise Exception("hplusMergeHistograms failed with exit code %d, command was\n%s" %(ret, " ".join(command)))
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
