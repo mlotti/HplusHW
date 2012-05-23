e60d9b572a09d1692c8020d8cbd94941f49db87f
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-05-23 12:36:37 +0300

    Better command line argument support for landsMergeHistograms

--- HeavyChHiggsToTauNu/scripts/landsMergeHistograms.py
+++ HeavyChHiggsToTauNu/scripts/landsMergeHistograms.py
@@ -3,30 +3,36 @@
 import sys
 import json
 import subprocess
+from optparse import OptionParser
 
+import HiggsAnalysis.HeavyChHiggsToTauNu.tools.multicrab as multicrab
 import HiggsAnalysis.HeavyChHiggsToTauNu.tools.LandSTools as lands
 
-def main():
-    args = sys.argv[1:]
-
+def main(opts, args):
     # Merge the LandS root files
     print "Merging job ROOT files"
     f = open("configuration.json")
     config = json.load(f)
     f.close()
+
+    # forward parameters
+    if opts.delete:
+        args.append("--delete")
+    if len(opts.filter) > 0:
+        args.extend(["--filter", opts.filter])
+    for d in opts.dirs:
+        args.extend(["-d", d])
+
     if config["clsType"] == "LEP":
         command = ["hplusMergeHistograms.py", "-i", "split\S+_limits_tree\S*\.root"]+args
     elif config["clsType"] == "LHC":
         command = ["hplusMergeHistograms.py", "-i", "split\S+_m2lnQ\S*\.root"]+args
     else:
         raise Exception("Unsupported clsType '%s' in configuration.json" % config["clsType"])
-    if not "--skipMerge" in args:
+    if not opts.skipMerge:
         ret = subprocess.call(command)
         if ret != 0:
             raise Exception("hplusMergeHistograms failed with exit code %d, command was\n%s" %(ret, " ".join(command)))
-
-    if "-h" in args or "--help" in args:
-        return 0
 
     # Run LandS to do obtain the results
     print "Running LandS for merged expected results"
@@ -37,4 +43,13 @@ def main():
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main())
+    parser = OptionParser(usage="Usage: %prog [options] -- [options to hplusMergeHistograms]")
+    multicrab.addOptions(parser)
+    parser.add_option("--skipMerge", dest="skipMerge", default=False, action="store_true",
+                      help="Don't run hplusMergeHistograms (i.e. run only LandS for the merged output)")
+    parser.add_option("--delete", dest="delete", default=False, action="store_true",
+                      help="Delete the source files to save disk space (default is to keep the files)")
+
+    (opts, args) = parser.parse_args()
+
+    sys.exit(main(opts, args))
