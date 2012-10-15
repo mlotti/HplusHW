e0b45c3c641a71ef72057d37dff35c7efe7567df
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-10-15 11:11:19 +0300

    Adjusting lumi mask names in preparation for 2012

--- HeavyChHiggsToTauNu/python/tools/certifiedLumi.py
+++ HeavyChHiggsToTauNu/python/tools/certifiedLumi.py
@@ -5,11 +5,14 @@
 #
 # The JSON files are assumed to exist in <tt>test</tt> directory.
 files = {
+    # 2011
+    "DCSONLY11": "json_DCSONLY_2011.txt",
+    "PromptReco11": "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    # 42X ReRecos
     "May10ReReco": "Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt",
     "Aug05ReReco": "Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt",
-    "PromptReco": "Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt",
+    # 44X ReRecos
     "Nov08ReReco": "Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt",
-    "DCSONLY11": "json_DCSONLY_2011.txt",
 }
 
 ## Get a JSON file name
