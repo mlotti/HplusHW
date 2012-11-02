1dc6555d4e248b0c261fef8706024bda7c377f17
Sami Lehti <sami.lehti@helsinki.fi>
2012-11-02 10:47:41 +0100

    AnalysisConfiguration.py: added builder for QCDInverted

--- HeavyChHiggsToTauNu/test/signalAnalysis_cfg.py
+++ HeavyChHiggsToTauNu/test/signalAnalysis_cfg.py
@@ -45,6 +45,7 @@ builder = ConfigBuilder(dataVersion, dataEras,
                         )
 
 process = builder.buildSignalAnalysis()
+#process = builder.buildSignalAnalysisInvertedTau()
 
 if builder.options.tauEmbeddingInput != 0:
     if builder.dataVersion.isMC():
