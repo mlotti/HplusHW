5b56d74b4e841fab079f5fbc3ed88c052203d07e
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-11-09 15:01:53 +0200

    technical: Store HistoWrapper::Level in WrappedTHN, activicity decision is done in HistoWrapper
    On route towards "temporary disabling" of histogram and counter filling

--- HeavyChHiggsToTauNu/interface/HistoWrapper.h
+++ HeavyChHiggsToTauNu/interface/HistoWrapper.h
@@ -51,6 +51,10 @@ namespace HPlus {
     /// Returns the event weight
     double getWeight() const { return fEventWeight.getWeight(); }
 
+    bool isActive(HistoLevel level) const {
+      return level <= fAmbientLevel;
+    }
+
   private:
     /// Method for checking if a directory exists
     bool checkIfDirExists(TDirectory* d, std::string name) const;
@@ -69,70 +73,70 @@ namespace HPlus {
   /// Wrapper class for TH1 object
   class WrappedTH1 {
   public:
-    WrappedTH1(HistoWrapper& histoWrapper, TH1* histo, bool isActive);
+    WrappedTH1(HistoWrapper& histoWrapper, TH1* histo, HistoWrapper::HistoLevel level);
     ~WrappedTH1();
 
     /// Returns true if the histogram exists
-    bool isActive() const { return bIsActive; }
+    bool isActive() const { return fHistoWrapper.isActive(fLevel); }
     /// Returns pointer to the histogram (Note: it can be a zero pointer if the histogram is not active)
     TH1* getHisto() { return h; }
     /// Returns the x axis of the histogram for bin label modification
     TAxis* GetXaxis() { return h->GetXaxis(); }
     /// Fills histogram (if it exists) with event weight
-    template<typename Arg1> void Fill(const Arg1& a1) { if (bIsActive) h->Fill(a1, fHistoWrapper.getWeight()); }
+    template<typename Arg1> void Fill(const Arg1& a1) { if (isActive()) h->Fill(a1, fHistoWrapper.getWeight()); }
     /// Fills histogram (if it exists) with custom event weight
-    template<typename Arg1, typename Arg2> void Fill(const Arg1& a1, const Arg2& a2) { if (bIsActive) h->Fill(a1, a2); }
+    template<typename Arg1, typename Arg2> void Fill(const Arg1& a1, const Arg2& a2) { if (isActive()) h->Fill(a1, a2); }
 
   private:
     HistoWrapper& fHistoWrapper;
     TH1* h;
-    bool bIsActive;
+    HistoWrapper::HistoLevel fLevel;
   };
 
   /// Wrapper class for TH2 object
   class WrappedTH2 {
   public:
-    WrappedTH2(HistoWrapper& histoWrapper, TH2* histo, bool isActive);
+    WrappedTH2(HistoWrapper& histoWrapper, TH2* histo, HistoWrapper::HistoLevel level);
     ~WrappedTH2();
 
     /// Returns true if the histogram exists
-    bool isActive() const { return bIsActive; }
+    bool isActive() const { return fHistoWrapper.isActive(fLevel); }
     /// Returns pointer to the histogram (Note: it can be a zero pointer if the histogram is not active)
     TH2* getHisto() { return h; }
     /// Returns the x axis of the histogram for bin label modification
     TAxis* GetXaxis() { return h->GetXaxis(); }
     /// Fills histogram (if it exists) with event weight
-    template<typename Arg1, typename Arg2> void Fill(const Arg1& a1, const Arg2& a2) { if (bIsActive) h->Fill(a1, a2, fHistoWrapper.getWeight()); }
+    template<typename Arg1, typename Arg2> void Fill(const Arg1& a1, const Arg2& a2) { if (isActive()) h->Fill(a1, a2, fHistoWrapper.getWeight()); }
     /// Fills histogram (if it exists) with custom event weight
-    template<typename Arg1, typename Arg2, typename Arg3> void Fill(const Arg1& a1, const Arg2& a2, const Arg3& a3) { if (bIsActive) h->Fill(a1, a2, a3); }
+    template<typename Arg1, typename Arg2, typename Arg3> void Fill(const Arg1& a1, const Arg2& a2, const Arg3& a3) { if (isActive()) h->Fill(a1, a2, a3); }
 
   private:
     HistoWrapper& fHistoWrapper;
     TH2* h;
-    bool bIsActive;
+    HistoWrapper::HistoLevel fLevel;
   };
 
   /// Wrapper class for TH3 object
   class WrappedTH3 {
   public:
-    WrappedTH3(HistoWrapper& histoWrapper, TH3* histo, bool isActive);
+    WrappedTH3(HistoWrapper& histoWrapper, TH3* histo, HistoWrapper::HistoLevel level);
     ~WrappedTH3();
 
     /// Returns true if the histogram exists
-    bool isActive() const { return bIsActive; }
+    bool isActive() const { return fHistoWrapper.isActive(fLevel); }
     /// Returns pointer to the histogram (Note: it can be a zero pointer if the histogram is not active)
     TH3* getHisto() { return h; }
     /// Returns the x axis of the histogram for bin label modification
     TAxis* GetXaxis() { return h->GetXaxis(); }
     /// Fills histogram (if it exists) with event weight
-    template<typename Arg1, typename Arg2> void Fill(const Arg1& a1, const Arg2& a2) { if (bIsActive) h->Fill(a1, a2, fHistoWrapper.getWeight()); }
+    template<typename Arg1, typename Arg2> void Fill(const Arg1& a1, const Arg2& a2) { if (isActive()) h->Fill(a1, a2, fHistoWrapper.getWeight()); }
     /// Fills histogram (if it exists) with custom event weight
-    template<typename Arg1, typename Arg2, typename Arg3> void Fill(const Arg1& a1, const Arg2& a2, const Arg3& a3) { if (bIsActive) h->Fill(a1, a2, a3); }
+    template<typename Arg1, typename Arg2, typename Arg3> void Fill(const Arg1& a1, const Arg2& a2, const Arg3& a3) { if (isActive()) h->Fill(a1, a2, a3); }
 
   private:
     HistoWrapper& fHistoWrapper;
     TH3* h;
-    bool bIsActive;
+    HistoWrapper::HistoLevel fLevel;
   };
 
   //////////////////////////////////////// Implementations of inline/template functions
@@ -144,17 +148,14 @@ namespace HPlus {
            typename Arg5>
   WrappedTH1* HistoWrapper::makeTH(HistoLevel level, TFileDirectory& fd, const Arg1& a1, const Arg2& a2, const Arg3& a3,
                                    const Arg4& a4, const Arg5& a5) {
+    T* histo = 0;
     if (level <= fAmbientLevel) {
       // making a separate directory for debug histograms is not that easy here;
       // it should be done at the level of code which is calling this command
-      T* histo = fd.make<T>(a1, a2, a3, a4, a5);
+      histo = fd.make<T>(a1, a2, a3, a4, a5);
       histo->Sumw2();
-      fAllTH1Histos.push_back(new WrappedTH1(*this, histo, true));
-    } else {
-      // Histogram is suppressed
-      TH1* histo = 0;
-      fAllTH1Histos.push_back(new WrappedTH1(*this, histo, false));
     }
+    fAllTH1Histos.push_back(new WrappedTH1(*this, histo, level));
     return fAllTH1Histos.at(fAllTH1Histos.size()-1);
   }
 
@@ -162,15 +163,12 @@ namespace HPlus {
            typename Arg5, typename Arg6, typename Arg7, typename Arg8>
   WrappedTH2* HistoWrapper::makeTH(HistoLevel level, TFileDirectory& fd, const Arg1& a1, const Arg2& a2, const Arg3& a3,
                      const Arg4& a4, const Arg5& a5, const Arg6& a6, const Arg7& a7, const Arg8& a8) {
+    T* histo = 0;
     if (level <= fAmbientLevel) {
-      T* histo = fd.make<T>(a1, a2, a3, a4, a5, a6, a7, a8);
+      histo = fd.make<T>(a1, a2, a3, a4, a5, a6, a7, a8);
       histo->Sumw2();
-      fAllTH2Histos.push_back(new WrappedTH2(*this, histo, true));
-    } else {
-      // Histogram is suppressed
-      TH2* histo = 0;
-      fAllTH2Histos.push_back(new WrappedTH2(*this, histo, false));
     }
+    fAllTH2Histos.push_back(new WrappedTH2(*this, histo, level));
     return fAllTH2Histos.at(fAllTH2Histos.size()-1);
   }
 
@@ -180,15 +178,12 @@ namespace HPlus {
   WrappedTH3* HistoWrapper::makeTH(HistoLevel level, TFileDirectory& fd, const Arg1& a1, const Arg2& a2, const Arg3& a3,
                      const Arg4& a4, const Arg5& a5, const Arg6& a6, const Arg7& a7, const Arg8& a8,
                      const Arg9& a9, const Arg10& a10, const Arg11& a11) {
+    T* histo = 0;
     if (level <= fAmbientLevel) {
-      T* histo = fd.make<T>(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11);
+      histo = fd.make<T>(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11);
       histo->Sumw2();
-      fAllTH3Histos.push_back(new WrappedTH3(*this, histo, true));
-    } else {
-      // Histogram is suppressed
-      TH3* histo = 0;
-      fAllTH3Histos.push_back(new WrappedTH3(*this, histo, false));
     }
+    fAllTH3Histos.push_back(new WrappedTH3(*this, histo, level));
     return fAllTH3Histos.at(fAllTH3Histos.size()-1);
   }
 
