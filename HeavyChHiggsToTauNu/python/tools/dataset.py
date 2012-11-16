aca824aec15c7dc614ba081d32e24314a8014b51
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2012-11-16 17:53:11 +0200

    bugfix: in dataset.py

--- HeavyChHiggsToTauNu/python/tools/dataset.py
+++ HeavyChHiggsToTauNu/python/tools/dataset.py
@@ -2338,7 +2338,7 @@ class SelectorArgs:
         return copy.deepcopy(self)
 
     def update(self, selectorArgs):
-        for a in self.options:
+        for a, dv in self.optionsDefaultValues:
             val = getattr(selectorArgs, a)
             if val is not None:
                 setattr(self, a, val)
