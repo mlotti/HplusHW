a77eec5dac3c11ffb4e4a101b5a8737a7798c59b
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-01-14 10:47:20 +0200

    Added possibility to not use W+Njets weighting in crosssection.setBackgroundCrossSections()

--- HeavyChHiggsToTauNu/python/tools/crosssection.py
+++ HeavyChHiggsToTauNu/python/tools/crosssection.py
@@ -131,9 +131,22 @@ backgroundCrossSections = CrossSectionList(
             }),
 )
 
-def setBackgroundCrossSections(datasets):
+## Set background process cross sections
+#
+# \param datasets            dataset.DatasetManager object
+# \param doWNJetsWeighting   Set W+Njets cross sections according to the weighting scheme
+def setBackgroundCrossSections(datasets, doWNJetsWeighting=True):
     for dset in datasets.getMCDatasets():
         value = backgroundCrossSections.crossSection(dset.getName(), dset.getEnergy())
+        if value is None:
+            for wnJets in ["W1Jets", "W2Jets", "W3Jets", "W4Jets"]:
+                if wnJets in dset.getName():
+                    inclusiveCrossSection = backgroundCrossSections.crossSection("WJets", dset.getEnergy())
+                    inclusiveLO = backgroundCrossSections.crossSection("PREP_WJets", dset.getEnergy())
+                    wnJetsLO = backgroundCrossSections.crossSection("PREP_"+wnJets, dset.getEnergy())
+                    value = inclusiveCrossSection * wnJetsLO/inclusieveLO
+                    break
+
         if value is not None:
             dset.setCrossSection(value)
             print "Setting %s cross section to %f pb" % (dset.getName(), value)
