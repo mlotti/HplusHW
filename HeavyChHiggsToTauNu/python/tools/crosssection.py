0ff62fc0992e773d0250f170d3f041c7a0d30e53
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-01-14 18:31:28 +0200

    Bugfix in crosssection.py

--- HeavyChHiggsToTauNu/python/tools/crosssection.py
+++ HeavyChHiggsToTauNu/python/tools/crosssection.py
@@ -288,6 +288,7 @@ def setHplusCrossSections(datasets, tanbeta=20, mu=defaultMu, toTop=False):
 # \param mu        Mu parameter
 # \param energy    sqrt(s) in TeV as string
 def printHplusCrossSections(tanbetas=[10, 20, 30, 40], mu=defaultMu, energy="7"):
+    ttCrossSection = backgroundCrossSections.crossSection("TTJets", energy)
     print "ttbar cross section %.1f pb" % ttCrossSection
     print "mu %.1f" % mu
     print
