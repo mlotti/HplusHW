602e92016366666cd5915d27e8bad2c69439d663
Stefan Richter <stefan.richter@cern.ch>
2013-01-31 17:11:17 +0100

    Trying to commit EventClassification.h in a different way

---
+++ HeavyChHiggsToTauNu/interface/EventClassification.h
@@ -0,0 +1,45 @@
+// -*- c++ -*-
+#ifndef HiggsAnalysis_HeavyChHiggsToTauNu_EventClassification_h
+#define HiggsAnalysis_HeavyChHiggsToTauNu_EventClassification_h
+
+
+/* #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/BaseSelection.h" */
+
+/* #include "FWCore/Framework/interface/Event.h" */
+/* #include "FWCore/Utilities/interface/InputTag.h" */
+/* #include "DataFormats/Common/interface/Ptr.h" */
+/* #include "DataFormats/Candidate/interface/Candidate.h" */
+/* #include "DataFormats/HepMCCandidate/interface/GenParticle.h" */
+/* #include "DataFormats/METReco/interface/GenMET.h" */
+/* #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/EventCounter.h" */
+
+/* namespace edm { */
+/*   class ParameterSet; */
+/*   class Event; */
+/*   class EventSetup; */
+/* } */
+
+/* namespace HPlus { */
+/*   class HistoWrapper; */
+/*   class WrappedTH1; */
+
+/*   class EventClassification: public BaseSelection { */
+/*   public: */
+/*     class Data { */
+/*     public: */
+
+/*       Data(); */
+/*       Data(const GenParticl *analysis); */
+/*       ~Data(); */
+
+/*       void check() const; */
+
+/*       const edm::Ptr<reco::GenMET>& getGenMET() const { */
+/*         check(); */
+/*         return fAnalysis->fGenMet; */
+/*       } */
+/*     private: */
+/*       const GenParticleAnalysis *fAnalysis; */
+/*     }; */
+
+#endif
