be48df60473bcfd88d9ec2bb7f6c6a9b540adac7
Stefan Richter <stefan.richter@cern.ch>
2013-01-31 17:05:48 +0100

    Trying adding EventClassification.cc again... sorry, still learning how to use git

---
+++ HeavyChHiggsToTauNu/src/EventClassification.cc
@@ -0,0 +1,19 @@
+#include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/GenParticleAnalysis.h"
+
+
+/*
+About this code
+***************
+PURPOSE.
+Classify simulated events with a reconstructed b, tau, and MET seeming to come from a Hplus decay
+according to whether these objects were identified correctly or not by comparing to the MC truth.
+
+IMPLEMENTATION OUTLINE.
+
+
+This code is called from SignalAnalysis.cc
+*/
+
+/*
+Next step:
+*/
