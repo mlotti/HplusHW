d5cf9cb65944f90f727e90ba43cd11e7bc90fa38
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-02-20 09:47:42 +0200

    bugfix: Labels in tau/met trigger scale factor histograms

--- HeavyChHiggsToTauNu/src/TauTriggerEfficiencyScaleFactor.cc
+++ HeavyChHiggsToTauNu/src/TauTriggerEfficiencyScaleFactor.cc
@@ -21,7 +21,7 @@ namespace HPlus {
     WrappedTH1 *hsfu = histoWrapper.makeTH<TH1F>(HistoWrapper::kVital, dir, "ScaleFactorUncertainty", "Scale factor;Tau p_{T} bin;Scale factor uncertainty", fBinned.nbins()+1, 0, fBinned.nbins()+1);
     if(hsf->getHisto()) {
       hsf->getHisto()->SetBinContent(1, 1); hsf->GetXaxis()->SetBinLabel(1, "control");
-      hsfu->getHisto()->SetBinContent(1, 1); hsf->GetXaxis()->SetBinLabel(1, "control");
+      hsfu->getHisto()->SetBinContent(1, 1); hsfu->GetXaxis()->SetBinLabel(1, "control");
       for(size_t i=0; i<fBinned.nbins(); ++i) {
         size_t bin = i+2;
         snprintf(buf, NBUF, "%.0f", fBinned.binLowEdge(i));
