ac55027e23839137ccf5d742b16dd59c365574e9
Alexandros Attikis <attikis@jade.hip.fi>
2013-03-28 01:44:06 +0200

    TreeAnalysis: Removed beta cut from tree analysis

--- HeavyChHiggsToTauNu/test/treeAnalysis_cfg.py
+++ HeavyChHiggsToTauNu/test/treeAnalysis_cfg.py
@@ -39,7 +39,7 @@ def customize(treeAnalysis):
     treeAnalysis.Tree.fillJetEnergyFractions=False
     treeAnalysis.Tree.fillNonIsoLeptonVars=True
     # Apply beta cut for jets to reject PU jets
-    treeAnalysis.jetSelection.betaCut = 0.2 # Disable by setting to 0.0; if you want to enable, set to 0.2
+    #treeAnalysis.jetSelection.betaCut = 0.2 # Disable by setting to 0.0; if you want to enable, set to 0.2
 #    treeAnalysis.tauSelection.ptCut = 80.0 #
 #    treeAnalysis.MET.METCut = 100.0
 
