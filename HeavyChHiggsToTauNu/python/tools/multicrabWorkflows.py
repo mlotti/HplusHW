de72759ecea883f49e0a13d295cd2f56ceb9cb14
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-04-16 08:14:48 +0200

    datasets: Added v53_3_test2 HW120 pattuple
    The only difference (intentionally not committed in) is the MC
    GlobalTag. Used START53_V15 (in v53_2) instead of START53_V19PR (in
    v53_3_test1).

--- HeavyChHiggsToTauNu/python/tools/multicrabWorkflows.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabWorkflows.py
@@ -509,6 +509,7 @@ multicrabWorkflowsPattuple.addPattuple_v53_1_2(datasets)
 multicrabWorkflowsPattuple.addPattuple_v53_2_test1(datasets)
 multicrabWorkflowsPattuple.addPattuple_v53_2(datasets)
 multicrabWorkflowsPattuple.addPattuple_v53_3_test1(datasets)
+multicrabWorkflowsPattuple.addPattuple_v53_3_test2(datasets)
 
 # Add embedding definitions
 multicrabWorkflowsTauEmbedding.addEmbeddingAodAnalysis_44X(datasets)
