mlotti/HplusHW

commit 2500f0602052f51be12785addf1f15db182c0a9c

technical&bugfix: Added primary vertex filter step before analysis modules
The bug was that TES systematic variation requires a valid selected
primary vertex. This stems from the need of running our tau ID once
before the variation, and our tau ID needs the selected primary
vertex.

After some discussion with Lauri we came to the conclusion that the
cleanest fix would be to move the primary vertex filtering step to be
run before the analysis modules and systematic variations.

This is implemented such that the filtering is applied to all analyses
using the AnalysisConfiguration module. For other analyses, the
situation should look the same as before.

HeavyChHiggsToTauNu/python/HChPatTuple.py
@@ -48,6 +48,7 @@ class PATBuilder:
                  patArgs={},
                  doTotalKinematicsFilter=False,
                  doHBHENoiseFilter=True, doPhysicsDeclared=False,
+                 selectedPrimaryVertexFilter=False,
                  calculateEventCleaning=False):
 
         self.process = process
@@ -80,10 +81,10 @@ class PATBuilder:
             # Add primary vertex selection
             # Selects the first primary vertex, applies the quality cuts to it
             # Applies quality cuts to all vertices too
-            HChPrimaryVertex.addPrimaryVertexSelection(process, sequence)
+            self.counters.extend(HChPrimaryVertex.addPrimaryVertexSelection(process, sequence, filter=selectedPrimaryVertexFilter))
             if options.tauEmbeddingInput != 0:
                 # for embedding input, do vertex object selection for original event too
-                HChPrimaryVertex.addPrimaryVertexSelection(process, sequence, srcProcess=dataVersion.getRecoProcess(), postfix="Original")
+                self.counters.extend(HChPrimaryVertex.addPrimaryVertexSelection(process, sequence, srcProcess=dataVersion.getRecoProcess(), postfix="Original", filter=selectedPrimaryVertexFilter))
 
             if options.doTauHLTMatchingInAnalysis != 0:
                 raise Exception("doTauLHTMatchingInAnalysis is not supported at the moment")
@@ -119,6 +120,10 @@ class PATBuilder:
             self.process.patSequence = self.addPat(dataVersion, patArgs=pargs, pvSelectionConfig=options.pvSelectionConfig)
         sequence *= self.process.eventPreSelection
         sequence *= self.process.patSequence
+
+        # Selects the first primary vertex, applies the quality cuts to it
+        # Applies quality cuts to all vertices too
+        self.counters.extend(HChPrimaryVertex.addPrimaryVertexSelection(process, sequence, filter=selectedPrimaryVertexFilter))
 
         # Add event filters if requested
         self.addFilters(dataVersion, self.process.eventPreSelection, doTotalKinematicsFilter, doHBHENoiseFilter, doPhysicsDeclared, patOnTheFly=True)
@@ -929,11 +934,6 @@ def addStandardPAT(process, dataVersion, doPatTrigger=True, doChsJets=True, patA
                 ])
 
         sequence *= process.primaryVertexSelectionSequence
-
-    # Selects the first primary vertex, applies the quality cuts to it
-    # Applies quality cuts to all vertices too
-    HChPrimaryVertex.addPrimaryVertexSelection(process, sequence)
-
 
     # Adjust output commands
     if hasOut:
@@ -2118,11 +2118,6 @@ def addPF2PAT(process, dataVersion, doPatTrigger=True, doChs=False, patArgs={}, 
                 ])
 
         sequence *= process.primaryVertexSelectionSequence
-
-    # Selects the first primary vertex, applies the quality cuts to it
-    # Applies quality cuts to all vertices too
-    HChPrimaryVertex.addPrimaryVertexSelection(process, sequence)
-
 
     # Adjust output commands
     if out != None:

HeavyChHiggsToTauNu/python/AnalysisConfiguration.py
@@ -489,7 +489,7 @@ class ConfigBuilder:
 
         # Fragment to run PAT on the fly if requested from command line
         from HiggsAnalysis.HeavyChHiggsToTauNu.HChPatTuple import addPatOnTheFly
-        process.commonSequence, additionalCounters = addPatOnTheFly(process, self.options, self.dataVersion)
+        process.commonSequence, additionalCounters = addPatOnTheFly(process, self.options, self.dataVersion, selectedPrimaryVertexFilter=True)
 
         # Add configuration information to histograms.root
         from HiggsAnalysis.HeavyChHiggsToTauNu.HChTools import addConfigInfo

HeavyChHiggsToTauNu/python/HChPrimaryVertex.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 
-def addPrimaryVertexSelection(process, sequence, srcProcess="", postfix=""):
+def addPrimaryVertexSelection(process, sequence, srcProcess="", postfix="", filter=False):
     m = cms.EDProducer("HPlusFirstVertexSelector",
         src = cms.InputTag("offlinePrimaryVertices", "", srcProcess)
     )
@@ -19,10 +19,27 @@ def addPrimaryVertexSelection(process, sequence, srcProcess="", postfix=""):
     setattr(process, "goodPrimaryVertices"+postfix, m)
     sequence *= m
 
-#    process.selectedPrimaryVertexFilter = cms.EDFilter("VertexCountFilter",
-#                                                       src = cms.InputTag("selectedPrimaryVertex"),
-#                                                       minNumber = cms.uint32(1),
-#                                                       maxNumber = cms.uint32(999)
-#                                                       )
-#    sequence *= process.selectedPrimaryVertexFilter
-    
+    ret = []
+    if filter:
+        m = cms.EDProducer("EventCountProducer")
+        name = "primaryVertexAllEvents"+postfix
+        setattr(process, name, m)
+        sequence *= m
+        ret.append(name)
+
+        m = cms.EDFilter("VertexCountFilter",
+            src = cms.InputTag("selectedPrimaryVertex"+postfix),
+            minNumber = cms.uint32(1),
+            maxNumber = cms.uint32(999)
+        )
+        setattr(process, "selectedPrimaryVertexFilter"+postfix, m)
+        sequence *= m
+
+        m = cms.EDProducer("EventCountProducer")
+        name = "primaryVertexSelected"+postfix
+        setattr(process, name, m)
+        sequence *= m
+        ret.append(name)
+
+    return ret
+

commit a832b0df2856ee7d1932cea5c08d043c2137bc81

bugfix: Fixed the order of vertex selection and PAT sequences
E.g. the data quality filters assume goodPrimaryVertices collection.

The bug was introduced in 2500f0602052f51be12785addf1f15db182c0a9c.

HeavyChHiggsToTauNu/python/HChPatTuple.py
@@ -122,11 +122,14 @@ class PATBuilder:
 
             self.process.patSequence = self.addPat(dataVersion, patArgs=pargs, pvSelectionConfig=options.pvSelectionConfig)
         sequence *= self.process.eventPreSelection
-        sequence *= self.process.patSequence
 
         # Selects the first primary vertex, applies the quality cuts to it
         # Applies quality cuts to all vertices too
+        # Must be done before PAT sequence
         self.counters.extend(HChPrimaryVertex.addPrimaryVertexSelection(process, sequence, filter=selectedPrimaryVertexFilter))
+
+        # Add PAT sequence
+        sequence *= self.process.patSequence
 
         # Add event filters if requested
         self.addFilters(dataVersion, self.process.eventPreSelection, doTotalKinematicsFilter, doHBHENoiseFilter, doPhysicsDeclared, patOnTheFly=True)