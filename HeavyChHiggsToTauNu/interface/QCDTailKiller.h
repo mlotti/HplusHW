ae27e9baebe9ca2923a0e41348d5ebf819fbc87f
Alexandros Attikis <attikis@jade.hip.fi>
2013-04-26 16:50:25 +0300

    QCDMeasurement: Added Tail Killer variables - Radius for back-to-back and collinear variables for n jets. Also added boolean to customise Tail Killer in python cfg file

--- HeavyChHiggsToTauNu/interface/QCDTailKiller.h
+++ HeavyChHiggsToTauNu/interface/QCDTailKiller.h
@@ -47,6 +47,7 @@ namespace HPlus {
       const double getDeltaPhiTauMET() const { return fDeltaPhiTauMET; }
       const double getDeltaPhiJetMET(int njet) const;
       const double getRadiusFromBackToBackCorner(int njet) const { return std::sqrt(std::pow(180.-getDeltaPhiTauMET(),2)+std::pow(getDeltaPhiJetMET(njet),2)); }
+      const double getRadiusFromCollinearCorner(int njet) const { return std::sqrt(std::pow(getDeltaPhiTauMET(),2)+std::pow(180.-getDeltaPhiJetMET(njet),2)); }
       const bool passBackToBackCutForJet(int njet) const;
       const bool passCollinearCutForJet(int njet) const;
 
