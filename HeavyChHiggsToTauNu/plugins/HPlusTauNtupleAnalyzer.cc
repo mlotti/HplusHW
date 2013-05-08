a8a3157fe6bb19241752de5014f96a1108d2778e
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-05-08 09:59:07 +0300

    analysis[embedding]: Added genTauDebugAnalysisNtuple

--- HeavyChHiggsToTauNu/plugins/HPlusTauNtupleAnalyzer.cc
+++ HeavyChHiggsToTauNu/plugins/HPlusTauNtupleAnalyzer.cc
@@ -16,16 +16,23 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
+#include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/EventCounter.h"
+#include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/EventWeight.h"
+#include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/HistoWrapper.h"
+
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeEventBranches.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeFunctionBranch.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/EventItem.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeTauBranches.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeJetBranches.h"
+#include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeMuonBranches.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeVertexBranches.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeTriggerBranches.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeGenBranches.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeGenParticleBranches.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TreeGenTauBranches.h"
+
+#include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/GenParticleTools.h"
 
 #include "TTree.h"
 
@@ -39,6 +46,9 @@ public:
 
 private:
   void analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup);
+  void endLuminosityBlock(const edm::LuminosityBlock& iBlock, const edm::EventSetup & iSetup);
+  void endJob();
+
   void reset();
 
   typedef math::XYZTLorentzVector XYZTLorentzVector;
@@ -47,6 +57,10 @@ private:
 
   typedef HPlus::EventItem<XYZTLorentzVector> MetItem;
   typedef HPlus::EventItem<double> DoubleItem;
+
+  HPlus::EventWeight fEventWeight;
+  HPlus::HistoWrapper fHistoWrapper;
+  HPlus::EventCounter eventCounter;
 
   edm::InputTag fGenParticleTauSrc;
   edm::InputTag fSelectedPrimaryVertexSrc;
@@ -60,16 +74,24 @@ private:
   HPlus::TreeTauBranches fTauBranches;
   HPlus::TreeJetBranches fJetBranches;
 
+  const bool fMuonEnabled;
+  HPlus::TreeMuonBranches fMuonBranches;
+
   HPlus::TreeGenTauBranches fGenTaus;
+
+  const bool fGenTTBarEnabled;
+  HPlus::TreeGenParticleBranches fGenTTBarBranches;
 
   std::vector<MetItem> fMets;
   std::vector<DoubleItem> fDoubles;
 
-  int fNSelectedPrimaryVertices;
-  int fNGoodPrimaryVertices;
+  HPlus::Count cAllEvents;
 };
 
 HPlusTauNtupleAnalyzer::HPlusTauNtupleAnalyzer(const edm::ParameterSet& iConfig):
+  fEventWeight(iConfig),
+  fHistoWrapper(fEventWeight, iConfig.getUntrackedParameter<std::string>("histogramAmbientLevel")),
+  eventCounter(iConfig, fEventWeight, fHistoWrapper),
   fGenParticleTauSrc(iConfig.getParameter<edm::InputTag>("genParticleTauSrc")),
   fGenBranches(iConfig),
   fSelectedVertexBranches(iConfig, "selectedPrimaryVertex", "selectedPrimaryVertexSrc"),
@@ -77,7 +99,12 @@ HPlusTauNtupleAnalyzer::HPlusTauNtupleAnalyzer(const edm::ParameterSet& iConfig)
   fTriggerBranches(iConfig),
   fTauBranches(iConfig),
   fJetBranches(iConfig, false),
-  fGenTaus("gentaus")
+  fMuonEnabled(iConfig.getParameter<bool>("muonEnabled")),
+  fMuonBranches(iConfig),
+  fGenTaus("gentaus"),
+  fGenTTBarEnabled(iConfig.getParameter<bool>("genTTBarEnabled")),
+  fGenTTBarBranches("genttbarwdecays"),
+  cAllEvents(eventCounter.addCounter("All events"))
 {
 
   edm::ParameterSet pset = iConfig.getParameter<edm::ParameterSet>("mets");
@@ -103,7 +130,13 @@ HPlusTauNtupleAnalyzer::HPlusTauNtupleAnalyzer(const edm::ParameterSet& iConfig)
   fTauBranches.book(fTree);
   fJetBranches.book(fTree);
 
+  if(fMuonEnabled)
+    fMuonBranches.book(fTree);
+
   fGenTaus.book(fTree);
+
+  if(fGenTTBarEnabled)
+    fGenTTBarBranches.book(fTree);
 
   for(size_t i=0; i<fMets.size(); ++i) {
     fTree->Branch(fMets[i].name.c_str(), &(fMets[i].value));
@@ -114,6 +147,13 @@ HPlusTauNtupleAnalyzer::HPlusTauNtupleAnalyzer(const edm::ParameterSet& iConfig)
 }
 
 HPlusTauNtupleAnalyzer::~HPlusTauNtupleAnalyzer() {}
+
+void HPlusTauNtupleAnalyzer::endLuminosityBlock(const edm::LuminosityBlock& iBlock, const edm::EventSetup & iSetup) {
+  eventCounter.endLuminosityBlock(iBlock, iSetup);
+}
+void HPlusTauNtupleAnalyzer::endJob() {
+  eventCounter.endJob();
+}
 
 void HPlusTauNtupleAnalyzer::reset() {
   double nan = std::numeric_limits<double>::quiet_NaN();
@@ -126,7 +166,10 @@ void HPlusTauNtupleAnalyzer::reset() {
   fTauBranches.reset();
   fJetBranches.reset();
 
+  fMuonBranches.reset();
+
   fGenTaus.reset();
+  fGenTTBarBranches.reset();
 
   for(size_t i=0; i<fMets.size(); ++i) {
     fMets[i].value.SetXYZT(nan, nan, nan, nan);
@@ -134,20 +177,23 @@ void HPlusTauNtupleAnalyzer::reset() {
   for(size_t i=0; i<fDoubles.size(); ++i) {
     fDoubles[i].value = nan;
   }
-
-  fNSelectedPrimaryVertices = -1;
-  fNGoodPrimaryVertices = -1;
 }
 
 void HPlusTauNtupleAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  fEventWeight.beginEvent();
+  increment(cAllEvents);
+
   fEventBranches.setValues(iEvent);
   fSelectedVertexBranches.setValues(iEvent);
   fGoodVertexBranches.setValues(iEvent);
   fTriggerBranches.setValues(iEvent);
 
+  edm::Handle<edm::View<reco::GenParticle> > hgenparticles;
   edm::Handle<edm::View<reco::GenParticle> > hgenparticlestau;
-  if(!iEvent.isRealData())
+  if(!iEvent.isRealData()) {
+    iEvent.getByLabel(fGenBranches.getInputTag(), hgenparticles);
     iEvent.getByLabel(fGenParticleTauSrc, hgenparticlestau);
+  }
 
   // Taus
   if(iEvent.isRealData()) {
@@ -162,9 +208,26 @@ void HPlusTauNtupleAnalyzer::analyze(const edm::Event& iEvent, const edm::EventS
         fGenTaus.addValue(&(*iGen));
       }
     }
+
+    if(fGenTTBarEnabled) {
+
+      std::vector<const reco::GenParticle *> ttbarDecays = HPlus::GenParticleTools::findTTBarWdecays(hgenparticles->ptrVector());
+      for(size_t i=0; i<ttbarDecays.size(); ++i) {
+        fGenTTBarBranches.addValue(ttbarDecays[i]);
+      }
+    }
   }
 
   fJetBranches.setValues(iEvent);
+
+  if(fMuonEnabled) {
+    if(iEvent.isRealData()) {
+      fMuonBranches.setValues(iEvent);
+    }
+    else {
+      fMuonBranches.setValues(iEvent, *hgenparticles);
+    }
+  }
 
   for(size_t i=0; i<fMets.size(); ++i) {
     edm::Handle<edm::View<reco::MET> > hmet;
