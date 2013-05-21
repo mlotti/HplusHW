8ae6682a85a546d59edd3172db85c69ce7c00f3d
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-05-21 14:58:20 +0300

    technical: Do QCD tail killer scenarios for optimization variations too
    Lauri requested this feature.

--- HeavyChHiggsToTauNu/python/AnalysisConfiguration.py
+++ HeavyChHiggsToTauNu/python/AnalysisConfiguration.py
@@ -376,6 +376,7 @@ class ConfigBuilder:
         self._printModule(analysisModules[0])
 
         # Construct normal path
+        analysisNamesForTailKillerScenarios = analysisNames
         if not self.doOptimisation:
             for module, name in zip(analysisModules, analysisNames):
                 setattr(process, name, module)
@@ -397,10 +398,11 @@ class ConfigBuilder:
             for module, name in zip(analysisModules, analysisNames):
                 names = self.optimisationScheme.generateVariations(process, additionalCounters, process.commonSequence, module, name)
                 self._accumulateAnalyzers("Optimisation", names)
+                analysisNamesForTailKillerScenarios = names
                 analysisNamesForSystematics.extend(names)
 
-        # QCD tail killer scenarios
-        analysisNamesForSystematics.extend(self._buildQCDTailKillerScenarios(process, analysisModules, analysisNames))
+        # QCD tail killer scenarios (do them also for optimisation variations)
+        analysisNamesForSystematics.extend(self._buildQCDTailKillerScenarios(process, analysisNamesForTailKillerScenarios))
 
         # Against electron scan
         self._buildAgainstElectronScan(process, analysisModules, analysisNames)
@@ -632,9 +634,8 @@ class ConfigBuilder:
     ## Build array of analyzers to scan various QCD tail killer scenarios
     #
     # \param process          cms.Process object
-    # \param analysisModules  List of analysis modules to be used as prototypes
     # \param analysisNames    List of analysis module names
-    def _buildQCDTailKillerScenarios(self, process, analysisModules, analysisNames):
+    def _buildQCDTailKillerScenarios(self, process, analysisNames):
         def createQCDTailKillerModule(process, modulePrefix, mod, names, modules):
             modName = name+"Opt"+modulePrefix
             if "Opt" in name:
@@ -651,7 +652,8 @@ class ConfigBuilder:
         from HiggsAnalysis.HeavyChHiggsToTauNu.HChSignalAnalysisParameters_cff import QCDTailKillerBin
         names = []
         modules = []
-        for module, name in zip(analysisModules, analysisNames):
+        for name in analysisNames:
+            module = getattr(process, name)
             # Zero plus scenario
             mod = module.clone()
             mod.QCDTailKiller.backToBack = cms.untracked.VPSet(
@@ -717,8 +719,6 @@ class ConfigBuilder:
             mod.QCDTailKiller.maxJetsToConsider = 4
             createQCDTailKillerModule(process, "QCDTailKillerTightPlus", mod, names, modules)
         self._accumulateAnalyzers("Modules for QCDTailKiller scenarios", names)
-        analysisModules.extend(modules)
-        analysisNames.extend(names)
 
         return names
 
@@ -766,8 +766,6 @@ class ConfigBuilder:
                         path = cms.Path(process.commonSequence * mod)
                         setattr(process, modName+"Path", path)
         self._accumulateAnalyzers("AgainstElectron/AgainstMuon scan", names)
-        analysisModules.extend(modules)
-        analysisNames.extend(names)
 
     ## Build array of analyzers to scan various tau isolation and jet PU ID discriminators
     #
@@ -813,8 +811,6 @@ class ConfigBuilder:
                         path = cms.Path(process.commonSequence * mod)
                         setattr(process, modName+"Path", path)
         self._accumulateAnalyzers("TauIsolation/JetPUID scan", names)
-        analysisModules.extend(modules)
-        analysisNames.extend(names)
 
     ## Build array of analyzers to scan various tau isolation and jet PU ID discriminators
     #
@@ -853,8 +849,6 @@ class ConfigBuilder:
                 path = cms.Path(process.commonSequence * mod)
                 setattr(process, modName+"Path", path)
         self._accumulateAnalyzers("btag efficiency scan", names)
-        analysisModules.extend(modules)
-        analysisNames.extend(names)
 
     ## Build "tau embedding"-like preselection for normal MC
     #
