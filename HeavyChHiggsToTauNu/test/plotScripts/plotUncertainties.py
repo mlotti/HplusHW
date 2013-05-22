2ef1f45318384dc9b6bf0004ea8c6feec26610bf
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-05-22 11:30:32 +0300

    plotting: Updated plotUncertainties.py

--- HeavyChHiggsToTauNu/test/plotScripts/plotUncertainties.py
+++ HeavyChHiggsToTauNu/test/plotScripts/plotUncertainties.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import math
+
 import ROOT
 ROOT.gROOT.SetBatch(True)
 
@@ -19,9 +21,6 @@ mcOnlyLumi = 5000 # pb
 
 def main():
     creator = dataset.readFromMulticrabCfg()
-    print creator.getSystematicVariations()
-    print creator.getSystematicVariationSources()
-
     datasets = creator.createDatasetManager(dataEra=dataEra)
 
     if not mcOnly:
@@ -44,27 +43,36 @@ def main():
             dset.setCrossSection(1.0)
 
     plots.mergeWHandHH(datasets)
+    datasets.merge("EWK", ["TTJets",
+                           "WJets",
+                           "DYJetsToLL",
+                           "SingleTop", "Diboson"
+                           ], keepSources=True)
 
     style = tdrstyle.TDRStyle()
 
     # Setup the systematics recipe object
-    systematics = dataset.Systematics(shapes=["SystVarJER", "SystVarJES", "SystVarMET", "SystVarTES"],
-                                      #verbose=True,
-                                      additionalNormalizations={"Foo": 0.2, "Bar": 0.05},
+    systematics = dataset.Systematics(
+        allShapes=True,
+        #shapes=["SystVarJER", "SystVarJES", "SystVarMET", "SystVarTES"],
+        #verbose=True,
+        #additionalNormalizations={"Foo": 0.2, "Bar": 0.05},
                                       )
 
     def dop(datasetName):
         doPlots(datasets, datasetName, systematics)
-        doCounters(datasets, datasetName)
+        doCounters(datasets, datasetName, creator.getSystematicVariationSources())
 
-    dop("TTToHplus_M80")
+#    dop("TTToHplus_M80")
     dop("TTToHplus_M120")
-    dop("TTToHplus_M160")
-    dop("HplusTB_M180")
+#    dop("TTToHplus_M160")
+#    dop("HplusTB_M180")
     dop("HplusTB_M200")
-    dop("HplusTB_M300")
+#    dop("HplusTB_M300")
     dop("TTJets")
+#    dop("DYJetsToLL")
     dop("WJets")
+    dop("EWK")
 
 drawPlot = plots.PlotDrawer()
 
@@ -81,7 +89,7 @@ def doPlots(datasets, datasetName, systematics):
 
     drawPlot.setDefaults(xlabel="m_{T}(#tau, E_{T}^{miss}) (GeV)", ylabel="dN/dm_{T} (%.0f-%.0f GeV)",
                          rebinX=ptBinning, divideByBinWidth=True,
-                         ratio=True,
+                         ratio=True, addLuminosityText=True,
                          opts2={"ymin": 0.8, "ymax": 1.2})
 
     drh = dset.getDatasetRootHisto(systematics.histogram("transverseMass"))
@@ -108,15 +116,88 @@ def doPlots(datasets, datasetName, systematics):
                 histograms.Histo(tpl[0], "Plus"),# drawStyle="L"),
                 histograms.Histo(tpl[1], "Minus"),# drawStyle="L")
                 ])
+        plot.setLuminosity(mcOnlyLumi)
         plot.histoMgr.forEachHisto(styles.Generator(styleList))
         plot.appendPlotObject(histograms.PlotText(x=0.7, y=0.7, text=datasetName, size=17))
         plot.appendPlotObject(histograms.PlotText(x=0.7, y=0.65, text=source, size=17))
-        drawPlot(plot, "%s_%s" % (datasetName, source))
+        if "TTToHplus_" in datasetName:
+            plot.appendPlotObject(histograms.PlotText(x=0.7, y=0.6, text="#it{B}(H^{+}#rightarrow#tau#nu)=0.01", size=17))
+        elif "HplusTB_" in datasetName:
+            plot.appendPlotObject(histograms.PlotText(x=0.7, y=0.6, text="#sigma#times#it{B}=1 pb", size=17))
+        args = {}
+        if source == "SystVarTauTrgSF":
+            args["opts2"] = {"ymin": 0.2, "ymax": 1.8}
+        drawPlot(plot, "%s_%s" % (datasetName, source), **args)
     
 
-def doCounters(datasets, datasetName):
-    pass
+def doCounters(datasets, datasetName, systematicVariationSources):
+    dset = datasets.getDataset(datasetName)
 
+    countName = "Selected events"
+    def getCount(ec):
+        return ec.getMainCounterTable().getCount(colName=datasetName, rowName=countName)
+    def createEC(**kwargs):
+        ec = counter.EventCounter([dset], **kwargs)
+        ec.normalizeMCToLuminosity(mcOnlyLumi)
+        return ec
+
+    ecNominal = createEC()
+
+    countNominal = getCount(ecNominal)
+
+    print "Dataset %s" % datasetName
+    print "Systematics from variations:"
+    for source in systematicVariationSources:
+        ecPlus = createEC(analysisPostfix=source+"Plus")
+        ecMinus = createEC(analysisPostfix=source+"Minus")
+
+        countPlus = getCount(ecPlus)
+        countMinus = getCount(ecMinus)
+    
+        diffPlus = countPlus.value() - countNominal.value()
+        diffMinus = countNominal.value() - countMinus.value()
+
+        relPlus = (diffPlus / countNominal.value()) * 100
+        relMinus = (diffMinus / countNominal.value()) * 100
+
+        print "  %s: %.2f + %.2f - %.2f (+ %.2f - %.2f %%)" % (source, countNominal.value(), diffPlus, diffMinus, relPlus, relMinus)
+
+    print "Systematics from propagation:"
+    scaleFactors = ["BtagScaleFactor", "TauTriggerScaleFactor"]
+    def getSum(drh):
+        s = 0.0
+        th1 = drh.getHistogram()
+        for i in xrange(1, th1.GetNbinsX()+1):
+            s += (th1.GetBinContent(i)*th1.GetBinCenter(i))**2
+        return s
+    def getCount(drh):
+        return drh.getHistogram().GetBinContent(1)
+
+    for sf in scaleFactors:
+        drhAbsUnc = dset.getDatasetRootHisto("ScaleFactorUncertainties/%sAbsUncert_AfterSelection"%sf)
+        drhCount = dset.getDatasetRootHisto("ScaleFactorUncertainties/%sAbsUncertCounts_AfterSelection"%sf)
+        
+        sums = drhAbsUnc.forEach(getSum)
+        countsRaw = drhCount.forEach(getCount)
+
+        drhCount.normalizeToLuminosity(mcOnlyLumi)
+        countsNorm = drhCount.forEach(getCount)
+
+        sigmaSquared = 0.0
+        countNormSum = 0.0
+        for squareSum, countRaw, countNorm in zip(sums, countsRaw, countsNorm):
+            if countRaw == 0.0:
+                continue
+
+            relUnc = math.sqrt(squareSum)/countRaw
+            absUnc = relUnc * countNorm
+            sigmaSquared += absUnc**2
+            countNormSum += countNorm
+
+        sigma = math.sqrt(sigmaSquared)
+        sigmaRel = sigma/countNormSum * 100
+        print "  %s: %.2f +- %.2f (+- %.2f %%)" % (sf, countNormSum, sigma, sigmaRel)
+    print
 
 if __name__ == "__main__":
     main()
