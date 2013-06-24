978e7b853e229cb31ab484d5bac9bc15b45fa29a
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-06-24 11:39:21 +0300

    analysis[embedding]: Updated muonAnalysisFromSkim_cfg.py

--- HeavyChHiggsToTauNu/test/tauEmbedding/muonAnalysisFromSkim_cfg.py
+++ HeavyChHiggsToTauNu/test/tauEmbedding/muonAnalysisFromSkim_cfg.py
@@ -20,6 +20,9 @@ options.register("WDecaySeparate",
                  options.varType.int,
                  "Separate W decays from MC information")
 options, dataVersion = getOptionsDataVersion(dataVersion, options, useDefaultSignalTrigger=False)
+if dataVersion.isMC() and len(options.trigger) == 0:
+    options.trigger = ["HLT_Mu40_eta2p1_v1"]
+
 
 #options.doPat=1
 
@@ -41,11 +44,13 @@ process.source = cms.Source('PoolSource',
         # For testing in jade
         #dataVersion.getAnalysisDefaultFileMadhatter()
 #        "file:skim.root"
-        "/store/group/local/HiggsChToTauNuFullyHadronic/tauembedding/CMSSW_4_4_X/TTJets_TuneZ2_Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/Tauembedding_skim_v44_1_TTJets_TuneZ2_Fall11//2f6341f5a210122b891e378fe7516bcf/skim_1001_1_qUS.root"
+        "/store/group/local/HiggsChToTauNuFullyHadronic/tauembedding/CMSSW_4_4_X/TTJets_TuneZ2_Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/Fall11_PU_S6_START44_V9B_v1_AODSIM_tauembedding_skim_v44_5_1/29c7b20153e31b2c86faa8316de20ff4/skim_997_1_NUn.root"
   )
 )
 if dataVersion.isData():
-    process.source.fileNames = ["/store/group/local/HiggsChToTauNuFullyHadronic/tauembedding/CMSSW_4_4_X/SingleMu_Mu_173693-177452_2011B_Nov19/SingleMu/Tauembedding_skim_v44_1_SingleMu_Mu_173693-177452_2011B_Nov19//079054b3ab4c4121ae105c34f9c44ff5/skim_100_1_Khr.root"]
+    process.source.fileNames = ["/store/group/local/HiggsChToTauNuFullyHadronic/tauembedding/CMSSW_4_4_X/SingleMu_173236-173692_2011A_Nov08/SingleMu/Run2011A_08Nov2011_v1_AOD_173236_173692_tauembedding_skim_v44_5_2/883c99ed2824db47683a70a86f21fc2e/skim_100_1_KK3.root"]
+    if len(options.trigger) == 0:
+        options.trigger = ["HLT_Mu40_eta2p1_v1"]
 
 ################################################################################
 
@@ -71,54 +76,40 @@ patArgs = {"doPatTrigger": False,
 process.commonSequence, additionalCounters = addPatOnTheFly(process, options, dataVersion, patArgs=patArgs,
                                                             doHBHENoiseFilter=False,
                                                             )
-#process.commonSequence.remove(process.goodPrimaryVertices10)
-# if options.doPat == 0:
-#     process.load("HiggsAnalysis.HeavyChHiggsToTauNu.HChPrimaryVertex_cfi")
-#     process.commonSequence *= (
-#         process.goodPrimaryVertices *
-#         process.goodPrimaryVertices10
-#     )
 
-from HiggsAnalysis.HeavyChHiggsToTauNu.HChTools import *
-# Pileup weights
-import HiggsAnalysis.HeavyChHiggsToTauNu.HChSignalAnalysisParameters_cff as param
-#param.changeCollectionsToPF2PAT(PF2PATVersion)
-puWeights = [
-    ("Run2011A", "Run2011A"),
-    ("Run2011B", "Run2011B"),
-    ("Run2011AB", "Run2011AB")
-    ]
-for era, name in puWeights:
-    modname = "pileupWeight"+name
-    setattr(process, modname, cms.EDProducer("HPlusVertexWeightProducer",
-        alias = cms.string(modname),
-    ))
-    param.setPileupWeight(dataVersion, process=process, commonSequence=process.commonSequence, era=era)
-    insertPSetContentsTo(param.vertexWeight.clone(), getattr(process, modname))
-    process.commonSequence.insert(0, getattr(process, modname))
-# FIXME: this is only a consequence of the swiss-knive effect...
-process.commonSequence.remove(process.goodPrimaryVertices)
-process.commonSequence.insert(0, process.goodPrimaryVertices)
-# FIXME: and this one because HBHENoiseFilter is not stored in embedding skims of v44_1
-#if dataVersion.isData():
-#    process.HBHENoiseSequence = cms.Sequence()
-#    process.commonSequence.replace(process.HBHENoiseFilter, process.HBHENoiseSequence*process.HBHENoiseFilter)
-#    import HiggsAnalysis.HeavyChHiggsToTauNu.HChDataSelection as DataSelection
-#    DataSelection.addHBHENoiseFilterResultProducer(process, process.HBHENoiseSequence)
+# PU weights
+import HiggsAnalysis.HeavyChHiggsToTauNu.AnalysisConfiguration as AnalysisConfiguration
+dataEras = [
+    "Run2011AB",
+    "Run2011A",
+    "Run2011B",
+]
+#puWeights = AnalysisConfiguration.addPuWeightProducers(dataVersion, process, process.commonSequence, dataEras)
+if dataVersion.isMC():
+    puEraSuffixWeights = AnalysisConfiguration.addPuWeightProducersVariations(dataVersion, process, process.commonSequence, dataEras)
+
+    # W+jets weights
+    import HiggsAnalysis.HeavyChHiggsToTauNu.WJetsWeight as WJetsWeight
+    wjetsEraSuffixWeights = []
+    for era, suffix, weight in puEraSuffixWeights:
+        weight = WJetsWeight.getWJetsWeight(dataVersion, options, "embedding_skim_v44_5_1", era, suffix)
+        name = "wjetsWeight"+era+suffix
+        weight.enabled = False
+        weight.alias = name
+        setattr(process, name, weight)
+        process.commonSequence += weight
+        wjetsEraSuffixWeights.append( (era, suffix, name) )
+        if options.wjetsWeighting != 0:
+            weight.enabled = True
     
 # Add the muon selection counters, as this is done after the skim
 import HiggsAnalysis.HeavyChHiggsToTauNu.tauEmbedding.muonSelectionPF as MuonSelection
+additionalCounters.extend(MuonSelection.getMuonPreSelectionCountersForEmbedding())
 additionalCounters.extend(MuonSelection.getMuonSelectionCountersForEmbedding(dataVersion))
 
 # Add configuration information to histograms.root
-process.infoPath = addConfigInfo(process, options, dataVersion)
-
-################################################################################
-
-#process.firstPrimaryVertex = cms.EDProducer("HPlusFirstVertexSelector",
-#    src = cms.InputTag("offlinePrimaryVertices")
-#)
-#process.commonSequence *= process.firstPrimaryVertex
+import HiggsAnalysis.HeavyChHiggsToTauNu.HChTools as HChTools
+process.infoPath = HChTools.addConfigInfo(process, options, dataVersion)
 
 
 import HiggsAnalysis.HeavyChHiggsToTauNu.tauEmbedding.customisations as customisations
@@ -134,70 +125,186 @@ muons = muons+"Iso"
 setattr(process, muons, isolation)
 process.commonSequence *= isolation
 
+# Electron Veto
 import HiggsAnalysis.HeavyChHiggsToTauNu.HChGlobalElectronVetoFilter_cfi as ElectronVeto
 process.eveto = ElectronVeto.hPlusGlobalElectronVetoFilter.clone(
     filter = False
 )
 process.commonSequence *= process.eveto
 
+# Muon preselection (without isolation)
+# start from 'selectedPatMuons' to double-check the selections in embedding job
+# store muons before pt>41 and trigger matching to allow muon veto in the ntuple-analysis
 process.preselectedMuons = cms.EDFilter("PATMuonSelector",
     src = cms.InputTag(muons),
     cut = cms.string(
         "isGlobalMuon() && isTrackerMuon()"
         "&& muonID('GlobalMuonPromptTight')"
-        "&& innerTrack().numberOfValidHits() > 10"
-        "&& innerTrack().hitPattern().pixelLayersWithMeasurement() >= 1"
-        "&& numberOfMatches() > 1"
+        "&& numberOfMatchedStations() > 1"
+        "&& abs(dB()) < 0.2" 
+        "&& innerTrack().hitPattern().numberOfValidPixelHits() > 0"
+        "&& track().hitPattern().trackerLayersWithMeasurement() > 8"
     )
 )
-process.preselectedMuons40 = cms.EDFilter("PATMuonSelector",
+muons = "preselectedMuons" # this is common to embedding muon selection and muon veto
+process.preselectedMuonsFilter = cms.EDFilter("CandViewCountFilter",
     src = cms.InputTag("preselectedMuons"),
-    cut = cms.string("pt() > 40 && abs(eta) < 2.1")
-)
-process.preselectedMuons40Filter = cms.EDFilter("CandViewCountFilter",
-    src = cms.InputTag("preselectedMuons40"),
     minNumber = cms.uint32(1)
 )
-process.preselectedMuons40Count = cms.EDProducer("EventCountProducer")
-process.commonSequence *= (
-    process.preselectedMuons *
-    process.preselectedMuons40 *
-    process.preselectedMuons40Filter *
-    process.preselectedMuons40Count
+process.preselectedMuonsCount = cms.EDProducer("EventCountProducer")
+process.commonSequence += (
+    process.preselectedMuons +
+    process.preselectedMuonsFilter +
+    process.preselectedMuonsCount
 )
-additionalCounters.append("preselectedMuons40Count")
+additionalCounters.append("preselectedMuonsCount")
+# Trigger matching
+import HiggsAnalysis.HeavyChHiggsToTauNu.HChTriggerMatching as HChTriggerMatching
+process.preselectedMuonsMatched = HChTriggerMatching.createMuonTriggerMatchingInAnalysis(options.trigger, "preselectedMuons")
+process.preselectedMuonsMatchedFilter = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("preselectedMuonsMatched"),
+    minNumber = cms.uint32(1)
+)
+process.preselectedMuonsMatchedCount = cms.EDProducer("EventCountProducer")
+process.commonSequence += (
+    process.preselectedMuonsMatched +
+    process.preselectedMuonsMatchedFilter +
+    process.preselectedMuonsMatchedCount
+)
+additionalCounters.append("preselectedMuonsMatchedCount")
+# Kinematic cuts
+process.preselectedMuons41 = cms.EDFilter("PATMuonSelector",
+    src = cms.InputTag("preselectedMuonsMatched"),
+    cut = cms.string("pt() > 41 && abs(eta) < 2.1")
+)
+process.preselectedMuons41Filter = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("preselectedMuons41"),
+    minNumber = cms.uint32(1)
+)
+process.preselectedMuons41Count = cms.EDProducer("EventCountProducer")
+process.commonSequence *= (
+    process.preselectedMuons41 *
+    process.preselectedMuons41Filter *
+    process.preselectedMuons41Count
+)
+additionalCounters.append("preselectedMuons41Count")
+# MuScleFit correction
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/MuScleFitCorrections2012
+muscle = cms.EDProducer("MuScleFitPATMuonCorrector", 
+    src = cms.InputTag(muons), 
+    debug = cms.bool(False), 
+    identifier = cms.string("Data2011_44X"),
+    applySmearing = cms.bool(False), 
+    fakeSmearing = cms.bool(False)
+)
+setattr(process, muons+"Muscle", muscle)
+process.commonSequence += muscle
+if dataVersion.isMC():
+    muscle.identifier = "Fall11_START44"
+    muscle.applySmearing = True
 
-process.preselectedJets = cms.EDFilter("PATJetSelector",
-#    src = cms.InputTag("goodJets"+PF2PATVersion),
-#    src = cms.InputTag("goodJets"),
-    src = cms.InputTag("selectedPatJets"),
-    cut = cms.string(customisations.jetSelection)
+
+
+# Jet selection
+# Cannot do filtering because of jet systematic variations
+import HiggsAnalysis.HeavyChHiggsToTauNu.HChSignalAnalysisParameters_cff as param
+param.setJERSmearedJets(dataVersion)
+import HiggsAnalysis.HeavyChHiggsToTauNu.HChJetFilter_cfi as jetFilter_cfi
+process.selectedJets = jetFilter_cfi.hPlusJetPtrSelectorFilter.clone(
+    tauSrc = "",
+    removeTau = False,
+    histogramAmbientLevel = "Systematics",
+    producePt20 = True,
+    filter=False,
 )
-process.preselectedJetsFilter = cms.EDFilter("CandViewCountFilter",
-    src = cms.InputTag("preselectedJets"),
-    minNumber = cms.uint32(3)
+#process.selectedJetsCount = cms.EDProducer("EventCountProducer")
+process.commonSequence += (
+    process.selectedJets #+
+#    process.selectedJetsCount
 )
-process.preselectedJetsCount = cms.EDProducer("EventCountProducer")
-process.commonSequence *= (
-    process.preselectedJets *
-#    process.preselectedJetsFilter * 
-    process.preselectedJetsCount
+#additionalCounters.append("selectedJetsCount")
+
+# Calculate b-tagging quantities
+import HiggsAnalysis.HeavyChHiggsToTauNu.HChBTaggingFilter_cfi as btagFilter_cfi
+process.btagging = btagFilter_cfi.hPlusBTaggingPtrSelectorFilter.clone(
+#    jetSrc = cms.InputTag("selectedJets", "selectedJetsPt20"),
+    jetSrc = process.selectedJets.jetSelection.src.value(),
+    histogramAmbientLevel = "Systematics",
+    filter = False,
 )
-additionalCounters.append("preselectedJetsCount")
+process.commonSequence += process.btagging
+#process.debug = cms.EDAnalyzer("EventContentAnalyzer")
+#process.commonSequence += process.debug
+
+# MET filters
+# disabled for now due to a bug in v44_5_1 skim
+import HiggsAnalysis.HeavyChHiggsToTauNu.HChMETFilter_cfi as METFilter_cfi
+process.metNoiseFilters = METFilter_cfi.hPlusMETNoiseFilters.clone(
+    filter = False,
+)
+#process.metNoiseFilters.metFilters.beamHaloEnabled = False
+#process.metNoiseFilters.metFilters.trackingFailureFilterEnabled = False
+#process.metNoiseFilters.metFilters.EcalDeadCellEventFilterEnabled = False
+#process.metNoiseFilters.metFilters.EcalDeadCellTPFilterEnabled = False
+process.commonSequence += process.metNoiseFilters
+
+
+# process.preselectedJets = cms.EDFilter("PATJetSelector",
+# #    src = cms.InputTag("goodJets"+PF2PATVersion),
+# #    src = cms.InputTag("goodJets"),
+#     src = cms.InputTag("selectedPatJets"),
+#     cut = cms.string(customisations.jetSelection)
+# )
+# process.preselectedJetsFilter = cms.EDFilter("CandViewCountFilter",
+#     src = cms.InputTag("preselectedJets"),
+#     minNumber = cms.uint32(3)
+# )
+# process.preselectedJetsCount = cms.EDProducer("EventCountProducer")
+# process.commonSequence *= (
+#     process.preselectedJets *
+# #    process.preselectedJetsFilter * 
+#     process.preselectedJetsCount
+# )
+# additionalCounters.append("preselectedJetsCount")
 
 # Configuration
 import HiggsAnalysis.HeavyChHiggsToTauNu.tauEmbedding.analysisConfig as analysisConfig
 import HiggsAnalysis.HeavyChHiggsToTauNu.Ntuple as Ntuple
 ntuple = cms.EDAnalyzer("HPlusMuonNtupleAnalyzer",
     patTriggerEvent = cms.InputTag("patTriggerEvent"),
+
     genParticleSrc = cms.InputTag("genParticles"),
+    genTTBarEnabled = cms.bool(False),
+
     selectedPrimaryVertexSrc = cms.InputTag("selectedPrimaryVertex"),
     goodPrimaryVertexSrc = cms.InputTag("goodPrimaryVertices"),
 
     muons = Ntuple.muons.clone(
-        src = "preselectedMuons",
-        functions = analysisConfig.muonFunctions.clone()
-    )
+        src = muons,
+        correctedEnabled = cms.bool(True),
+        correctedSrc = muons+"Muscle",
+        functions = analysisConfig.muonFunctions.clone(),
+        bools = cms.PSet(
+            triggerMatched = cms.InputTag(muons+"Matched")
+        ),
+    ),
+    muonEfficiencies = cms.PSet(
+        id_Run2011A = param.embeddingMuonEfficiencyID.clone(
+            dataSelect = ["Run2011A"],
+            mcSelect = "Run2011A",
+        ),
+        id_Run2011B = param.embeddingMuonEfficiencyID.clone(
+            dataSelect = ["Run2011B"],
+            mcSelect = "Run2011B",
+        ),
+        id_Run2011AB = param.embeddingMuonEfficiencyID.clone(
+            dataSelect = ["Run2011A", "Run2011B"],
+            mcSelect = "Run2011AB",
+        ),
+        trigger = param.embeddingMuonEfficiencyTrigger.clone(
+            dataSelect = ["Run2011AB"],
+        ),
+    ),
 
 #    electronSrc = cms.InputTag("selectedPatElectrons"),
 #    electronConversionSrc = cms.InputTag("allConversions"),
@@ -218,37 +325,83 @@ ntuple = cms.EDAnalyzer("HPlusMuonNtupleAnalyzer",
         ),
     ),
 
-    mets = analysisConfig.mets.clone(),
+    mets = analysisConfig.mets.clone(
+        pfMetRaw_p4 = cms.InputTag("patPFMet"),
+        pfMetType1_p4 = cms.InputTag("patType1CorrectedPFMet"), # this is automatically from smeared jets for MC
+#        pfMetType1p2_p4 = cms.InputTag("patType1p2CorrectedPFMet"), # this is automatically from smeared jets for MC
+    ),
     doubles = cms.PSet(),
     bools = cms.PSet(
-        ElectronVetoPassed = cms.InputTag("eveto")
+        METNoiseFiltersPassed = cms.InputTag("metNoiseFilters"),
+        ElectronVetoPassed = cms.InputTag("eveto"),
     ),
+
+    eventCounter = param.eventCounter.clone(),
+    histogramAmbientLevel = cms.untracked.string("Informative"),
 )
-for era, name in puWeights:
-    setattr(ntuple.doubles, "weightPileup_"+name, cms.InputTag("pileupWeight"+name))
-if dataVersion.isData():
-    ntuple.bools.HBHENoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer", "HBHENoiseFilterResult")
-    ntuple.bools.HBHENoiseFilterMETWG = cms.InputTag("HBHENoiseFilterResultProducerMETWG", "HBHENoiseFilterResult")
+del ntuple.mets.pfMet_p4
+if dataVersion.isMC():
+    for era, suffix, weight in puEraSuffixWeights:
+        setattr(ntuple.doubles, "weightPileup_"+era+suffix, cms.InputTag(weight))
+    for era, suffix, weight in wjetsEraSuffixWeights:
+        setattr(ntuple.doubles, "weightWJets_"+era+suffix, cms.InputTag(weight))
 
+    for name in ntuple.muonEfficiencies.parameterNames_():
+        pset = getattr(ntuple.muonEfficiencies, name)
+        pset.mode = "scaleFactor"
 
-addAnalysis(process, "muonNtuple", ntuple,
-            preSequence=process.commonSequence,
-            additionalCounters=additionalCounters,
-            signalAnalysisCounters=False)
-process.muonNtupleCounters.printMainCounter = True
+    def addMetVariation(name, metSrc):
+        for d in ["Up", "Down"]:
+            midfix = name%d
+            setattr(ntuple.mets, "pfMetType1%s_p4" % midfix, cms.InputTag(metSrc%d))
+
+    def addJetVariation(name, src, metName, metSrc):
+        addMetVariation(metName, metSrc)
+        for d in ["Up", "Down"]:
+            postfix = src%d
+            selectedJets = process.selectedJets.clone()
+            selectedJets.jetSelection.src = postfix
+            param.setJetPUIdSrc(selectedJets.jetSelection, "selectedJets"+postfix)
+            setattr(process, "selectedJets"+postfix, selectedJets)
+            process.commonSequence += selectedJets
+
+            btagging = process.btagging.clone()
+            btagging.jetSrc = selectedJets.jetSelection.src.value()
+            setattr(process, "btagging"+postfix, btagging)
+            process.commonSequence += btagging
+
+            jets = ntuple.jets.jets.clone()
+            jets.detailsEnabled = False
+            jets.functions = cms.PSet()
+            jets.src.setModuleLabel("selectedJets"+postfix)
+            jets.floats.btagScaleFactor.setModuleLabel("btagging"+postfix)
+            del jets.floats.btagScaleFactorUncertainty
+            jets.bools.btagged.setModuleLabel("btagging"+postfix)
+
+            setattr(ntuple.jets, name%d, jets)
+
+#    addJetVariation("jetsEn%s", "shiftedPatJetsEn%sForCorrMEt", "JetEn%s", "patType1CorrectedPFMetJetEn%s")
+#    addJetVariation("jetsRes%s", "smearedPatJetsRes%s", "JetRes%s", "patType1CorrectedPFMetJetRes%s")
+#    addMetVariation("UnclusteredEn%s", "patType1CorrectedPFMetUnclusteredEn%s")
+
+HChTools.addAnalysis(process, "muonNtuple", ntuple,
+                     preSequence=process.commonSequence,
+                     additionalCounters=additionalCounters)
+process.muonNtuple.eventCounter.printMainCounter = True
 
 # Replace all event counters with the weighted one
-eventCounters = []
-for label, module in process.producers_().iteritems():
-    if module.type_() == "EventCountProducer":
-        eventCounters.append(label)
-prototype = cms.EDProducer("HPlusEventCountProducer",
-    weightSrc = cms.InputTag("pileupWeight"+puWeights[-1][1])
-)
-for label in eventCounters:
-    process.globalReplace(label, prototype.clone())
+if dataVersion.isMC():
+    eventCounters = []
+    for label, module in process.producers_().iteritems():
+        if module.type_() == "EventCountProducer":
+            eventCounters.append(label)
+    prototype = cms.EDProducer("HPlusEventCountProducer",
+        weightSrc = cms.InputTag(puEraSuffixWeights[0][2])
+    )
+    for label in eventCounters:
+        process.globalReplace(label, prototype.clone())
+    
 
-
-#f = open("configDump.py", "w")
-#f.write(process.dumpPython())
-#f.close()
+f = open("configDumpMuonAnalysis.py", "w")
+f.write(process.dumpPython())
+f.close()
