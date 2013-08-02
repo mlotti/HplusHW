85b16d3037e016688d7168ccc056accebf9eb460
Lauri A. Wendland <lauri.wendland@cern.ch>
2013-08-02 19:13:12 +0300

    technical: moved tau fake rate analysis away from NormalisationAnalysis; NormalisationAnalysis reverted to a base class for normalisation studies

--- HeavyChHiggsToTauNu/interface/NormalisationAnalysis.h
+++ HeavyChHiggsToTauNu/interface/NormalisationAnalysis.h
@@ -10,10 +10,12 @@
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/ElectronSelection.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/MuonSelection.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/JetSelection.h"
+#include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/METTriggerEfficiencyScaleFactor.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/METSelection.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/BTagging.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/TopChiSelection.h"
 #include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/EvtTopology.h"
+#include "HiggsAnalysis/HeavyChHiggsToTauNu/interface/QCDTailKiller.h"
 
 #include <string>
 #include <vector>
@@ -34,35 +36,26 @@ namespace HPlus {
    */
   class NormalisationAnalysis {
   public:
-    NormalisationAnalysis(EventCounter& eventCounter, HistoWrapper& histoWrapper);
-    NormalisationAnalysis(const edm::ParameterSet& iConfig, EventCounter& eventCounter, HistoWrapper& histoWrapper);
+    NormalisationAnalysis(EventCounter& eventCounter, HistoWrapper& histoWrapper, std::string label);
+    NormalisationAnalysis(const edm::ParameterSet& iConfig, EventCounter& eventCounter, HistoWrapper& histoWrapper, std::string label);
     ~NormalisationAnalysis();
 
-    /**
-     * Does tag and probe for a selected electron and the selected tau using Z->ee mass as a constraint.
-     * Derives normalisation scale factors in bins of tau pT and tau decay mode
-     */
-    void analyseEToTauFakes(const VertexSelection::Data& vertexData,
-                            const TauSelection::Data& tauData,
-                            const FakeTauIdentifier::Data& fakeTauData,
-                            const ElectronSelection::Data& electronData,
-                            const MuonSelection::Data& muondata,
-                            const JetSelection::Data& jetData,
-                            const METSelection::Data& metData);
-    /**
-     * Analyses tau fake rate
-     */
-    void analyseTauFakeRate(const edm::Event& iEvent,
-                            const VertexSelection::Data& vertexData,
-                            TauSelection& tauSelection,
-                            const TauSelection::Data& tauData,
-                            FakeTauIdentifier& fakeTauIdentifier,
-                            const JetSelection::Data& jetData);
+    virtual void analyse(const edm::Event& iEvent,
+                          const TauSelection::Data& tauData,
+                          const FakeTauIdentifier::Data& fakeTauData,
+                          const ElectronSelection::Data& electronData,
+                          const MuonSelection::Data& muonData,
+                          const JetSelection::Data& jetData,
+                          METTriggerEfficiencyScaleFactor& metTrgSF,
+                          const QCDTailKiller::Data& tailKillerData,
+                          const METSelection::Data& metData,
+                          const BTagging::Data& btagData);
 
+  private:
+    /// Creates histograms
+    void createHistograms(std::string label);
 
   protected:
-    /// Creates histograms
-    void createHistograms();
     /// Event counter object
     EventCounter& fEventCounter;
     /// HistoWrapper object
@@ -70,58 +63,25 @@ namespace HPlus {
 
     // Input parameters
 
-    // Counters - needed or not?
+    // Counters
 
     // Histograms ------------------------------------------
-    // e -> tau fakes
-    WrappedTH1* hEtoTauZmassAll;
-    WrappedTH1* hEtoTauZmassDecayMode0;
-    WrappedTH1* hEtoTauZmassDecayMode1;
-    WrappedTH1* hEtoTauZmassDecayMode2;
-    WrappedTH1* hEtoTauTauPtAll;
-    WrappedTH1* hEtoTauTauPtDecayMode0;
-    WrappedTH1* hEtoTauTauPtDecayMode1;
-    WrappedTH1* hEtoTauTauPtDecayMode2;
-    // tau fake rate
-    WrappedTH1 *hTauVsJetDeltaPt;
-    WrappedTH1 *hTauVsJetDeltaR;
-    WrappedTH1 *hTauVsJetMCFlavor;
-    WrappedTH1 *hTauVsJetDeltaPtGenuineTaus;
-    WrappedTH1 *hTauVsJetDeltaPtElectrons;
-    WrappedTH1 *hTauVsJetDeltaPtHeavyFlavor;
-    WrappedTH1 *hTauVsJetDeltaRHeavyFlavor;
-    WrappedTH1 *hTauVsJetDeltaPtLightFlavor;
-    WrappedTH1 *hTauVsJetDeltaRLightFlavor;
+    // All histograms are after the full selection (it may differ from signal analysis)
+    WrappedTH1* hTauPt;
+    WrappedTH1* hNJets;
+    WrappedTH1* hMET;
+    WrappedTH1* hMETphi;
+    WrappedTH1* hNBJets;
+    WrappedTH1* hTransverseMass;
+    WrappedTH1* hZMass;
 
-    WrappedTH1 *hTauVsJetTauPtbBefore;
-    WrappedTH1 *hTauVsJetTauPtbleptonicBefore;
-    WrappedTH1 *hTauVsJetTauPtcBefore;
-    WrappedTH1 *hTauVsJetTauPtudsBefore;
-    WrappedTH1 *hTauVsJetTauPtgBefore;
-    WrappedTH1 *hTauVsJetTauPteBefore;
-    WrappedTH1 *hTauVsJetTauPtmuBefore;
-    WrappedTH1 *hTauVsJetTauPtbAfter;
-    WrappedTH1 *hTauVsJetTauPtbleptonicAfter;
-    WrappedTH1 *hTauVsJetTauPtcAfter;
-    WrappedTH1 *hTauVsJetTauPtudsAfter;
-    WrappedTH1 *hTauVsJetTauPtgAfter;
-    WrappedTH1 *hTauVsJetTauPteAfter;
-    WrappedTH1 *hTauVsJetTauPtmuAfter;
-
-    WrappedTH1 *hTauVsJetTauPtbByJetPtBefore;
-    WrappedTH1 *hTauVsJetTauPtbleptonicByJetPtBefore;
-    WrappedTH1 *hTauVsJetTauPtcByJetPtBefore;
-    WrappedTH1 *hTauVsJetTauPtudsByJetPtBefore;
-    WrappedTH1 *hTauVsJetTauPtgByJetPtBefore;
-    WrappedTH1 *hTauVsJetTauPteByJetPtBefore;
-    WrappedTH1 *hTauVsJetTauPtmuByJetPtBefore;
-    WrappedTH1 *hTauVsJetTauPtbByJetPtAfter;
-    WrappedTH1 *hTauVsJetTauPtbleptonicByJetPtAfter;
-    WrappedTH1 *hTauVsJetTauPtcByJetPtAfter;
-    WrappedTH1 *hTauVsJetTauPtudsByJetPtAfter;
-    WrappedTH1 *hTauVsJetTauPtgByJetPtAfter;
-    WrappedTH1 *hTauVsJetTauPteByJetPtAfter;
-    WrappedTH1 *hTauVsJetTauPtmuByJetPtAfter;
+    WrappedTH1* hFakeTauTauPt;
+    WrappedTH1* hFakeTauNJets;
+    WrappedTH1* hFakeTauMET;
+    WrappedTH1* hFakeTauMETphi;
+    WrappedTH1* hFakeTauNBJets;
+    WrappedTH1* hFakeTauTransverseMass;
+    WrappedTH1* hFakeTauZMass;
 
   };
 }
