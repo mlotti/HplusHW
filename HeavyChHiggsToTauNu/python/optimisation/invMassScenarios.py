132103c3b044f69fa6dea49909be84aee293e72e
Matti Kortelainen <matti.kortelainen@helsinki.fi>
2013-08-16 09:19:28 +0300

    bugfix: Fixed naming of invariant mass optimisation scenarios to be the same as with doInvariantMassReconstructionScenarios

--- HeavyChHiggsToTauNu/python/optimisation/invMassScenarios.py
+++ HeavyChHiggsToTauNu/python/optimisation/invMassScenarios.py
@@ -7,22 +7,22 @@ _neutrinoPzSolutionSelectionMethods = ["DeltaEtaMax", "Smaller"]
 scenarios = []
 for currentPzSelectionMethod in _neutrinoPzSolutionSelectionMethods:
         scenarios.extend([
-            Scenario("RecoPZSelection"+currentPzSelectionMethod+"TopInvMassCutNone",
+            Scenario("PzSelection"+currentPzSelectionMethod+"TopInvMassCutNone",
                      topInvMassLowerCut = -1, # negative value means no cut
                      topInvMassUpperCut = -1, # negative value means no cut
                      pzSelectionMethod = currentPzSelectionMethod
                  ),
-            Scenario("RecoPZSelection"+currentPzSelectionMethod+"TopInvMassCutLoose",
+            Scenario("PzSelection"+currentPzSelectionMethod+"TopInvMassCutLoose",
                      topInvMassLowerCut = 100, # negative value means no cut
                      topInvMassUpperCut = 240, # negative value means no cut
                      pzSelectionMethod = currentPzSelectionMethod
                  ),
-            Scenario("RecoPZSelection"+currentPzSelectionMethod+"TopInvMassCutMedium",
+            Scenario("PzSelection"+currentPzSelectionMethod+"TopInvMassCutMedium",
                      topInvMassLowerCut = 140, # negative value means no cut
                      topInvMassUpperCut = 200, # negative value means no cut
                      pzSelectionMethod = currentPzSelectionMethod
                  ),
-            Scenario("RecoPZSelection"+currentPzSelectionMethod+"TopInvMassCutTight",
+            Scenario("PzSelection"+currentPzSelectionMethod+"TopInvMassCutTight",
                      topInvMassLowerCut = 157, # negative value means no cut
                      topInvMassUpperCut = 187, # negative value means no cut
                      pzSelectionMethod = currentPzSelectionMethod
