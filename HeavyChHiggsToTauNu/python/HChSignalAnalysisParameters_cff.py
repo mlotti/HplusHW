7d8753dfee2cf5e3aeb5ec2f7bb0539ec98eef0e
Santeri <santeri.laurila@cern.ch>
2013-08-26 16:26:27 +0200

    bugfix: instructions in python/HChSignalAnalysisParameters_cff.py about top reco updated

--- HeavyChHiggsToTauNu/python/HChSignalAnalysisParameters_cff.py
+++ HeavyChHiggsToTauNu/python/HChSignalAnalysisParameters_cff.py
@@ -409,7 +409,7 @@ invMassReco = cms.untracked.PSet(
     pzSelectionMethod = cms.untracked.string("deltaEtaMax"),
     )
 
-topReconstruction = cms.untracked.string("Bselection") # Options: None, chi, std, Wselection, Bselection
+topReconstruction = cms.untracked.string("None") # Options: None, chi, std, Bselection, Wselection
 
 transverseMassCut = cms.untracked.double(100) # Not used
 
