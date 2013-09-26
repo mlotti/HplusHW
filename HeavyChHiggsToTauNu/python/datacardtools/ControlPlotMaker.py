49f7dca6c000c7a510e93afbd4fecaff650804e4
Matti Kortelainen <matti.kortelainen@cern.ch>
2013-09-26 14:11:11 +0300

    bugfix[datacard]: Fix control plot drawing order in ControlPlotMaker.py

--- HeavyChHiggsToTauNu/python/datacardtools/ControlPlotMaker.py
+++ HeavyChHiggsToTauNu/python/datacardtools/ControlPlotMaker.py
@@ -115,14 +115,14 @@ class ControlPlotMaker:
                     self._applyBlinding(hData,myCtrlPlot.blindedRange)
                 myHisto = histograms.Histo(hData,"Data")
                 myHisto.setIsDataMC(isData=True, isMC=False)
-                myStackList.append(myHisto)
+                myStackList.insert(0, myHisto)
                 # Add signal
                 mySignalLabel = "TTToHplus_M%d"%m
                 if m > 179:
                     mySignalLabel = "HplusTB_M%d"%m
                 myHisto = histograms.Histo(hSignal,mySignalLabel)
                 myHisto.setIsDataMC(isData=False, isMC=True)
-                myStackList.append(myHisto)
+                myStackList.insert(1, myHisto)
                 # Make plot
                 myStackPlot = plots.DataMCPlot2(myStackList)
                 myStackPlot.setLuminosity(self._luminosity)
