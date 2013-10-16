106db7e1a79ff585958aaa135cdc2d43716f2ba6
Lauri A. Wendland <lauri.wendland@cern.ch>
2013-10-16 19:21:15 +0300

    [technical]: fix to met scenarios

--- HeavyChHiggsToTauNu/python/optimisation/metScenarios.py
+++ HeavyChHiggsToTauNu/python/optimisation/metScenarios.py
@@ -5,6 +5,8 @@ optimisation = HPlusOptimisationScheme()
 
 _pt = [50,60,70,80,90]
 
+scenarios = []
+
 for pt in _pt:
     scenarios.append(Scenario("%d"%(pt),
                               METCut = pt))
