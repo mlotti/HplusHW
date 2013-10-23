6cdf8dfdb420c6e7c3f4ec0db8b6bf6a4d1146c4
Lauri Wendland <lauri.wendland@cern.ch>
2013-10-23 15:36:00 +0200

    [bugfix]: SelectedTauForVariation no longer added to SystVar list when reading multicrab directories

--- HeavyChHiggsToTauNu/python/tools/dataset.py
+++ HeavyChHiggsToTauNu/python/tools/dataset.py
@@ -3462,6 +3462,8 @@ class DatasetManagerCreator:
             # Look for systematic variation
             start = directoryName.find("SystVar")
             if start >= 0:
+                if "SelectedTauForVariation" in directoryName[start:]:
+                    continue
                 systematicVariations[directoryName[start:]] = 1
                 directoryName = directoryName[:start]
 
