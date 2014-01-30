cc0e7201eaa1a2d847867fe9118e8daa889cefd5
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-01-30 14:14:54 +0200

    analysis[embdding]: Update plotEwkValidationCompareCaloMet.py

--- HeavyChHiggsToTauNu/test/plotScripts/plotEwkValidationCompareCaloMet.py
+++ HeavyChHiggsToTauNu/test/plotScripts/plotEwkValidationCompareCaloMet.py
@@ -39,19 +39,26 @@ import HiggsAnalysis.HeavyChHiggsToTauNu.tools.tauEmbedding as tauEmbedding
 
 
 def main():
-    dirNormal = "../multicrab_signalAnalysisGenTauSkim_140127_092222"
+    dirNormal = "../multicrab_signalAnalysisGenTauSkim_140129_140448"
     dirEmb = "."
 
     dsetsNormal = dataset.getDatasetsFromMulticrabCfg(directory=dirNormal, analysisName="signalAnalysisGenuineTau")
+    dsetsNormalCut = dataset.getDatasetsFromMulticrabCfg(directory=dirNormal, analysisName="signalAnalysisGenuineTauCaloMet60")
     dsetsEmb = dataset.getDatasetsFromMulticrabCfg(directory=dirEmb, analysisName="signalAnalysisMIdEffTrgEffWTauMu")
+    dsetsEmbCut = dataset.getDatasetsFromMulticrabCfg(directory=dirEmb, analysisName="signalAnalysisMIdEffTrgEffWTauMuCaloMet60")
 
     dsetsNormal.updateNAllEventsToPUWeighted()
+    dsetsNormalCut.updateNAllEventsToPUWeighted()
     dsetsEmb.updateNAllEventsToPUWeighted()
+    dsetsEmbCut.updateNAllEventsToPUWeighted()
 
     dsetsEmb.loadLuminosities()
+    dsetsEmbCut.loadLuminosities()
 
     plots.mergeRenameReorderForDataMC(dsetsNormal)
+    plots.mergeRenameReorderForDataMC(dsetsNormalCut)
     plots.mergeRenameReorderForDataMC(dsetsEmb)
+    plots.mergeRenameReorderForDataMC(dsetsEmbCut)
 
     # Apply TDR style
     style = tdrstyle.TDRStyle()
@@ -59,19 +66,21 @@ def main():
     histograms.cmsText[histograms.CMSMode.SIMULATION] = "Simulation"
     histograms.createLegend.setDefaults(y1=0.93, y2=0.8, x1=0.82, x2=0.93)
 
-    doPlots(dsetsNormal.getDataset("TTJets"), dsetsEmb.getDataset("TTJets"), dsetsEmb.getDataset("Data").getLuminosity())
+    #doPlots(dsetsNormal.getDataset("TTJets"), dsetsEmb.getDataset("TTJets"), dsetsEmb.getDataset("Data").getLuminosity())
+    doEffPlots(dsetsNormalCut.getDataset("TTJets"), dsetsNormal.getDataset("TTJets"), dsetsEmbCut.getDataset("TTJets"), dsetsEmb.getDataset("TTJets"), dsetsEmb.getDataset("Data").getLuminosity())
+
+def doLineStyle(l):
+    l.SetLineColor(ROOT.kBlue)
+    l.SetLineWidth(2)
+    l.SetLineStyle(ROOT.kDotted)
+    return l
+
 
 def doPlots(dsetNormal, dsetEmb, lumi):
     def doStyle(h, color):
         th = h.getRootHisto()
         th.SetLineColor(color)
         th.SetLineWidth(3)
-
-    def doLineStyle(l):
-        l.SetLineColor(ROOT.kBlue)
-        l.SetLineWidth(2)
-        l.SetLineStyle(ROOT.kDotted)
-        return l
 
     global ind
     ind = 0
@@ -116,6 +125,90 @@ def doPlots(dsetNormal, dsetEmb, lumi):
         "DeltaPhiBackToBack",
         "Selected"]:
         compare(step)
+
+def doEffPlots(dsetNormalNum, dsetNormalDenom, dsetEmbNum, dsetEmbDenom, lumi):
+    def doStyle(h, color, style=None):
+        th = h.getRootHisto()
+        th.SetLineColor(color)
+        th.SetLineWidth(2)
+        if style is not None:
+            th.SetMarkerColor(color)
+            th.SetMarkerStyle(style)
+            th.SetMarkerSize(1)
+
+    global ind
+    ind = 0
+    def compare(step):
+        path = "CommonPlots/AtEveryStep/%s/MET_MET" % step
+
+        bins = range(0, 200, 20) + [200, 250, 300, 500]
+
+        def getTH1(ds):
+            drh = ds.getDatasetRootHisto(path)
+            drh.normalizeToLuminosity(lumi)
+            th1 = drh.getHistogram()
+            #th1.Rebin(2)
+            #return th1
+            return th1.Rebin(len(bins)-1, th1.GetName(), array.array("d", bins))
+
+        th1NormalNum = getTH1(dsetNormalNum)
+        th1NormalDenom = getTH1(dsetNormalDenom)
+
+        print th1NormalNum.Integral(0, th1NormalNum.GetNbinsX()+1), th1NormalDenom.Integral(0, th1NormalDenom.GetNbinsX()+1)
+
+        th1EmbNum = getTH1(dsetEmbNum)
+        th1EmbDenom = getTH1(dsetEmbDenom)
+
+        print th1EmbNum.Integral(0, th1EmbNum.GetNbinsX()+1), th1EmbDenom.Integral(0, th1EmbDenom.GetNbinsX()+1)
+
+        effNormal = ROOT.TGraphAsymmErrors(th1NormalNum, th1NormalDenom, "n")
+        effEmb = ROOT.TGraphAsymmErrors(th1EmbNum, th1EmbDenom, "n")
+
+        effNormal.SetName("Normal ttbar")
+        effEmb.SetName("Embedded ttbar")
+
+        p = plots.ComparisonPlot(effNormal, effEmb)
+        p.setLuminosity(lumi)
+        p.histoMgr.forHisto("Normal ttbar", lambda h: doStyle(h, ROOT.kBlack, ROOT.kFullCircle))
+        p.histoMgr.forHisto("Embedded ttbar", lambda h: doStyle(h, ROOT.kRed, ROOT.kFullSquare))
+        p.histoMgr.setHistoDrawStyleAll("EP")
+
+        p.prependPlotObjectToRatio(doLineStyle(ROOT.TLine(0, 1.1, 500, 1.1)))
+        p.prependPlotObjectToRatio(doLineStyle(ROOT.TLine(0, 0.9, 500, 0.9)))
+
+        global ind
+        ind += 1
+        plots.drawPlot(p, "eff_%02d_calomet_%s"%(ind, step), xlabel="Type I PF MET (GeV)", ylabel="CaloMET cut efficiency",
+                       ratio=True, ratioYlabel="Norm./emb.", ratioType="errorScale",
+                       opts2={"ymin": 0.5, "ymax": 1.5},
+                       addLuminosityText=True, moveLegend={"dx": -0.2, "dy": -0.5})
+
+
+        # th1EmbNum.SetName("Numerator")
+        # th1EmbDenom.SetName("Denominator")
+        # p = plots.ComparisonPlot(th1EmbNum, th1EmbDenom)
+        # p.histoMgr.forHisto("Numerator", lambda h: doStyle(h, ROOT.kBlack))
+        # p.histoMgr.forHisto("Denominator", lambda h: doStyle(h, ROOT.kRed))
+        # plots.drawPlot(p, "effdebug_emb_%02d_%s" %(ind, step), ratio=True)
+
+        # th1NormalNum.SetName("Numerator")
+        # th1NormalDenom.SetName("Denominator")
+        # p = plots.ComparisonPlot(th1NormalNum, th1NormalDenom)
+        # p.histoMgr.forHisto("Numerator", lambda h: doStyle(h, ROOT.kBlack))
+        # p.histoMgr.forHisto("Denominator", lambda h: doStyle(h, ROOT.kRed))
+        # plots.drawPlot(p, "effdebug_normal_%02d_%s" %(ind, step), ratio=True)
+
+
+    for step in [
+        "JetSelection",
+#        "MET",
+#        "METPhiCorrected",
+#        "BTagging",
+#        "DeltaPhiBackToBack",
+#        "Selected"
+        ]:
+        compare(step)
+
 
 if __name__ == "__main__":
     main()
