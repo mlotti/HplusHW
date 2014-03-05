06e1b02983568f3f85d1cd437ebc8f1d1b7842cf
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-03-05 13:53:55 +0200

    technical: Switch crab configurations to use DBS3

--- HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCommon.py
+++ HeavyChHiggsToTauNu/python/tools/multicrabDatasetsCommon.py
@@ -3,7 +3,7 @@
 # Provides the pattuple DBS writer URL
 
 # pattuple DBS writer URL
-pattuple_dbs = "http://cmsdbsprod.cern.ch/cms_dbs_ph_analysis_01/servlet/DBSServlet"
-pattuple_dbs_writer = "https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_01_writer/servlet/DBSServlet"
-tteff_dbs = "http://cmsdbsprod.cern.ch/cms_dbs_ph_analysis_01/servlet/DBSServlet"
+pattuple_dbs = "phys01"
+pattuple_dbs_writer = "phys03"
+tteff_dbs = "phys01"
 #tteff_dbs = "http://cmsdbsprod.cern.ch/cms_dbs_ph_analysis_02/servlet/DBSServlet"
