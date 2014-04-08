3e3d9f471b4580842d9f41cd331a3c0917c418fb
Lauri A. Wendland <lauri.wendland@cern.ch>
2014-04-08 15:21:11 +0200

    technical: updated purity plot for QCD

--- HeavyChHiggsToTauNu/test/QCDInverted/plotPurity_new.py
+++ HeavyChHiggsToTauNu/test/QCDInverted/plotPurity_new.py
@@ -4,121 +4,102 @@ import ROOT
 ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 import math
+import array
 import sys
 import os
 from optparse import OptionParser
 
 import HiggsAnalysis.HeavyChHiggsToTauNu.tools.dataset as dataset
 import HiggsAnalysis.HeavyChHiggsToTauNu.tools.histograms as histograms
-import HiggsAnalysis.HeavyChHiggsToTauNu.tools.counter as counter
 import HiggsAnalysis.HeavyChHiggsToTauNu.tools.tdrstyle as tdrstyle
 import HiggsAnalysis.HeavyChHiggsToTauNu.tools.styles as styles
 import HiggsAnalysis.HeavyChHiggsToTauNu.tools.plots as plots
-import HiggsAnalysis.HeavyChHiggsToTauNu.tools.crosssection as xsect
+import HiggsAnalysis.HeavyChHiggsToTauNu.tools.aux as aux
+import HiggsAnalysis.HeavyChHiggsToTauNu.tools.systematics as systematics
 from HiggsAnalysis.HeavyChHiggsToTauNu.tools.analysisModuleSelector import *
 from HiggsAnalysis.HeavyChHiggsToTauNu.qcdCommon.dataDrivenQCDCount import *
-from HiggsAnalysis.HeavyChHiggsToTauNu.datacardtools.ShapeHistoModifier import *
 
-myHistoSpecs = { "bins": 13,
-                 "rangeMin": 0.0,
-                 "rangeMax": 500.0,
-                 #"variableBinSizeLowEdges": [], # if an empty list is given, then uniform bin width is used
-                 "variableBinSizeLowEdges": [0,20,40,60,80,100,120,140,160,200,250,300,400], # if an empty list is given, then uniform bin width is used
-                 "xtitle": "Transverse mass / GeV",
-                 "ytitle": "Events" }
+# Set here the names of the histograms you want to access
+histoNameList = ["shapeTransverseMass",
+                 "shapeInvariantMass"]
 
+def doSinglePlot(histograms, myDir, histoname, luminosity):
+    plot = plots.PlotBase(histograms)
+    plot.setLuminosity(luminosity)
+    plot.histoMgr.forEachHisto(lambda h: h.getRootHisto().SetLineWidth(3))
+    myPlotName = "%s/QCDInv_Purity_%s"%(myDir, histoName)
+    myParams = {}
+    myParams["ylabel"] = "Purity"
+    myParams["log"] = False
+    #myParams["opts2"] = {"ymin": 0.0, "ymax":2.0}
+    myParams["opts"] = {"ymin": 0.0, "ymax":1.0}
+    #myParams["ratio"] = True
+    #myParams["ratioType"] = "errorScale"
+    #myParams["ratioYlabel"] = "Var./Nom."
+    myParams["cmsText"] = "CMS preliminary"
+    myParams["addLuminosityText"] = True
+    myParams["moveLegend"] = {"dx": -0.05, "dy": 0.0}
+    #myParams["createLegend"] = None
+    #myParams["divideByBinWidth"] = True
+    plots.drawPlot(plot, myPlotName, **myParams)
 
-def doPurityPlots(opts, dsetMgr, moduleInfoString, myDir, luminosity):
-    # Set here the names of the histograms you want to access
-    histoNameList = ["baseline/METBaselineTauIdAfterCollinearCuts",
-                     "Inverted/METInvertedTauIdAfterCollinearCuts",
-                     "Inverted/MTInvertedTauIdAfterBackToBackCuts"]
-                     #"shapeTransverseMass" # should be the same as mt after leg 1
+def doPurityPlots(opts, histoName, dsetMgr, moduleInfoString, myDir, luminosity, myShapePurityHistograms):
+    def handleOverflow(h):
+        h.SetBinContent(1, h.GetBinContent(0)+h.GetBinContent(1))
+        h.SetBinError(1, math.sqrt(h.GetBinContent(0)**2 + h.GetBinContent(1)**2))
+        h.SetBinContent(h.GetNbinsX()+1, h.GetBinContent(h.GetNbinsX()+1)+h.GetBinContent(h.GetNbinsX()+2))
+        h.SetBinError(h.GetNbinsX()+1, math.sqrt(h.GetBinError(h.GetNbinsX()+1)**2 + h.GetBinError(h.GetNbinsX()+2)**2))
+        h.SetBinContent(0, 0.0)
+        h.SetBinError(0, 0.0)
+        h.SetBinContent(h.GetNbinsX()+2, 0.0)
+        h.SetBinError(h.GetNbinsX()+2, 0.0)
 
-    for histoName in histoNameList:
-        print histoName
-        myQCDShape = DataDrivenQCDShape(dsetMgr, "Data", "EWK", histoName, luminosity)
-        # Get integrated purity
-        myIntegratedPurity = myQCDShape.getIntegratedPurity()
-        print "Module: %s / histogram %s : integrated purity = %f +- %f"%(moduleInfoString, histoName, myIntegratedPurity.value(), myIntegratedPurity.uncertainty())
-        # Get purity as function of splitted phase space bin
-        hPurityBySplittedBin = myQCDShape.getPurityHisto()
-        # Get integrated purity as function of the shape bins
-        hPurityByFinalShapeBin = myQCDShape.getIntegratedPurityForShapeHisto(myHistoSpecs)
-
-        # Do plotting - this needs to be edited to use tdr style ...
-        c1 = ROOT.TCanvas()
-        c1.Draw()
-        hPurityBySplittedBin.Draw()
-        c1.Print("%s/purityBySplittedBin_%s_%s.png"%(myDir, histoName.replace("/","_"), moduleInfoString))
-        c2 = ROOT.TCanvas()
-        c2.Draw()
-        hPurityByFinalShapeBin.Draw()
-        c2.Print("%s/purityByFinalShapeBin_%s_%s.png"%(myDir, histoName.replace("/","_"), moduleInfoString))
-    print HighlightStyle()+"doPurityPlots is ready"+NormalStyle()
-
-def doQCDfactorisedResultPlots(opts, dsetMgr, moduleInfoString, myDir, luminosity):
-    # Set here the names of the histograms you want to access
-    myBasicName = "QCDfactorised/MtAfterStandardSelections"
-    myLeg1Name = "QCDfactorised/MtAfterLeg1"
-    myLeg2Name = "QCDfactorised/MtAfterLeg2"
-
-    # Obtain QCD shapes
-    myBasicShape = DataDrivenQCDShape(dsetMgr, "Data", "EWK", myBasicName, luminosity)
-    myLeg1Shape = DataDrivenQCDShape(dsetMgr, "Data", "EWK", myLeg1Name, luminosity)
-    myLeg2Shape = DataDrivenQCDShape(dsetMgr, "Data", "EWK", myLeg2Name, luminosity)
-
-    # Calculate final shape in signal region (leg1 * leg2 / basic)
-    nSplitBins = myBasicShape.getNumberOfPhaseSpaceSplitBins()
-    myModifier = ShapeHistoModifier(myHistoSpecs)
-    hTotalQCD = myModifier.createEmptyShapeHistogram("NQCD_Total_%s"%moduleInfoString)
-    myBinQCDList = []
-    for i in range(0, nSplitBins):
-        hBin = myModifier.createEmptyShapeHistogram("NQCD_%s_%s"%(myBasicShape.getPhaseSpaceBinFileFriendlyTitle(i).replace(" ",""), moduleInfoString))
-        myBinQCDList.append(hBin)
-    for i in range(0, nSplitBins):
-        hBasic = myBasicShape.getDataDrivenQCDHistoForSplittedBin(i, myHistoSpecs)
-        hLeg1 = myLeg1Shape.getDataDrivenQCDHistoForSplittedBin(i, myHistoSpecs)
-        hLeg2 = myLeg2Shape.getDataDrivenQCDHistoForSplittedBin(i, myHistoSpecs)
-        for j in range(1,hBasic.GetNbinsX()+1):
-            myResult = 0.0
-            myStatUncertSquared = 0.0
-            if hBasic.GetBinContent(j) > 0.0:
-                # Calculate result
-                myResult = abs(hLeg1.GetBinContent(j) * hLeg2.GetBinContent(j) / hBasic.GetBinContent(j))
-                # Calculate abs. stat. uncert.
-                myStatUncertSquared = ((hLeg1.GetBinError(j) * hLeg2.GetBinContent(j))**2 + (hLeg2.GetBinError(j) * hLeg1.GetBinContent(j))**2) / hBasic.GetBinContent(j)**2
-                # Treat negative numbers
-                if hLeg1.GetBinContent(j) < 0.0 or hLeg2.GetBinContent(j) < 0.0 or hBasic.GetBinContent(j) < 0.0:
-                    myResult = -myResult;
-            myBinQCDList[i].SetBinContent(j, myResult)
-            myBinQCDList[i].SetBinError(j, sqrt(myStatUncertSquared))
-            hTotalQCD.SetBinContent(j, hTotalQCD.GetBinContent(j) + myResult)
-            hTotalQCD.SetBinError(j, hTotalQCD.GetBinError(j) + myStatUncertSquared) # Sum squared
-    # Take square root of uncertainties
-    myModifier.finaliseShape(dest=hTotalQCD)
-    # Print result
-    nQCD = 0.0
-    nQCDStatUncert = 0.0
-    for i in range(1, hTotalQCD.GetNbinsX()+1):
-        nQCD += hTotalQCD.GetBinContent(i)
-        nQCDStatUncert += hTotalQCD.GetBinError(i)**2
-    nQCDStatUncert = sqrt(nQCDStatUncert)
-    # To obtain syst. uncert. for EWK is quite a bit more difficult. Will develop it later.
-    print "NQCD = %f +- %f (stat.)"%(nQCD, nQCDStatUncert)
-
-    # Do plotting - this needs to be edited to use tdr style ...
-    c1 = ROOT.TCanvas()
-    c1.Draw()
-    hTotalQCD.Draw()
-    c1.Print("%s/QCDShape_total_%s.png"%(myDir, moduleInfoString))
-    for i in range(0,len(myBinQCDList)):
-        c = ROOT.TCanvas()
-        c.Draw()
-        myBinQCDList[i].Draw()
-        myBinTitle = myBasicShape.getPhaseSpaceBinFileFriendlyTitle(i)
-        c.Print("%s/QCDShape_%s_%s.png"%(myDir, myBinTitle, moduleInfoString))
-    print HighlightStyle()+"doQCDfactorisedResultPlots is ready"+NormalStyle()
+    myQCDShape = DataDrivenQCDShape(dsetMgr, "Data", "EWK", histoName, luminosity)
+    # Get data and EWK histograms
+    hData = myQCDShape.getIntegratedDataHisto()
+    hEWK = myQCDShape.getIntegratedEwkHisto()
+    # Rebin and handle overflow
+    myBinning = systematics.getBinningForPlot(histoName)
+    myArray = array.array("d",myBinning)
+    hData = hData.Rebin(len(myBinning)-1,"",myArray)
+    hEWK = hEWK.Rebin(len(myBinning)-1,"",myArray)
+    handleOverflow(hData)
+    handleOverflow(hEWK)
+    # Create purity histo
+    hPurity = aux.Clone(hData)
+    hPurity.Reset()
+    # Calculate purity
+    for i in xrange(1, hPurity.GetNbinsX()+1):
+        myPurity = 0.0
+        myUncert = 0.0
+        nData = hData.GetBinContent(i)
+        nEWK = hEWK.GetBinContent(i)
+        if (nData > 0.0):
+            myPurity = (nData - nEWK) / nData
+            if myPurity < 0.0:
+                myPurity = 0.0
+                myUncert = 0.0
+            else:
+                myUncert = sqrt(myPurity * (1.0-myPurity) / nData) # Assume binomial error
+        print nData,nEWK,myPurity,myUncert
+        hPurity.SetBinContent(i, myPurity)
+        hPurity.SetBinError(i, myUncert)
+    # Find out tail killer scenario
+    myScenario = ""
+    if "QCDTailKillerLoose" in moduleInfoString:
+        myScenario = "Loose"
+    elif "QCDTailKillerMedium" in moduleInfoString:
+        myScenario = "Medium"
+    if "QCDTailKillerTight" in moduleInfoString:
+        myScenario = "Tight"
+    myScenario += " R_{bb}^{min.}"
+    # Make histogram
+    hPurity.SetLineColor(styles.styles[len(myShapePurityHistograms)].color)
+    hPurity.SetMarkerColor(styles.styles[len(myShapePurityHistograms)].color)
+    hPurity.SetMarkerStyle(styles.styles[len(myShapePurityHistograms)].marker)
+    # Plot
+    histo = histograms.Histo(hPurity, myScenario, drawStyle="", legendStyle="lp")
+    myShapePurityHistograms.append(histo)
 
 def createOutputdirectory(myDir):
     if os.path.exists(myDir):
@@ -139,8 +120,6 @@ if __name__ == "__main__":
     parser = OptionParser(usage="Usage: %prog [options]",add_help_option=True,conflict_handler="resolve")
     myModuleSelector.addParserOptions(parser)
     parser.add_option("--mdir", dest="multicrabDir", action="store", help="Multicrab directory")
-    # Add here parser options, if necessary, following line is an example
-    #parser.add_option("--showcard", dest="showDatacard", action="store_true", default=False, help="Print datacards also to screen")
 
     # Parse options
     (opts, args) = parser.parse_args()
@@ -156,38 +135,45 @@ if __name__ == "__main__":
     # Select modules
     myModuleSelector.doSelect(opts)
 
+    # Apply TDR style
+    style = tdrstyle.TDRStyle()
+
     myDisplayStatus = True
     # Loop over era/searchMode/optimizationMode options
     for era in myModuleSelector.getSelectedEras():
         for searchMode in myModuleSelector.getSelectedSearchModes():
-            for optimizationMode in myModuleSelector.getSelectedOptimizationModes():
+            for histoName in histoNameList:
+                print histoName
+                myShapePurityHistograms = []
                 # Construct info string of module
-                myModuleInfoString = "%s_%s_%s"%(era, searchMode, optimizationMode)
+                myModuleInfoString = "%s_%s"%(era, searchMode)
                 print HighlightStyle()+"Module:",myModuleInfoString,NormalStyle()
-                # Obtain dataset manager
-                dsetMgr = dsetMgrCreator.createDatasetManager(dataEra=era,searchMode=searchMode,optimizationMode=optimizationMode)
-                # Do the usual normalisation
-                dsetMgr.updateNAllEventsToPUWeighted()
-                dsetMgr.loadLuminosities()
-                plots.mergeRenameReorderForDataMC(dsetMgr)
-                dsetMgr.merge("EWK", [
-                              "TTJets",
-                              "WJets",
-                              "DYJetsToLL",
-                              "SingleTop",
-                              "Diboson"
-                              ])
                 # Make a directory for output
-                myDir = "plots_%s"%myModuleInfoString
+                myDir = "purityplots_%s"%myModuleInfoString
                 createOutputdirectory(myDir)
-                # Obtain luminosity
-                myLuminosity = dsetMgr.getDataset("Data").getLuminosity()
-                # Print info so that user can check that merge went correct
-                if myDisplayStatus:
-                    dsetMgr.printDatasetTree()
-                    print "Luminosity = %f 1/fb"%(myLuminosity / 1000.0)
-                    print
-                    myDisplayStatus = False
-                # Run plots
-                doPurityPlots(opts, dsetMgr, myModuleInfoString, myDir, myLuminosity)
-                #doQCDfactorisedResultPlots(opts, dsetMgr, myModuleInfoString, myDir, myLuminosity)
+                for optimizationMode in myModuleSelector.getSelectedOptimizationModes():
+                    # Obtain dataset manager
+                    dsetMgr = dsetMgrCreator.createDatasetManager(dataEra=era,searchMode=searchMode,optimizationMode=optimizationMode)
+                    # Do the usual normalisation
+                    dsetMgr.updateNAllEventsToPUWeighted()
+                    dsetMgr.loadLuminosities()
+                    plots.mergeRenameReorderForDataMC(dsetMgr)
+                    dsetMgr.merge("EWK", [
+                                  "TTJets",
+                                  "WJets",
+                                  "DYJetsToLL",
+                                  "SingleTop",
+                                  "Diboson"
+                                  ])
+                    # Obtain luminosity
+                    myLuminosity = dsetMgr.getDataset("Data").getLuminosity()
+                    # Print info so that user can check that merge went correct
+                    if myDisplayStatus:
+                        dsetMgr.printDatasetTree()
+                        print "Luminosity = %f 1/fb"%(myLuminosity / 1000.0)
+                        print
+                        myDisplayStatus = False
+                    # Run plots
+                    doPurityPlots(opts, histoName, dsetMgr, optimizationMode, myDir, myLuminosity, myShapePurityHistograms)
+                # Plot
+                doSinglePlot(myShapePurityHistograms, myDir, histoName, myLuminosity)
