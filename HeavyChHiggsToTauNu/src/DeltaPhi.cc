c0c39091c4ddcba7e80880f0938618d06c7969e8
Lauri A. Wendland <lauri.wendland@cern.ch>
2014-08-22 14:58:57 +0300

    technical: Added various tail killer related control plots; disabled w and top mass control plots from systematics level

--- HeavyChHiggsToTauNu/src/DeltaPhi.cc
+++ HeavyChHiggsToTauNu/src/DeltaPhi.cc
@@ -20,4 +20,19 @@ namespace HPlus {
     if ( myCosPhi < 1) myDeltaPhi =   acos(myCosPhi);
     return myDeltaPhi; 
   }
+  
+  double DeltaPhi::reconstruct(const reco::Candidate& tau, const reco::Candidate& jet) {
+    // Construct tau vector, mtau = 1.777 GeV/c2
+    TLorentzVector myTau;
+    myTau.SetXYZM(tau.px(), tau.py(), tau.pz(), 1.777); 
+    // Calculate cosine of angle between jet and met direction
+          //    double myEtMiss = TMath::Sqrt(met.px()*met.px() + met.py()*met.py());
+    double myJetPt = jet.pt();
+    double myCosPhi = 100;
+    if (myEtMiss > 0 && myTau.Pt() > 0)
+      myCosPhi = (myTau.X()*jet.px() + myTau.Y()*jet.py()) / (myTau.Pt()*myJetPt);
+    double myDeltaPhi = -999;
+    if ( myCosPhi < 1) myDeltaPhi =   acos(myCosPhi);
+    return myDeltaPhi; 
+  }
 }
