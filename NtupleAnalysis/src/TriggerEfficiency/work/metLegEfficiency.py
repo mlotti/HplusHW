177e25c3568140e9721691f43995bd96ffce5707
S. Lehti <sami.lehti@cern.ch>
2015-02-26 09:04:16 +0100

    TriggerEfficiency: added dir work

---
+++ NtupleAnalysis/src/TriggerEfficiency/work/metLegEfficiency.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from HiggsAnalysis.NtupleAnalysis.main import Process, PSet, Analyzer
+
+process = Process()
+
+# Example of adding a dataset which has its files defined in data/<dataset_name>.txt file
+#process.addDatasets(["TTbar_HBWB_HToTauNu_M_160_13TeV_pythia6"])
+
+# Example of adding datasets from a multicrab directory
+import sys
+if len(sys.argv) != 2:
+    print "Usage: ./exampleAnalysis.py <path-to-multicrab-directory>"
+    sys.exit(0)
+process.addDatasetsFromMulticrab(sys.argv[1])
+
+# Example of adding an analyzer
+process.addAnalyzer("test", Analyzer("TriggerEfficiency",
+                                     tauPtCut = 10
+))
+
+# Run the analysis
+process.run()
+
+# Run the analysis with PROOF
+# By default it uses all cores, but you can give proofWorkers=<N> as a parameter
+#process.run(proof=True)
