b3b73bac8ec3dd7b505ac036ed00be2ceacadbbd
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-03-04 09:04:31 +0100

    Move getter implementation to class template
    Only class templates can be partially specialized, will be needed later.

--- NtupleAnalysis/src/Framework/interface/ParameterSet.h
+++ NtupleAnalysis/src/Framework/interface/ParameterSet.h
@@ -7,18 +7,33 @@
 
 #include <type_traits>
 
+namespace ParameterSetImpl {
+  template <typename T>
+  struct ParameterGetter {
+    static
+    T get(const boost::property_tree::ptree& config, const std::string& name) {
+      return config.get<T>(name);
+    };
+
+    static
+    boost::optional<T> getOptional(const boost::property_tree::ptree& config, const std::string& name) {
+      return config.get_optional<T>(name);
+    }
+  };
+}
+
 class ParameterSet {
 public:
   explicit ParameterSet(const boost::property_tree::ptree& config);
 
   template <typename T>
   T getParameter(const std::string& name) const {
-    return fConfig.get<T>(name);
+    return ParameterSetImpl::ParameterGetter<T>::get(fConfig, name);
   }
 
   template <typename T>
   boost::optional<T> getParameterOptional(const std::string& name) const {
-    return fConfig.get_optional<T>(name);
+    return ParameterSetImpl::ParameterGetter<T>::getOptional(fConfig, name);
   }
 
 private:
