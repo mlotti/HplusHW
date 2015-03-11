fdd0585dfca5b0c6f9d83b25981316a7652b505f
Ritva Kinnunen <rkinnune@jade.hip.fi>
2015-03-11 16:13:19 +0200

    merge

--- NtupleAnalysis/src/MetAnalysis/src/MetAnalysis.cc
+++ NtupleAnalysis/src/MetAnalysis/src/MetAnalysis.cc
@@ -67,6 +67,72 @@ MetAnalysis::MetAnalysis(const ParameterSet& config):
   cElectronVeto(fEventCounter.addCounter("Electron veto")),
   cJetSelection(fEventCounter.addCounter("Jet selection"))
 {
+  fECALDeadCellEtaTable.push_back(-2.35); fECALDeadCellPhiTable.push_back(-1.52639);
+  fECALDeadCellEtaTable.push_back(-2.35); fECALDeadCellPhiTable.push_back(-1.43917);
+  fECALDeadCellEtaTable.push_back(-2.25); fECALDeadCellPhiTable.push_back(-1.52639);
+  fECALDeadCellEtaTable.push_back(-2.25); fECALDeadCellPhiTable.push_back(-1.43917);
+  fECALDeadCellEtaTable.push_back(-2.25); fECALDeadCellPhiTable.push_back(-1.35194);
+  fECALDeadCellEtaTable.push_back(-2.15); fECALDeadCellPhiTable.push_back(-2.48583);
+  fECALDeadCellEtaTable.push_back(-2.05); fECALDeadCellPhiTable.push_back(-2.57306);
+  fECALDeadCellEtaTable.push_back(-2.05); fECALDeadCellPhiTable.push_back(-2.48583);
+  fECALDeadCellEtaTable.push_back(-2.05); fECALDeadCellPhiTable.push_back(-2.39861);
+  fECALDeadCellEtaTable.push_back(-1.95); fECALDeadCellPhiTable.push_back(-2.48583);
+  fECALDeadCellEtaTable.push_back(-1.65); fECALDeadCellPhiTable.push_back(0.654167);
+  fECALDeadCellEtaTable.push_back(-1.55); fECALDeadCellPhiTable.push_back(0.566944);
+  fECALDeadCellEtaTable.push_back(-1.55); fECALDeadCellPhiTable.push_back(0.654167);
+  fECALDeadCellEtaTable.push_back(-1.55); fECALDeadCellPhiTable.push_back(1.00306);
+  fECALDeadCellEtaTable.push_back(-1.45); fECALDeadCellPhiTable.push_back(-3.09639);
+  fECALDeadCellEtaTable.push_back(-1.45); fECALDeadCellPhiTable.push_back(-1.61361);
+  fECALDeadCellEtaTable.push_back(-1.35); fECALDeadCellPhiTable.push_back(-0.130833);
+  fECALDeadCellEtaTable.push_back(-1.25); fECALDeadCellPhiTable.push_back(-2.66028);
+  fECALDeadCellEtaTable.push_back(-1.25); fECALDeadCellPhiTable.push_back(-1.1775);
+  fECALDeadCellEtaTable.push_back(-1.15); fECALDeadCellPhiTable.push_back(-1.1775);
+  fECALDeadCellEtaTable.push_back(-0.95); fECALDeadCellPhiTable.push_back(2.04972);
+  fECALDeadCellEtaTable.push_back(-0.95); fECALDeadCellPhiTable.push_back(3.00917);
+  fECALDeadCellEtaTable.push_back(-0.85); fECALDeadCellPhiTable.push_back(2.04972);
+  fECALDeadCellEtaTable.push_back(-0.85); fECALDeadCellPhiTable.push_back(3.00917);
+  fECALDeadCellEtaTable.push_back(-0.75); fECALDeadCellPhiTable.push_back(-0.741389);
+  fECALDeadCellEtaTable.push_back(-0.55); fECALDeadCellPhiTable.push_back(2.7475);
+  fECALDeadCellEtaTable.push_back(-0.45); fECALDeadCellPhiTable.push_back(2.7475);
+  fECALDeadCellEtaTable.push_back(-0.35); fECALDeadCellPhiTable.push_back(0.218056);
+  fECALDeadCellEtaTable.push_back(-0.25); fECALDeadCellPhiTable.push_back(0.130833);
+  fECALDeadCellEtaTable.push_back(-0.25); fECALDeadCellPhiTable.push_back(0.218056);
+  fECALDeadCellEtaTable.push_back(-0.15); fECALDeadCellPhiTable.push_back(-2.57306);
+  fECALDeadCellEtaTable.push_back(-0.15); fECALDeadCellPhiTable.push_back(0.130833);
+  fECALDeadCellEtaTable.push_back(-0.15); fECALDeadCellPhiTable.push_back(0.741389);
+  fECALDeadCellEtaTable.push_back(-0.05); fECALDeadCellPhiTable.push_back(-2.57306);
+  fECALDeadCellEtaTable.push_back(-0.05); fECALDeadCellPhiTable.push_back(0.741389);
+  fECALDeadCellEtaTable.push_back(-0.05); fECALDeadCellPhiTable.push_back(0.915833);
+  fECALDeadCellEtaTable.push_back(0.05); fECALDeadCellPhiTable.push_back(1.78806);
+  fECALDeadCellEtaTable.push_back(0.15); fECALDeadCellPhiTable.push_back(1.78806);
+  fECALDeadCellEtaTable.push_back(0.85); fECALDeadCellPhiTable.push_back(0.3925);
+  fECALDeadCellEtaTable.push_back(0.85); fECALDeadCellPhiTable.push_back(1.70083);
+  fECALDeadCellEtaTable.push_back(0.85); fECALDeadCellPhiTable.push_back(2.83472);
+  fECALDeadCellEtaTable.push_back(0.95); fECALDeadCellPhiTable.push_back(0.654167);
+  fECALDeadCellEtaTable.push_back(0.95); fECALDeadCellPhiTable.push_back(1.70083);
+  fECALDeadCellEtaTable.push_back(0.95); fECALDeadCellPhiTable.push_back(2.83472);
+  fECALDeadCellEtaTable.push_back(1.05); fECALDeadCellPhiTable.push_back(-3.09639);
+  fECALDeadCellEtaTable.push_back(1.05); fECALDeadCellPhiTable.push_back(0.654167);
+  fECALDeadCellEtaTable.push_back(1.15); fECALDeadCellPhiTable.push_back(-3.09639);
+  fECALDeadCellEtaTable.push_back(1.25); fECALDeadCellPhiTable.push_back(-0.479722);
+  fECALDeadCellEtaTable.push_back(1.45); fECALDeadCellPhiTable.push_back(-2.57306);
+  fECALDeadCellEtaTable.push_back(1.45); fECALDeadCellPhiTable.push_back(-0.218056);
+  fECALDeadCellEtaTable.push_back(1.45); fECALDeadCellPhiTable.push_back(2.7475);
+  fECALDeadCellEtaTable.push_back(1.55); fECALDeadCellPhiTable.push_back(-3.09639);
+  fECALDeadCellEtaTable.push_back(1.55); fECALDeadCellPhiTable.push_back(-0.741389);
+  fECALDeadCellEtaTable.push_back(1.55); fECALDeadCellPhiTable.push_back(-0.654167);
+  fECALDeadCellEtaTable.push_back(1.55); fECALDeadCellPhiTable.push_back(-0.566944);
+  fECALDeadCellEtaTable.push_back(1.65); fECALDeadCellPhiTable.push_back(-2.83472);
+  fECALDeadCellEtaTable.push_back(1.65); fECALDeadCellPhiTable.push_back(-0.741389);
+  fECALDeadCellEtaTable.push_back(1.65); fECALDeadCellPhiTable.push_back(-0.654167);
+  fECALDeadCellEtaTable.push_back(1.65); fECALDeadCellPhiTable.push_back(-0.566944);
+  fECALDeadCellEtaTable.push_back(1.75); fECALDeadCellPhiTable.push_back(-0.654167);
+  fECALDeadCellEtaTable.push_back(1.75); fECALDeadCellPhiTable.push_back(-0.566944);
+  fECALDeadCellEtaTable.push_back(1.75); fECALDeadCellPhiTable.push_back(0.741389);
+  fECALDeadCellEtaTable.push_back(1.75); fECALDeadCellPhiTable.push_back(0.828611);
+
+/*
+2011 towers
    fECALDeadCellEtaTable.push_back(-2.35); fECALDeadCellPhiTable.push_back(-1.52639);
     fECALDeadCellEtaTable.push_back(-2.35); fECALDeadCellPhiTable.push_back(-1.43917);
     fECALDeadCellEtaTable.push_back(-2.35); fECALDeadCellPhiTable.push_back(-1.35194);
@@ -124,7 +190,7 @@ MetAnalysis::MetAnalysis(const ParameterSet& config):
     fECALDeadCellEtaTable.push_back(2.85); fECALDeadCellPhiTable.push_back(-0.915833);
     fECALDeadCellEtaTable.push_back(2.85); fECALDeadCellPhiTable.push_back(-0.828611);
     fECALDeadCellEtaTable.push_back(2.85); fECALDeadCellPhiTable.push_back(-0.741389);
-
+*/
 
 }
 
