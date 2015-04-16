16b4e1cf5ba6aaa405030652eaa175ded28be898
Lauri A. Wendland <lauri.wendland@cern.ch>
2015-04-16 18:08:54 +0300

    technical: developing root support for tan beta scan

--- HeavyChHiggsToTauNu/test/brlimit/doTanBetaScan.py
+++ HeavyChHiggsToTauNu/test/brlimit/doTanBetaScan.py
@@ -1022,7 +1022,7 @@ if __name__ == "__main__":
         for m in myMassPoints:
             for scen in myScenarios:
                 if opts.gridRunAllMassesInOneJob:
-                    if m == mymassPoints[0]:
+                    if m == myMassPoints[0]:
                         print scen
                 else:
                     print scen,m
