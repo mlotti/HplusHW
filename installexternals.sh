47bb2333d2d68b33cb729dfd24d844ddcb8dd6aa
Lauri A. Wendland <lauri.wendland@cern.ch>
2015-06-01 11:21:21 +0300

    technical: small fix to installation script

--- installexternals.sh
+++ installexternals.sh
@@ -4,7 +4,7 @@
 if [ ! -e ./NtupleAnalysis/external/boost ]; then
   echo "Getting and installing boost"
   cd ./NtupleAnalysis/external
-  #wget http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz
+  wget http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz
   tar xfz boost_1_57_0.tar.gz
   rm boost_1_57_0.tar.gz
   ln -s boost_1_57_0/boost boost
