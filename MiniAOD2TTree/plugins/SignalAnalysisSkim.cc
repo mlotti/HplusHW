0dd946a7e4ec5b628e4be136219932bc493d7a96
S. Lehti <sami.lehti@cern.ch>
2015-06-05 10:11:10 +0200

    MiniAOD2TTree: added SignalAnalysisSkim

---
+++ MiniAOD2TTree/plugins/SignalAnalysisSkim.cc
@@ -0,0 +1,139 @@
+/** \class SignalAnalysisSkim
+ *
+ *  
+ *  Filter to select events for 
+ *  tau+MET trigger MET-leg efficiency study
+ *
+ *  \author Sami Lehti  -  HIP Helsinki
+ *
+ */
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/Common/interface/View.h"
+#include "DataFormats/Common/interface/Ptr.h"
+
+#include "FWCore/Framework/interface/TriggerNamesService.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+
+#include "DataFormats/PatCandidates/interface/Jet.h"
+
+#include "FWCore/Framework/interface/LuminosityBlock.h"
+
+#include <iostream>
+#include <regex>
+
+class SignalAnalysisSkim : public edm::EDFilter {
+
+    public:
+        explicit SignalAnalysisSkim(const edm::ParameterSet&);
+        ~SignalAnalysisSkim();
+
+  	virtual bool filter(edm::Event&, const edm::EventSetup& );
+
+   private:
+	edm::InputTag triggerResults;
+	std::vector<std::string> triggerBits;
+
+        edm::InputTag	jetCollection;
+
+        std::vector<std::string> jetUserFloats;
+
+	double jetEtCut,jetEtaCut;
+	int nJets;
+
+        int nEvents, nSelectedEvents;
+};
+
+SignalAnalysisSkim::SignalAnalysisSkim(const edm::ParameterSet& iConfig) {
+    triggerResults     = iConfig.getParameter<edm::InputTag>("TriggerResults");
+    triggerBits        = iConfig.getParameter<std::vector<std::string> >("HLTPaths");
+
+    jetCollection      = iConfig.getParameter<edm::InputTag>("JetCollection");
+    jetUserFloats      = iConfig.getParameter<std::vector<std::string> >("JetUserFloats");
+
+    jetEtCut           = iConfig.getParameter<double>("JetEtCut");
+    jetEtaCut          = iConfig.getParameter<double>("JetEtaCut");                            
+    nJets              = iConfig.getParameter<int>("NJets");
+
+    nEvents         = 0;
+    nSelectedEvents = 0;
+}
+
+
+SignalAnalysisSkim::~SignalAnalysisSkim(){
+    std::cout << "SignalAnalysisSkim: " //  	edm::LogVerbatim("SignalAnalysisSkim") 
+              << " Number_events_read " << nEvents
+              << " Number_events_kept " << nSelectedEvents
+              << " Efficiency         " << ((double)nSelectedEvents)/((double) nEvents) << std::endl;
+}
+
+
+bool SignalAnalysisSkim::filter(edm::Event& iEvent, const edm::EventSetup& iSetup ){
+
+    nEvents++;
+
+    // Trigger bits
+    edm::Handle<edm::TriggerResults> trghandle;
+    iEvent.getByLabel(triggerResults,trghandle);
+    if(trghandle.isValid()){
+        edm::TriggerResults tr = *trghandle;
+        bool fromPSetRegistry;
+        edm::Service<edm::service::TriggerNamesService> tns;
+        std::vector<std::string> hlNames; 
+        tns->getTrigPaths(tr, hlNames, fromPSetRegistry);
+
+	bool passed = false;
+        for(size_t i = 0; i < triggerBits.size(); ++i){
+	    std::regex hlt_re(triggerBits[i]);
+	    int n = 0;
+            for(std::vector<std::string>::const_iterator j = hlNames.begin(); j!= hlNames.end(); ++j){
+		if (std::regex_search(*j, hlt_re)) {
+		    if(trghandle->accept(n)) {
+			passed = true;
+                        break;
+		    }
+                }
+		n++;
+            }
+        }
+	if(!passed) return false; 
+    }
+
+    edm::Handle<edm::View<pat::Jet> > jethandle;
+    iEvent.getByLabel(jetCollection, jethandle);
+    int njets = 0;
+    if(jethandle.isValid()){
+        for(size_t i=0; i<jethandle->size(); ++i) {
+            const pat::Jet& obj = jethandle->at(i);
+
+	    if(obj.p4().pt() < jetEtCut) continue;
+	    if(fabs(obj.p4().eta()) > jetEtaCut) continue;
+/*
+	    bool passed = true;
+	    for(size_t j = 0; j < jetUserFloats.size(); ++j){
+		if(obj.userFloat(jetUserFloats[j]) < 0) {
+		    passed = false;
+		    break;
+		}
+	    }
+	    if(!passed) continue;
+*/
+	    njets++;
+	}
+    }
+    if(njets == 0) return false;
+
+    nSelectedEvents++;
+    return true;
+}
+
+DEFINE_FWK_MODULE(SignalAnalysisSkim);   
+
---
+++ MiniAOD2TTree/test/miniAOD2TTree_SignalAnalysisSkim_cfg.py
@@ -0,0 +1,179 @@
+import FWCore.ParameterSet.Config as cms
+import HiggsAnalysis.MiniAOD2TTree.tools.git as git #HiggsAnalysis.HeavyChHiggsToTauNu.tools.git as git
+
+process = cms.Process("TTreeDump")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+process.load("FWCore/MessageService/MessageLogger_cfi")
+process.MessageLogger.categories.append("TriggerBitCounter")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000 # print the event number for every 100th event
+process.MessageLogger.cerr.TriggerBitCounter = cms.untracked.PSet(limit = cms.untracked.int32(10)) # print max 100 warnings
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+	'/store/mc/RunIISpring15DR74/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/2A98D4CF-F9FE-E411-AB1A-047D7BD6DD44.root',
+#        'file:miniAOD-prod_PAT_TT_RECO_721_11112014.root'
+#	'file:miniAOD-prod_PAT_TT_RECO_740p1_02122014.root'
+#	'file:miniAOD-prod_PAT_Hp200_RECO_740p1_09122014.root'
+#	'file:miniAOD-prod_PAT_SingleMu_Run2012D_v1_RECO.root'
+#	'file:PYTHIA6_Tauola_TTbar_H160_taunu_13TeV_cff_py_GEN.root'
+    )
+)
+
+process.dump = cms.EDFilter('MiniAOD2TTreeFilter',
+    OutputFileName = cms.string("miniaod2tree.root"),
+    CodeVersion = cms.string(git.getCommitId()),
+    DataVersion = cms.string("74Xmc"),
+    CMEnergy = cms.int32(13),
+    Skim = cms.PSet(
+	Counters = cms.VInputTag(
+	    "skimCounterAll",
+            "skimCounterPassed"
+        ),
+    ),
+    EventInfo = cms.PSet(
+	PileupSummaryInfoSrc = cms.InputTag("addPileupInfo"),
+#	LHESrc = cms.InputTag(""),
+	OfflinePrimaryVertexSrc = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    ),
+    Trigger = cms.PSet(
+	TriggerResults = cms.InputTag("TriggerResults::HLT"),
+	TriggerBits = cms.vstring(
+	    "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET120_v",
+            "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET80_v",
+            "HLT_LooseIsoPFTau50_Trk30_eta2p1_v",
+#	    "HLT_IsoMu24_IterTrk02_v1"
+        ),
+	L1Extra = cms.InputTag("l1extraParticles::MET"),
+	TriggerObjects = cms.InputTag("selectedPatTrigger"),
+	filter = cms.untracked.bool(False)
+    ),
+    Taus = cms.VPSet(
+        cms.PSet(
+            branchname = cms.untracked.string("Taus"),
+            src = cms.InputTag("slimmedTaus"),
+            discriminators = cms.vstring(
+                "againstElectronLoose",
+                "againstElectronLooseMVA5",
+                "againstElectronMVA5category",
+                "againstElectronMVA5raw",
+                "againstElectronMedium",
+                "againstElectronMediumMVA5",
+                "againstElectronTight",
+                "againstElectronTightMVA5",
+                "againstElectronVLooseMVA5",
+                "againstElectronVTightMVA5",
+                "againstMuonLoose",
+                "againstMuonLoose2",
+                "againstMuonLoose3",
+                "againstMuonLooseMVA",
+                "againstMuonMVAraw",
+                "againstMuonMedium",
+                "againstMuonMedium2",
+                "againstMuonMediumMVA",
+                "againstMuonTight",
+                "againstMuonTight2",
+                "againstMuonTight3",
+                "againstMuonTightMVA",
+                "byCombinedIsolationDeltaBetaCorrRaw3Hits",
+                "byIsolationMVA3newDMwLTraw",
+                "byIsolationMVA3newDMwoLTraw",
+                "byIsolationMVA3oldDMwLTraw",
+                "byIsolationMVA3oldDMwoLTraw",
+                "byLooseCombinedIsolationDeltaBetaCorr3Hits",
+                "byLooseIsolationMVA3newDMwLT",
+                "byLooseIsolationMVA3newDMwoLT",
+                "byLooseIsolationMVA3oldDMwLT",
+                "byLooseIsolationMVA3oldDMwoLT",
+                "byMediumCombinedIsolationDeltaBetaCorr3Hits",
+                "byMediumIsolationMVA3newDMwLT",
+                "byMediumIsolationMVA3newDMwoLT",
+                "byMediumIsolationMVA3oldDMwLT",
+                "byMediumIsolationMVA3oldDMwoLT",
+                "byTightCombinedIsolationDeltaBetaCorr3Hits",
+                "byTightIsolationMVA3newDMwLT",
+                "byTightIsolationMVA3newDMwoLT",
+                "byTightIsolationMVA3oldDMwLT",
+                "byTightIsolationMVA3oldDMwoLT",
+                "byVLooseIsolationMVA3newDMwLT",
+                "byVLooseIsolationMVA3newDMwoLT",
+                "byVLooseIsolationMVA3oldDMwLT",
+                "byVLooseIsolationMVA3oldDMwoLT",
+                "byVTightIsolationMVA3newDMwLT",
+                "byVTightIsolationMVA3newDMwoLT",
+                "byVTightIsolationMVA3oldDMwLT",
+                "byVTightIsolationMVA3oldDMwoLT",
+                "byVVTightIsolationMVA3newDMwLT",
+                "byVVTightIsolationMVA3newDMwoLT",
+                "byVVTightIsolationMVA3oldDMwLT",
+                "byVVTightIsolationMVA3oldDMwoLT",
+                "chargedIsoPtSum",
+                "decayModeFinding",
+                "decayModeFindingNewDMs",
+                "neutralIsoPtSum",
+                "puCorrPtSum"
+	    ),
+            filter = cms.untracked.bool(False)
+        )
+    ),
+    Electrons = cms.VPSet(
+        cms.PSet(
+            branchname = cms.untracked.string("Electrons"),
+            src = cms.InputTag("slimmedElectrons"),
+            discriminators = cms.vstring()
+        )
+    ),
+    Muons = cms.VPSet(   
+        cms.PSet(
+            branchname = cms.untracked.string("Muons"),   
+            src = cms.InputTag("slimmedMuons"),    
+            discriminators = cms.vstring() 
+        )   
+    ),
+    Jets = cms.VPSet(      
+        cms.PSet(
+            branchname = cms.untracked.string("Jets"),       
+            src = cms.InputTag("slimmedJets"),      
+            discriminators = cms.vstring(
+                "jetBProbabilityBJetTags",
+                "jetProbabilityBJetTags",
+                "trackCountingHighPurBJetTags", 
+                "trackCountingHighEffBJetTags",
+                "simpleSecondaryVertexHighEffBJetTags",
+                "simpleSecondaryVertexHighPurBJetTags",
+                "combinedSecondaryVertexBJetTags",
+                "combinedInclusiveSecondaryVertexBJetTags",
+                "combinedInclusiveSecondaryVertexV2BJetTags",
+            ),
+	    userFloats = cms.vstring(
+		"pileupJetId:fullDiscriminant"
+	    ),
+        )
+    ),
+    METs = cms.VPSet(
+        cms.PSet(
+            branchname = cms.untracked.string("MET_Type1"),
+            src = cms.InputTag("slimmedMETs")
+        ),
+    )
+)
+
+process.load("HiggsAnalysis.MiniAOD2TTree.SignalAnalysisSkim_cfi")
+
+process.skimCounterAll    = cms.EDProducer("HPlusEventCountProducer")
+process.skimCounterPassed = cms.EDProducer("HPlusEventCountProducer")
+
+
+# module execution
+process.runEDFilter = cms.Path(process.skimCounterAll*process.skim*process.skimCounterPassed*process.dump)
+
+#process.output = cms.OutputModule("PoolOutputModule",
+#    outputCommands = cms.untracked.vstring(
+#        "keep *",
+#    ),
+#    fileName = cms.untracked.string("CMSSW.root")
+#)
+#process.out_step = cms.EndPath(process.output)
