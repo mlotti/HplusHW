c5f18e09d2fdcecbab65f05130b38b2f2089e9c0
S. Lehti <sami.lehti@cern.ch>
2015-07-15 13:49:22 +0200

    MiniAOD2TTree: added options, dataVersion, GT in gen2TTree_cfg.py

--- MiniAOD2TTree/test/gen2TTree_cfg.py
+++ MiniAOD2TTree/test/gen2TTree_cfg.py
@@ -1,7 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 import HiggsAnalysis.MiniAOD2TTree.tools.git as git #HiggsAnalysis.HeavyChHiggsToTauNu.tools.git as git
+from HiggsAnalysis.HeavyChHiggsToTauNu.HChOptions import getOptionsDataVersion
 
 process = cms.Process("TTreeDump")
+
+dataVersion = "74Xmc"
+
+options, dataVersion = getOptionsDataVersion(dataVersion)
+print dataVersion
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -9,7 +15,7 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-	'file:events_MG5_aMCatNLO_TTbar_H160_taunu_13TeV_GEN_6.root'
+	'file:ChargedHiggs_heavy_M200_v2.root'
 #	'file:PYTHIA8_Tauola_TTbar_H160_taunu_13TeV_cff_py_GEN.root'
 #	'file:MG5_aMCatNLO_PYTHIA8_Tauola_TTbar_H160_taunu_13TeV_GEN.root'
 #       'file:/afs/cern.ch/work/e/epekkari/GEN2015/events_MG5_aMCatNLO_TTbar_H200_taunu_13TeV_GEN_4.root'
@@ -17,6 +23,11 @@ process.source = cms.Source("PoolSource",
 #       'file:/afs/cern.ch/work/e/epekkari/GEN/events_MG5_aMCatNLO_TTbar_H160_taunu_13TeV_GEN_0.root'
     )
 )
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, str(dataVersion.getGlobalTag()), '')
+print "GlobalTag="+dataVersion.getGlobalTag()
 
 process.dump = cms.EDFilter('MiniAOD2TTreeFilter',
     OutputFileName = cms.string("miniaod2tree.root"),
