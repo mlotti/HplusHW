e21d0e2073673ddbf53627e1a2cb5ad7996e0312
Lauri A. Wendland <lauri.wendland@cern.ch>
2015-10-14 13:43:15 +0300

    validation: added unit test

--- NtupleAnalysis/python/tools/histogramsExtras.py
+++ NtupleAnalysis/python/tools/histogramsExtras.py
@@ -14,3 +14,24 @@ def makeFlowBinsVisible(histo):
     moveBinContent(0,1,histo)
     moveBinContent(histo.GetNbinsX()+1,histo.GetNbinsX(),histo)
 
+if __name__ == "__main__":
+    import unittest
+    class TestFiles(unittest.TestCase):
+        def testFlowBinsVisible(self):
+            n = 10
+            h = ROOT.TH1F("h","h",n,0,n)
+            for i in range(n+2):
+                h.Fill(i-1)
+            self.assertEqual(h.GetNbinsX(), n)
+            self.assertEqual(h.GetBinContent(0), 1)
+            self.assertEqual(h.GetBinContent(1), 1)
+            self.assertEqual(h.GetBinContent(n+1), 1)
+            self.assertEqual(h.GetBinContent(n+2), 1)
+            makeFlowBinsVisible(h)
+            self.assertEqual(h.GetNbinsX(), n)
+            self.assertEqual(h.GetBinContent(0), 0)
+            self.assertEqual(h.GetBinContent(1), 2)
+            self.assertEqual(h.GetBinContent(n), 2)
+            self.assertEqual(h.GetBinContent(n+1), 0)
+
+    unittest.main()
