7fb4ce1bf9b533a4f6c658b5601b4c4d403d54cb
Lauri A. Wendland <lauri.wendland@cern.ch>
2015-12-11 18:20:25 +0200

    eventselection: added support for reading SF's from jsons

--- NtupleAnalysis/python/parameters/scaleFactors.py
+++ NtupleAnalysis/python/parameters/scaleFactors.py
@@ -1,4 +1,5 @@
 from HiggsAnalysis.NtupleAnalysis.main import PSet
+import json
 
 # This file contains all the scale factors and their uncertainties used in the analysis
 # There are two types of scale factors:
@@ -61,16 +62,14 @@ def _assignJetToTauSF(tauSelectionPset, etaRegion, dirNumber):
 # \param direction         "nominal, "up", "down"
 # \param variationType     "MC", "data"  (the uncertainty in MC and data are variated separately)
 def assignTauTriggerSF(tauSelectionPset, direction, variationType="MC"):
-    binLeftEdges = [50, 60, 70, 80, 100]
-    scaleFactors = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-    scaleFactorsDataUp = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-    scaleFactorsDataDown = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-    scaleFactorsMCUp = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-    scaleFactorsMCDown = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    # FIXME: there is no mechanic right now to choose correct era / run range
+    # FIXME: this approach works as long as there is just one efficiency for the simulated samples
+    reader = TriggerSFJsonReader("2015D", "runs_256629_260627", "tauLegTriggerEfficiency2015.json")
+    result = reader.getResult()
     if variationType == "MC":
-        _assingSF(binLeftEdges, scaleFactors, scaleFactorsMCUp, scaleFactorsMCDown, tauSelectionPset, direction)
+        _assignSF(result["binEdges"], result["SF"], result["mcup"], result["mcdown"], tauSelectionPset, direction)
     elif variationType == "data":
-        _assingSF(binLeftEdges, scaleFactors, scaleFactorsDataUp, scaleFactorsDataDown, tauSelectionPset, direction)
+        _assignSF(result["binEdges"], result["SF"], result["dataup"], result["datadown"], tauSelectionPset, direction)
     else:
         raise Exception("Error: Unsupported variation type '%s'! Valid options are: 'MC', 'data'"%variationType)
 
@@ -80,11 +79,8 @@ def assignTauTriggerSF(tauSelectionPset, direction, variationType="MC"):
 ##===== Btag SF 
 
 
-##===== Top pT SF
-
-
-
-def _assingSF(binEdges, SF, SFup, SFdown, pset, direction):
+## Helper function
+def _assignSF(binEdges, SF, SFup, SFdown, pset, direction):
     if not direction in ["nominal", "up", "down"]:
         raise Exception("Error: unknown option for SF direction('%s')!"%direction)
     myScaleFactors = SF[:]
@@ -97,5 +93,76 @@ def _assingSF(binEdges, SF, SFup, SFdown, pset, direction):
         scaleFactors = myScaleFactors
     )
 
+## Reader for trigger SF json files
+class TriggerSFJsonReader:
+    def __init__(self, era, runrange, jsonname):
+        # Read json
+        _jsonpath = os.path.join(os.getenv("HIGGSANALYSIS_BASE"), "NtupleAnalysis", "data", "TriggerEfficiency")
+        filename = os.path.join(_jsonpath, jsonname)
+        if not os.path.exists(filename):
+            raise Exception("Error: file '%s' does not exist!"%filename)
+        f = open(filename)
+        contents = json.load(f)
+        f.close()
+        # Obtain data efficiencies
+        param = "dataParameters"
+        if not param in contents.keys():
+            raise Exception("Error: missing key '%s' in json '%s'! Options: %s"%(param,filename,", ".join(map(str,contents.keys()))))
+        if not runrange in contents[param].keys():
+            raise Exception("Error: missing run range '%s' for data in json '%s'!"(runrange,filename,", ".join(map(str,contents[param].keys()))))
+        if not era in contents[param][runrange].keys():
+            raise Exception("Error: missing era '%s' for data in json '%s'!"(runrange,filename,", ".join(map(str,contents[param][runrange].keys()))))
+        datadict = self._readValues(contents[param][runrange][era], "data")
+        # Obtain MC efficiencies
+        param = "mcParameters"
+        if not param in contents.keys():
+            raise Exception("Error: missing key '%s' in json '%s'! Options: %s"%(param,filename,", ".join(map(str,contents.keys()))))
+        if not era in contents[param].keys():
+            raise Exception("Error: missing era '%s' for mc in json '%s'!"(runrange,filename,", ".join(map(str,contents[param].keys()))))
+        mcdict = self._readValues(contents[param][runrange][era], "mc")
+        # Calculate SF's
+        keys = datadict.keys()
+        if len(keys) != len(mcdict.keys()):
+            raise Exception("Error: different number of bins for data and mc in json '%s'!"%filename)
+        keys.sort()
+        self.result = {}
+        self.result["binEdges"] = []
+        self.result["SF"] = []
+        self.result["SFdataUp"] = []
+        self.result["SFdataDown"] = []
+        self.result["SFmcUp"] = []
+        self.result["SFmcDown"] = []
+        i = 0
+        for key in keys:
+            if i > 0:
+                self.result["binEdges"].append(key)
+            i += 1
+            self.result["SF"].append(datadict[key]["eff"] / mcdict[key]["eff"])
+            self.result["SFdataUp"].append(datadict[key]["effup"] / mcdict[key]["eff"])
+            self.result["SFdataDown"].append(datadict[key]["effdown"] / mcdict[key]["eff"])
+            self.result["SFmcUp"].append(datadict[key]["eff"] / mcdict[key]["effup"])
+            if abs(mcdict[key]["effdown"] - 0.00001):
+                raise Exception("Down variation in bin '%s' is zero in json '%s'"%(key, filename))
+            self.result["SFmcDown"].append(datadict[key]["eff"] / mcdict[key]["effdown"])
 
-
+    def getResult(self):
+        return self.result
+        
+    def _readValues(inputdict, label):
+        outdict = {}
+        for item in inputdict["bins"]:
+            bindict = {}
+            bindict[label+"eff"] = item["efficiency"]
+            # FIXME: check if the uncertainties are relative or absolute uncertainties
+            value = item["efficiency"]+item["efficiencyPlus"]
+            if value > 1.0:
+                bindict[label+"effup"] = 1.0
+            else:
+                bindict[label+"effup"] = value
+            value = item["efficiency"]+item["efficiencyMinus"]
+            if value < 0.0:
+                bindict[label+"effdown"] = 0.0
+            else:
+                bindict[label+"effdown"] = value
+            outdict[item["pt"]] = bindict
+        return outdict
