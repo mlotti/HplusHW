4ddd4a005b5e81696118e8fa9bbfee57a2443087
Lauri A. Wendland <lauri.wendland@cern.ch>
2015-12-11 17:23:05 +0200

    framework: small fix

--- NtupleAnalysis/python/main.py
+++ NtupleAnalysis/python/main.py
@@ -334,7 +334,8 @@ class Process:
                     # ttbar status for top pt corrections
                     ttbarStatus = "0"
                     useTopPtCorrection = analyzer.exists("useTopPtWeights") and analyzer.__getattr__("useTopPtWeights")
-                    if useTopPtCorrection and dset.getName().startswith("TTJets"):
+                    useTopPtCorrection = useTopPtCorrection and dset.getName().startswith("TTJets")
+                    if useTopPtCorrection:
                         ttbarStatus = "1"
                     inputList.Add(ROOT.TNamed("isttbar", ttbarStatus))
                     # Pileup reweighting
