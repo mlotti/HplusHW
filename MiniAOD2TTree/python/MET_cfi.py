8f9f9ae027d602af28c2b89605a97d59ee701f34
S. Lehti <sami.lehti@cern.ch>
2016-01-15 12:34:30 +0100

    Disabled MET_Type1_NoHF

--- MiniAOD2TTree/python/MET_cfi.py
+++ MiniAOD2TTree/python/MET_cfi.py
@@ -5,10 +5,10 @@ METs = cms.VPSet(
         branchname = cms.untracked.string("MET_Type1"),
         src = cms.InputTag("slimmedMETs")
     ),
-    cms.PSet(
-        branchname = cms.untracked.string("MET_Type1_NoHF"),
-        src = cms.InputTag("slimmedMETsNoHF")
-    ),
+#    cms.PSet(
+#        branchname = cms.untracked.string("MET_Type1_NoHF"),
+#        src = cms.InputTag("slimmedMETsNoHF")
+#    ),
     cms.PSet(
         branchname = cms.untracked.string("MET_Puppi"),
         src = cms.InputTag("slimmedMETsPuppi")
