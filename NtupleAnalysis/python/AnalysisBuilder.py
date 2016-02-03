7ab758472ee8f3ecde61d796bf97d124052017fe
Lauri A. Wendland <lauri.wendland@cern.ch>
2016-02-03 09:51:38 +0200

    framework: Added nominal module for optimization runs

--- NtupleAnalysis/python/AnalysisBuilder.py
+++ NtupleAnalysis/python/AnalysisBuilder.py
@@ -173,19 +173,19 @@ class AnalysisBuilder:
                 modStr = "%s_%s_Run%s"%(self._name, searchMode, dataEra)
                 # Create nominal module without any variation
                 if "systematics" in self._variations.keys():
-                    if len(self._variations.keys()) == 1:
+                    if len(self._variations.keys()) > 1:
                         configs.append(AnalysisConfig(self._name, modStr, config))
                         print "Created nominal module: %s"%modStr
                 else:
-                    if len(self._variations.keys()) == 0:
-                        configs.append(AnalysisConfig(self._name, modStr, config))
-                        print "Created nominal module: %s"%modStr
+                    configs.append(AnalysisConfig(self._name, modStr, config))
+                    print "Created nominal module: %s"%modStr
                 # Create modules for optimization and systematics variations
                 configs.extend(self._buildVariation(config, modStr))
         # Register the modules
         for module in configs:
             module.registerAnalysis(process)
         print "\nAnalysisBuilder created %d modules\n"%len(configs)
+        raise Exception
         #print configs[0]._config
     
     ## Builds iteratively the variations
