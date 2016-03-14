e00737e421609ec7b209aec84b7b09513bb5e965
S.Lehti <sami.lehti@cern.ch>
2016-03-14 16:57:08 +0200

    DataFormat: added support for reading all the genPArticles

--- NtupleAnalysis/src/DataFormat/src/GenParticleGenerated.cc
+++ NtupleAnalysis/src/DataFormat/src/GenParticleGenerated.cc
@@ -7,6 +7,7 @@
 #include "Framework/interface/BranchManager.h"
 
 void GenParticleGeneratedCollection::setupBranches(BranchManager& mgr) {
+  fGenParticles.setupBranches(mgr);
   fGenElectron.setupBranches(mgr);
   fGenHplus.setupBranches(mgr);
   fGenHplusNeutrinos.setupBranches(mgr);
@@ -32,6 +33,13 @@ void GenParticleGeneratedCollection::setupBranches(BranchManager& mgr) {
   mgr.book("genParticles_GenTauProngs", &fGenTauProngs);
   mgr.book("genParticles_GenTopDecayMode", &fGenTopDecayMode);
   mgr.book("genParticles_GenWDecayMode", &fGenWDecayMode);
+}
+
+const std::vector<Particle<ParticleCollection<double>>> GenParticleGeneratedCollection::getGenParticles() const {
+  std::vector<Particle<ParticleCollection<float_type>>> v;
+  for (size_t i = 0; i < fGenParticles.size(); ++i)
+    v.push_back(Particle<ParticleCollection<float_type>>(&fGenParticles, i));
+  return v;
 }
 
 const std::vector<Particle<ParticleCollection<double>>> GenParticleGeneratedCollection::getGenElectronCollection() const {
