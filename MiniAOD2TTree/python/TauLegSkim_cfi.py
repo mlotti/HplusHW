cf2df0114940b565c0d389a8c9c851a63413316e
S. Lehti <sami.lehti@cern.ch>
2016-06-07 16:24:01 +0200

    Updated triggers to 2016 triggers

--- MiniAOD2TTree/python/TauLegSkim_cfi.py
+++ MiniAOD2TTree/python/TauLegSkim_cfi.py
@@ -2,11 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 skim = cms.EDFilter("TauLegSkim",
     TriggerResults = cms.InputTag("TriggerResults::HLT"),
-    HLTPaths       = cms.vstring("HLT_IsoMu16_eta2p1_CaloMET30_v",
-                                 "HLT_IsoMu16_eta2p1_MET30_JetIdCleaned_v",
-                                 "HLT_IsoMu17_eta2p1_v",
-                                 "HLT_IsoMu20_eta2p1_v",
-                                 "HLT_IsoMu24_eta2p1_v"
+    HLTPaths       = cms.vstring("HLT_IsoMu16_eta2p1_MET30_v",
+                                 "HLT_IsoMu18_v",
+                                 "HLT_IsoMu20_v",
+                                 "HLT_IsoMu22_v"
     ),
     TauCollection  = cms.InputTag("slimmedTaus"),
     TauDiscriminators = cms.vstring(
