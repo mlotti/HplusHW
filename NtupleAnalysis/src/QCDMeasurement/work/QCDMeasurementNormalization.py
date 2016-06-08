be4321032ddff2bc15250ee13294c5709bc19b75
S.Lehti <sami.lehti@cern.ch>
2016-06-08 12:41:46 +0300

    Updated the EWK fit in QCDMeasurementNormalization.py

--- NtupleAnalysis/src/QCDMeasurement/work/QCDMeasurementNormalization.py
+++ NtupleAnalysis/src/QCDMeasurement/work/QCDMeasurementNormalization.py
@@ -33,7 +33,7 @@ _rebinFactor = 10
 
 print "Analysis name:", analysis
 
-selectOnlyBins = []#["Inclusive"] #["1"]
+selectOnlyBins = ["Inclusive"] #["1"]
 
 def usage():
     print "\n"
@@ -91,7 +91,7 @@ def main(argv, dsetMgr, moduleInfoString):
         myMergeList.append("TTJets") # Madgraph with negative weights
         print "Warning: using TTJets as input, but this is suboptimal. Please switch to the TT sample (much more stats.)."
     myMergeList.append("WJetsHT")
-    myMergeList.append("DYJetsToLLHT")
+####    myMergeList.append("DYJetsToLLHT")
     myMergeList.append("SingleTop")
     if "Diboson" in dsetMgr.getMCDatasetNames():
         myMergeList.append("Diboson")
@@ -199,7 +199,7 @@ def main(argv, dsetMgr, moduleInfoString):
         template_EWKInclusive_Baseline.setFitter(QCDNormalization.FitFunction("EWKFunction", boundary=boundary, norm=1, rejectPoints=1),
                                                  FITMIN, FITMAX)
         template_EWKInclusive_Baseline.setDefaultFitParam(defaultLowerLimit=[0.5,  90, 30, 0.0001],
-                                                          defaultUpperLimit=[ 20, 150, 50,    1.0])
+                                                          defaultUpperLimit=[ 20, 150, 60,    1.0])
         # Note that the same function is used for QCD only and QCD+EWK fakes
         template_QCD_Inverted.setFitter(QCDNormalization.FitFunction("QCDFunction", norm=1), FITMIN, FITMAX)
         template_QCD_Inverted.setDefaultFitParam(defaultLowerLimit=[0.0001, 0.001, 0.1, 0.0,  10, 0.0001, 0.001],
@@ -267,7 +267,7 @@ def main(argv, dsetMgr, moduleInfoString):
             manager.plotTemplates()
             
             #===== Fit individual templates to data
-            fitOptions = "R B" # RBLW
+            fitOptions = "R BLW" # RBLW
             manager.calculateNormalizationCoefficients(hmetBase_data, fitOptions, FITMIN, FITMAX)
             
             #===== Calculate combined normalisation coefficient (f_fakes = w*f_QCD + (1-w)*f_EWKfakes)
