3b24aaac3795c986c59f883049a0e4765e21a54e
S. Lehti <sami.lehti@cern.ch>
2016-06-10 16:41:13 +0200

    datasets.py: added signal dataset

--- MiniAOD2TTree/test/datasets.py
+++ MiniAOD2TTree/test/datasets.py
@@ -24,3 +24,8 @@ datasetsMuonData.append(Dataset('/SingleMuon/Run2016B-PromptReco-v1/MINIAOD',dat
 datasetsMuonData.append(Dataset('/SingleMuon/Run2016B-PromptReco-v2/MINIAOD',dataVersion="80Xdata"))
 
 
+datasetsTop =[]
+datasetsTop.append(Dataset('/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext4-v1/MINIAODSIM'))
+
+datasetsSignal = []
+datasetsSignal.append(Dataset('/ChargedHiggs_HplusTB_HplusToTB_M-400_13TeV_amcatnlo_pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'))
