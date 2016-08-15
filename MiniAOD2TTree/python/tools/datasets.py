388171d13712a71df4965232e19541170ad60ae1
S. Lehti <sami.lehti@cern.ch>
2016-08-15 11:52:50 +0200

    Changed JSON to the ICHEP golden json 12.9 bf-1

--- MiniAOD2TTree/python/tools/datasets.py
+++ MiniAOD2TTree/python/tools/datasets.py
@@ -1,4 +1,4 @@
-lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-277148_13TeV_PromptReco_Collisions16_JSON.txt"
+lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt" # ICHEP dataset 271036-276811
 
 #================================================================================================ 
 # Class Definition
