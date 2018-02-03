71e8eef2dedf017c1e6d1036a3544b99f15267fe
Marina Kolosova <marina.kolosova@cern.ch>
2018-02-03 10:55:53 -0600

    miniaod2ttree: Removed AK8 related variables from JetDumper

--- MiniAOD2TTree/src/JetDumper.cc
+++ MiniAOD2TTree/src/JetDumper.cc
@@ -82,10 +82,6 @@ JetDumper::JetDumper(edm::ConsumesCollector&& iConsumesCollector, std::vector<ed
       systJERup = new FourVectorDumper[inputCollections.size()];
       systJERdown = new FourVectorDumper[inputCollections.size()];
     }
-    // Marina - start
-    //nSubjets     = new std::vector<int>[inputCollections.size()];
-    //hasBTagSubjet= new std::vector<bool>[inputCollections.size()];
-    // Marina - end
 }
 
 JetDumper::~JetDumper(){}
@@ -145,14 +141,6 @@ void JetDumper::book(TTree* tree){
       systJERup[i].book(tree, name, "JERup");
       systJERdown[i].book(tree, name, "JERdown");
     }
-    
-    // Marina - start
-    //bool checkSubjets = inputCollections[i].getParameter<bool>("checkSubjets");
-    //if (checkSubjets){
-    //  tree->Branch((name+"_nSubjets").c_str(),&nSubjets[i]);
-    //  tree->Branch((name+"_hasBTagSubjet").c_str(), &hasBTagSubjet[i]);
-    // }
-    // Marina - end
   }
 }
 
@@ -183,6 +171,9 @@ bool JetDumper::fill(edm::Event& iEvent, const edm::EventSetup& iSetup){
       }
     }
 */
+    
+
+
     edm::Handle <reco::GenParticleCollection> genParticlesHandle;
     if (!iEvent.isRealData())
       iEvent.getByToken(genParticleToken, genParticlesHandle);
@@ -192,10 +183,6 @@ bool JetDumper::fill(edm::Event& iEvent, const edm::EventSetup& iSetup){
 	std::vector<std::string> userfloatNames = inputCollections[ic].getParameter<std::vector<std::string> >("userFloats");
 	std::vector<std::string> userintNames = inputCollections[ic].getParameter<std::vector<std::string> >("userInts");
 	
-	// Marina - start
-	//bool checkSubjets = inputCollections[ic].getParameter<bool>("checkSubjets");
-	// Marina - end
-
         edm::Handle<edm::View<pat::Jet>> jetHandle;
         iEvent.getByToken(jetToken[ic], jetHandle);
 
@@ -376,22 +363,6 @@ bool JetDumper::fill(edm::Event& iEvent, const edm::EventSetup& iSetup){
                   // JER
 */                
                 }
-		// Marina - start		
-		//if (checkSubjets){
-		//  auto &subjets = obj.subjets("SoftDrop");
-		  
-		//  int nSub = 0;
-		//  bool hasBSubjet = false;
-		//  for (auto const & sj: subjets)
-		//    {
-		//      float csv = sj->bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags");
-		//      if (csv >= 0.8484) hasBSubjet = true;   // Medium working point (Recommended for 2016 Analysis)
-		//     nSub++;
-		//    }
-		//  nSubjets[ic].push_back(nSub);
-		//  hasBTagSubjet[ic].push_back(hasBSubjet);
-		//	}
-		// Marina - end
             }
         }
     }
@@ -432,10 +403,6 @@ void JetDumper::reset(){
           systJERup[ic].reset();
           systJERdown[ic].reset();
 	}
-	// Marina - start
-	//nSubjets[ic].clear();
-	//hasBTagSubjet[ic].clear();
-	// Marina - end
     }
     for(size_t ic = 0; ic < inputCollections.size()*nDiscriminators; ++ic){
         discriminators[ic].clear();
