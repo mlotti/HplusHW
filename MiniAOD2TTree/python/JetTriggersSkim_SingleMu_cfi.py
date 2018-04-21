1b4d055f60837668075e4248b3702c8a1df2eff0
Sotiroulla Konstantinou <skonstan@cern.ch>
2018-04-21 11:34:38 -0500

    conflict resolved

--- MiniAOD2TTree/python/JetTriggersSkim_SingleMu_cfi.py
+++ MiniAOD2TTree/python/JetTriggersSkim_SingleMu_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 skim = cms.EDFilter("JetTriggersSkim",
     Verbose = cms.bool(False),
     TriggerResults = cms.InputTag("TriggerResults::HLT"),
-    HLTPaths       = cms.vstring("HLT_Mu50_v", 
+    HLTPaths       = cms.vstring("HLT_IsoMu24_v", 
                                 ),
     # Jets (https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID#Recommendations_for_13_TeV_data)
     JetCollection  = cms.InputTag("slimmedJets"),
@@ -27,7 +27,7 @@ skim = cms.EDFilter("JetTriggersSkim",
     ElectronRhoSource    = cms.InputTag("fixedGridRhoFastjetCentralNeutral"),
     ElectronMiniRelIsoEA = cms.double(0.2), # MIT cut is at 0.40. Allow wiggle room by cutting at LOWER value
     ElectronPtCut        = cms.double(10.0),
-    ElectronEtaCut       = cms.double(2.4),
+    ElectronEtaCut       = cms.double(2.1),
     ElectronNCut         = cms.int32(0),
 
     # Muons (https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideMuonIdRun2)
