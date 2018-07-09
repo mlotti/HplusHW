b29da4e27b3b8ccf1005e07e0abdd1cf95cba1bb
Alexandros Attikis <attikis@cern.ch>
2018-07-09 12:52:04 +0200

    combine: Enhanced GoF lxbatch script

--- NtupleAnalysis/src/LimitCalc/work/GoF.sh
+++ NtupleAnalysis/src/LimitCalc/work/GoF.sh
@@ -18,28 +18,55 @@
 if [ $# -eq 0 ]
 then
     echo "=== You must give exactly 1 argument; the datacard txt file name. For example:"
-    echo "./GoF.csh 500"
+    echo "./GoF.csh 500 saturated"
     echo
     exit 1
 fi
 
+if [ "${2}" != "saturated" ] && [ "${2}" != "KS" ]  && [ "${2}" != "AD" ]
+then
+    echo "=== Invalid algorith \"${2}\". Please select between \"saturated\", \"KS\", and \"AD\"."
+    exit 1
+fi
+
+
 #====================================================================================================
 # Define Variables
 #====================================================================================================
-nToys=1000
+nToys=10000
 nJobs=50
 queue="8nm"
 toysPerJob=$((nToys/nJobs))
 datacard="combine_datacard_hplushadronic_m${1}.txt"
+rootfile="combine_histograms_hplushadronic_m${1}.root"
+algorithm=${2} # "saturated", "KS" (Kolmogorov-Smirnov), AD (Anderson-Darling)
+dirname="GoF_${algorithm}"
 
 echo "=== Run GoF for $nToys toys over $nJobs for card $1 (#toys per job = $toysPerJob)"
 
+if [ -d ${dirname} ]; then
+    cp ${datacard} ${dirname}/.
+    cp ${rootfile} ${dirname}/.
+    cd ${dirname}
+    #ls -lt
+else
+    mkdir ${dirname}
+    cp ${datacard} ${dirname}/.
+    cp ${rootfile} ${dirname}/.
+    cd ${dirname}
+    #ls -lt
+fi
+
+# For-loop: All jobs
+echo "=== Submit job for toys (`pwd`)"
 for (( t=1; t<=$nJobs; t++ ))
 do
     echo "=== Submit job for toys $t/$nJobs"
-    combineTool.py -m ${1} -M GoodnessOfFit --algorithm saturated ${datacard} -t $toysPerJob --job-mode lxbatch --sub-opts="-q 8nm" --task-name $t -n toys$t --seed "$((123456*$t))"
+    combineTool.py -m ${1} -M GoodnessOfFit --algorithm ${algorithm} ${datacard} -t $toysPerJob --job-mode lxbatch --sub-opts="-q 8nm" --task-name $t -n toys$t --seed "$((123456*$t))"
 done
 
+echo "=== Run job on data:(`pwd`)"
+combineTool.py -m ${1} -M GoodnessOfFit --algorithm ${algorithm} ${datacard} -t 0 -n Data
+cd ..
 
-echo "=== Run job on data:"
-combineTool.py -m ${1} -M GoodnessOfFit --algorithm saturated ${datacard} -t 0 -n Data
+echo "=== Done"
